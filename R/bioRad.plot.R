bd00335e53749b6692cc7ae790037a56c8be9448
Adriaan Dokter <adriaandokter@gmail.com>
2016-11-30 18:11:17 -0500

    added sunrise/sunset function and added speed barbs to profile plots

--- R/bioRad.plot.R
+++ R/bioRad.plot.R
@@ -20,6 +20,10 @@ is.na2=function(x) is.na(x) & !is.nan(x)
 #' @param ylab a title for the y-axis
 #' @param quantity character string with the quantity to plot, one of '\code{dens}','\code{eta}','\code{dbz}','\code{DBZH}' for density, reflectivity, reflectivity factor and total reflectivity factor, respectively.
 #' @param log logical. Whether to display \code{quantity} data on a logarithmic scale
+#' @param barbs logical. Whether to plot overlay speed barbs
+#' @param barbs.h integer. Number of barbs to plot in altitudinal dimension
+#' @param barbs.t integer. Number of barbs to plot in temporal dimension
+#' @param barbs.dens numeric. Lower threshold in aerial density of individuals for plotting speed barbs
 #' @param zlim numerical atomic vector of length 2, specifying the range of \code{quantity} values to plot
 #' @param legend.ticks numeric atomic vector specifying the ticks on the color bar
 #' @param main a title for the plot
@@ -36,6 +40,8 @@ is.na2=function(x) is.na(x) & !is.nan(x)
 #'  \item{"\code{DBZH}"}{total reflectivity factor. This quantity equals the reflectivity factor of all scatterers (biological and meteorological scattering cambined)}
 #' }
 #'
+#' In the speed barbs, each half flag represents 2.5 m/s, each full flag 5 m/s, each pennant (triangle) 25 m/s
+#'
 #' @examples
 #' # locate example file:
 #' VPtable <- system.file("extdata", "VPtable.txt", package="bioRad")
@@ -45,8 +51,9 @@ is.na2=function(x) is.na(x) & !is.nan(x)
 #' plot(ts[1:500], ylim=c(0,3000))
 #' # plot total reflectivity factor (rain,birds,insects together):
 #' plot(ts[1:500], ylim=c(0,3000), quantity="DBZH")
-plot.VPTimeSeries = function(x, xlab="time [UTC]",ylab="height [m]",quantity="dens",log=T, zlim, legend.ticks, main,...){
+plot.VPTimeSeries = function(x, xlab="time [UTC]",ylab="height [m]",quantity="dens",log=T, barbs=T, barbs.h=10, barbs.t=20, barbs.dens=5, zlim, legend.ticks, main, ...){
   stopifnot(inherits(x,"VPTimeSeries"))
+  args <- list(...)
   if(!x$regular) warning("Irregular time-series: x-axis is not a linear time scale. Use 'regularize' to make time series regular.")
 
   # prepare zlim, ticks and legendticks
@@ -135,5 +142,176 @@ plot.VPTimeSeries = function(x, xlab="time [UTC]",ylab="height [m]",quantity="de
   # plotdata[is.na2(plotdata)]=(breaks[1]+breaks[2])/2
   # when vol2bird stdout also differentiates between NA and NaN:
   plotdata[is.na(plotdata)]=(breaks[2]+breaks[3])/2
+
+  #plot the image
   image.plot(x$dates,x$heights,plotdata,col=plot.colors,xlab=xlab,ylab=ylab,axis.args=axis.args,breaks=breaks,zlim=zlim,main=main,...)
+
+  # overlay speed barbs
+  if(barbs){
+    if("xlim" %in% names(args)){
+      stepsize.t=(max(args$xlim)-min(args$xlim))/barbs.t
+      t.barbs=seq(min(args$xlim)+stepsize.t/2, max(args$xlim)-stepsize.t/2,length.out=barbs.t)
+    }
+    else{
+      stepsize.t=(max(x$dates)-min(x$dates))/barbs.t
+      t.barbs=seq(x$dates[1]+stepsize.t/2, tail(x$dates,1)-stepsize.t/2,length.out=barbs.t)
+    }
+    if("ylim" %in% names(args)){
+      stepsize.h=(max(args$ylim)-min(args$ylim))/barbs.h
+      h.barbs=seq(x$heights[1]+stepsize.h/2, tail(x$heights,1)-stepsize.h/2,length.out=barbs.h)
+    }
+    else{
+      stepsize.h=(max(x$heights)-min(x$heights))/barbs.h
+      h.barbs=seq(x$heights[1]+stepsize.h/2, tail(x$heights,1)-stepsize.h/2,length.out=barbs.h)
+    }
+    barbdata=expand.grid(date=t.barbs,height=h.barbs)
+    barbdata$indext=sapply(barbdata$date,function(y) which.min(abs(x$dates - y)))
+    barbdata$indexh=sapply(barbdata$height,function(y) which.min(abs(x$heights - y)))
+    barbdata$ff=mapply(function(xx,yy) x$data$ff[xx,yy],barbdata$indexh,barbdata$indext)
+    barbdata$dd=mapply(function(xx,yy) x$data$dd[xx,yy],barbdata$indexh,barbdata$indext)
+    barbdata$dens=mapply(function(xx,yy) x$data$dens[xx,yy],barbdata$indexh,barbdata$indext)
+    barbdata=barbdata[barbdata$dens>barbs.dens,]
+    plot_wind_barbs(barbdata$date,barbdata$height,180+barbdata$dd,2*barbdata$ff,cex=0.7)
+  }
+}
+
+plot_wind_barbs = function(cx, cy, direction = 0, speed = NA, fill = rep(0, length(cx)), circle = FALSE, cex = 1, col = "black")
+{
+  ### press is actually height in upper air ###
+  ns = length(cx)
+  if (length(cy) != ns) stop("X AND Y COORDINATES SHOULD HAVE SAME LENGTH!")
+
+  msg = "ALL VARIABLES SHOULD HAVE SAME LENGTH AS COORDINATES, OR BE MISSING!!!"
+  if (ns > 1) {
+    if (length(direction) == 1) if (!is.na(direction)) stop(msg)
+    if (length(speed) == 1) if (!is.na(speed)) stop(msg)
+    if (length(fill) == 1) if (!is.na(fill)) stop(msg)
+    if (length(direction) > 1 & length(direction) != ns) stop(msg)
+    if (length(speed) > 1 & length(speed) != ns) stop(msg)
+    if (length(fill) > 1 & length(fill) != ns) stop(msg)
+  }
+
+  tpar = par()
+  size = tpar$csi
+  scalex = (tpar$usr[2] - tpar$usr[1]) / tpar$pin[1]
+  scaley = (tpar$usr[4] - tpar$usr[3]) / tpar$pin[2]
+  scalex = (cex * (scalex * size)) / 5
+  scaley = (cex * (scaley * size)) / 5
+  for (i in 1 : ns) {
+    x = cx[i]
+    y = cy[i]
+    if (is.na(x) | is.na(y)) next
+    spd = speed[i]
+
+    if (circle) {
+      ts = seq(0, 2 * pi, length.out = 200)
+      RX = sin(ts) * scalex
+      X1 = RX + x
+      RY = cos(ts) * scaley
+      Y1 = RY + y
+      if (!is.na(spd)) {
+        if (spd == 0) {
+          lines(RX * 2 + x, RY * 2 + y, col = col)
+        }
+      }
+      if (fill[i] > 0) {
+        lim = c(51, 101, 151, 200)
+        polygon(c(x, X1[1 : lim[fill[i]]]), c(y, Y1[1 : lim[fill[i]]]), density = -1, col = col)
+      }
+      lines(RX + x, RY + y, col = col)
+    } #end of circle
+
+    if (!is.na(spd)) {
+      if (spd > 0) {
+        X1 = 0
+        X2 = 0
+        Y1 = 0
+        Y2 = 5
+        if (spd >= 5 & spd < 10) {
+          X1 = c(X1, 0)
+          X2 = c(X2, 1)
+          Y1 = c(Y1, 5)
+          Y2 = c(Y2, 5)
+        }
+        if (spd >= 10 & spd < 15) {
+          X1 = c(X1, 0)
+          X2 = c(X2, 2)
+          Y1 = c(Y1, 5)
+          Y2 = c(Y2, 5)
+        }
+        if (spd >= 15 & spd < 20) {
+          X1 = c(X1, 0, 0)
+          X2 = c(X2, 1, 2)
+          Y1 = c(Y1, 4, 5)
+          Y2 = c(Y2, 4, 5)
+        }
+        if (spd >= 20 & spd < 25) {
+          X1 = c(X1, 0, 0)
+          X2 = c(X2, 2, 2)
+          Y1 = c(Y1, 4, 5)
+          Y2 = c(Y2, 4, 5)
+        }
+        if (spd >= 25 & spd < 30) {
+          X1 = c(X1, 0, 0, 0)
+          X2 = c(X2, 1, 2, 2)
+          Y1 = c(Y1, 3, 4, 5)
+          Y2 = c(Y2, 3, 4, 5)
+        }
+        if (spd >= 30 & spd < 35) {
+          X1 = c(X1, 0, 0, 0)
+          X2 = c(X2, 2, 2, 2)
+          Y1 = c(Y1, 3, 4, 5)
+          Y2 = c(Y2, 3, 4, 5)
+        }
+        if (spd >= 35 & spd < 40) {
+          X1 = c(X1, 0, 0, 0, 0)
+          X2 = c(X2, 1, 2, 2, 2)
+          Y1 = c(Y1, 2, 3, 4, 5)
+          Y2 = c(Y2, 2, 3, 4, 5)
+        }
+        if (spd >= 40 & spd < 45) {
+          X1 = c(X1, 0, 0, 0, 0)
+          X2 = c(X2, 2, 2, 2, 2)
+          Y1 = c(Y1, 2, 3, 4, 5)
+          Y2 = c(Y2, 2, 3, 4, 5)
+        }
+        if (spd >= 45 & spd < 50) {
+          X1 = c(X1, 0, 0, 0, 0, 0)
+          X2 = c(X2, 1, 2, 2, 2, 2)
+          Y1 = c(Y1, 1, 2, 3, 4, 5)
+          Y2 = c(Y2, 1, 2, 3, 4, 5)
+        }
+        if (spd >= 50 & spd < 55) {
+          X1 = c(X1, 0, 0)
+          X2 = c(X2, 2, 2)
+          Y1 = c(Y1, 4, 5)
+          Y2 = c(Y2, 4.5, 4.5)
+        }
+        if (spd >= 55 & spd < 60) {
+          X1 = c(X1, 0, 0, 0)
+          X2 = c(X2, 1, 2, 2)
+          Y1 = c(Y1, 3, 4, 5)
+          Y2 = c(Y2, 3, 4.5, 4.5)
+        }
+        if (spd >= 60 & spd < 65) {
+          X1 = c(X1, 0, 0, 0)
+          X2 = c(X2, 2, 2, 2)
+          Y1 = c(Y1, 3, 4, 5)
+          Y2 = c(Y2, 3, 4.5, 4.5)
+        }
+        dir = (direction[i] / 360) * 2 * pi
+        rot = cbind(c(cos(dir), -sin(dir)), c(sin(dir), cos(dir)))
+        S1 = rbind(X1, Y1)
+        S2 = rbind(X2, Y2)
+        S1 = rot %*% S1
+        S2 = rot %*% S2
+        S1 = S1 * c(scalex, scaley) + c(x, y)
+        S2 = S2 * c(scalex, scaley) + c(x, y)
+      }
+      if (spd > 0) {
+        segments(S1[1, ], S1[2, ], S2[1, ], S2[2, ], col = col, lwd = 1)
+      }
+    } #end of (!is.na(spd))
+  } #end of ns
+  invisible()
 }
