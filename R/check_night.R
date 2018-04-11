78e5028ff8bd1cb719ae16cebdb26cd26b197916
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-12 00:53:53 +0200

    Refactor day and night to check_night

--- R/check_night.R
+++ R/check_night.R
@@ -1,65 +1,80 @@
+
 #' Calculate whether it is night at a geographic location and time
-#' @inheritParams suntime
+#'
+#' The night check is based on the combination of lat, lon, date and the sun
+#' elevation. When testing a bioRad object for day/night time, this information
+#' is extracted from the bioRad data types directly
+#'
+#' @param x An object of class \code{numeric}, \code{vp},
+#' \code{vplist} or \code{vpts}.
+#' @param lon longitude in decimal degrees
+#' @param lat latitude in decimal degrees
+#' @param date date inhereting from class "\code{POSIXt}" or a string
+#' interpretable by \link[base]{as.Date}
+#' @param elev sun elevation in degrees
+#'
+#' @details The angular diameter of the sun is about 0.536 degrees, therefore
+#' the moment of sunrise/sunset corresponds to half that elevation at
+#' -0.268 degrees.
+#'
+#' check_night evaluates to FALSE when the sun has a higher elevation than
+#' parameter elev, otherwise to TRUE
+#'
+#' Approximate astronomical formula are used, therefore the day/night
+#' transition may be off by a few minutes
+#'
 #' @export
-#' @return TRUE when night, FALSE when day
-#' @details The angular diameter of the sun is about 0.536 degrees, therefore the moment
-#' of sunrise/sunset corresponds to half that elevation at -0.268 degrees.
+#' @return TRUE when night, FALSE when day, NA if unknown (either datetime or
+#' geographic location missing). For objects of class vpts an atomic logical
+#' vector
 #'
-#' day evaluates to true when the sun has a higher elevation than parameter elev, otherwise to false
-#'
-#' Approximate astronomical formula are used, therefore the day/night transition may
-#' be off by a few minutes
 #' @examples
 #' # it's day in the Netherlands at UTC noon on January first:
-#' night(5,53,"2016-01-01 12:00")
-#'
+#' check_night(5,53,"2016-01-01 12:00")
+#' # applied on the bioRad object classes
+#' check_night(VP)
+#' check_night(VPTS)
+check_night <- function(x, ..., elev=-0.268) UseMethod("check_night", x)
+
+#' @rdname check_night
 #' @export
-night=function(lon,lat,date,elev=-0.268){
-  trise=suntime(lon,lat,date,elev,rise=T)
-  tset=suntime(lon,lat,date,elev,rise=F)
-  output=rep(NA,length(date))
-  itsday=(date>trise & date<tset)
-  output[trise<tset]=itsday[trise<tset]
-  itsday=(date<tset | date>trise)
-  output[trise>=tset]=itsday[trise>=tset]
+check_night.default <- function(lon, lat, date, elev = -0.268){
+  trise <- suntime(lon, lat, date, elev, rise = TRUE)
+  tset <- suntime(lon, lat, date, elev, rise = FALSE)
+  output <- rep(NA, length(date))
+  itsday <- (date > trise & date < tset)
+  output[trise < tset] <- itsday[trise < tset]
+  itsday <- (date < tset | date > trise)
+  output[trise >= tset] <- itsday[trise >= tset]
   !output
 }
 
-#' Test a bioRad object for night time
-#'
-#' Test a bioRad object for day time. Dispatches to the logical inverse of \link{night}.
-#' @inheritParams night
-#' @param x An object of class \code{vp},\code{vplist} or \code{vpts}.
+#' @rdname check_night
 #' @export
-#' @return TRUE when day, FALSE when night, NA if unknown (either datetime or geographic location missing). For objects of class vpts an atomic logical vector
-#' @examples
-#' day(VP)
-day <- function (x, elev=-0.268) UseMethod("day", x)
-
-#' @rdname day
-#' @export
-day.vp <- function(x,elev=-0.268) {
-  stopifnot(inherits(x,"vp"))
-  !night(x$attributes$where$lon,x$attributes$where$lat,x$datetime,elev=elev)
+check_night.vp <- function(x, elev = -0.268) {
+  stopifnot(inherits(x, "vp"))
+  check_night(x$attributes$where$lon, x$attributes$where$lat,
+              x$datetime, elev = elev)
 }
 
-#' @rdname day
+#' @rdname check_night
 #' @export
-day.vplist <- function(x,elev=-0.268) {
-  stopifnot(inherits(x,"vplist"))
-  sapply(x,day.vp,elev=elev)
+check_night.vplist <- function(x, elev = -0.268) {
+  stopifnot(inherits(x, "vplist"))
+  sapply(x, check_night.vp, elev = elev)
 }
 
-#' @rdname day
+#' @rdname check_night
 #' @export
-day.vpts <- function(x,elev=-0.268) {
-  stopifnot(inherits(x,"vpts"))
-  !night(x$attributes$where$lon,x$attributes$where$lat,x$dates,elev=elev)
+check_night.vpts <- function(x, elev = -0.268) {
+  stopifnot(inherits(x, "vpts"))
+  check_night(x$attributes$where$lon, x$attributes$where$lat,
+         x$dates, elev = elev)
 }
 
-#' @rdname day
+#' @rdname check_night
 #' @export
-day.pvol <- function(x,elev=-0.268) {
-  stopifnot(inherits(x,"pvol"))
-  !night(x$geo$lon,x$geo$lat,x$datetime,elev=elev)
+check_night.pvol <- function(x, elev = -0.268) {
+  stopifnot(inherits(x, "pvol"))
+  check_night(x$geo$lon, x$geo$lat, x$datetime, elev = elev)
 }
