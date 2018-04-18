4db5b44fde611ab9bdb84d8c98266c8702c44dd4
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-18 21:06:09 +0200

    Update code formatting map

--- R/map.R
+++ R/map.R
@@ -58,76 +58,130 @@
 #' map(ppi, map = basemap, radar.size = 5, radar.color = "green")
 #' # crop the map:
 #' map(ppi, map = basemap, xlim = c(12.4, 13.2), ylim = c(56, 56.5))
-map <- function (x, ...) {
+map <- function(x, ...) {
   UseMethod("map", x)
 }
 
 #' @describeIn map plot a 'ppi' object on a map
 #' @export
-map.ppi=function(x,map,param,alpha=0.7,xlim,ylim,zlim=c(-20,20),ratio,radar.size=3,radar.color="red",n.color=1000,...){
-  stopifnot(inherits(x,"ppi"))
-  if(missing(param)){
-    if("DBZH" %in% names(x$data)) param="DBZH"
-    else param=names(x$data)[1]
-  }
-  else if(!is.character(param)) stop("'param' should be a character string with a valid scan parameter name")
-  if(missing(zlim)) zlim=get_zlim(param)
-  if(!(param %in% names(x$data))) stop(paste("no scan parameter '",param,"' in this ppi",sep=""))
-  if(!attributes(map)$ppi) stop("not a ppi map, use download_basemap() to download a map")
-  if(attributes(map)$geo$lat!=x$geo$lat || attributes(map)$geo$lon!=x$geo$lon) stop("not a basemap for this radar location")
-  # extract the scan parameter
-  data=do.call(function(y) x$data[y],list(param))
-  wgs84=CRS("+proj=longlat +datum=WGS84")
-  epsg3857=CRS("+init=epsg:3857") # this is the google mercator projection
-  mybbox=suppressWarnings(spTransform(SpatialPoints(t(data@bbox),proj4string=data@proj4string),CRS("+init=epsg:3857")))
-  mybbox.wgs=suppressWarnings(spTransform(SpatialPoints(t(data@bbox),proj4string=data@proj4string),wgs84))
-  e=raster::extent(mybbox.wgs)
-  r <- raster(raster::extent(mybbox), ncol=data@grid@cells.dim[1]*.9, nrow=data@grid@cells.dim[2]*.9,crs=CRS(proj4string(mybbox)))
-  # convert to google earth mercator projection
-  data=suppressWarnings(as.data.frame(spTransform(data,CRS("+init=epsg:3857"))))
-  # bring z-values within plotting range
-  index=which(data$z<zlim[1])
-  if(length(index)>0) data[index,]$z=zlim[1]
-  index=which(data$z>zlim[2])
-  if(length(index)>0) data[index,]$z=zlim[2]
-  # rasterize
-  r<-raster::rasterize(data[,2:3], r, data[,1])
-  # assign colors
-  if(param %in% c("VRADH","VRADV","VRAD")) cols=add_color_transparency(colorRampPalette(colors=c("blue","white","red"),alpha=TRUE)(n.color),alpha=alpha)
-  else cols=add_color_transparency(colorRampPalette(colors=c("lightblue","darkblue","green","yellow","red","magenta"),alpha=TRUE)(n.color),alpha=alpha)
+map.ppi <- function(x, map, param, alpha = 0.7, xlim, ylim,
+                    zlim = c(-20, 20), ratio, radar.size = 3,
+                    radar.color = "red", n.color = 1000, ...){
+  stopifnot(inherits(x, "ppi"))
 
-  color_scale=function(value,lim){
-    output=rep(0,length(value))
-    output=round((value-lim[1])/(lim[2]-lim[1])*n.color)
-    output[output>n.color]=n.color
-    output[output<1]=1
+  if (missing(param)) {
+    if ("DBZH" %in% names(x$data)) {
+      param <- "DBZH"
+    } else {
+      param <- names(x$data)[1]
+    }
+  } else if (!is.character(param)) {
+    stop("'param' should be a character string with a valid ",
+         "scan parameter name.")
+  }
+  if (missing(zlim)) {
+    zlim <- get_zlim(param)
+  }
+  if (!(param %in% names(x$data))) {
+    stop(paste("no scan parameter '", param, "' in this ppi", sep = ""))
+  }
+  if (!attributes(map)$ppi) {
+    stop("Not a ppi map, use download_basemap() to download a map.")
+  }
+  if (attributes(map)$geo$lat != x$geo$lat ||
+      attributes(map)$geo$lon != x$geo$lon) {
+    stop("Not a basemap for this radar location.")
+  }
+
+  # extract the scan parameter
+  data <- do.call(function(y) x$data[y], list(param))
+  wgs84 <- CRS("+proj=longlat +datum=WGS84")
+  epsg3857 <- CRS("+init=epsg:3857") # this is the google mercator projection
+  mybbox <- suppressWarnings(
+    spTransform(SpatialPoints(t(data@bbox),
+                              proj4string = data@proj4string),
+                CRS("+init=epsg:3857")))
+  mybbox.wgs <- suppressWarnings(
+    spTransform(SpatialPoints(t(data@bbox),
+                              proj4string = data@proj4string),
+                wgs84))
+  e <- raster::extent(mybbox.wgs)
+  r <- raster(raster::extent(mybbox), ncol = data@grid@cells.dim[1]*.9,
+              nrow = data@grid@cells.dim[2]*.9, crs = CRS(proj4string(mybbox)))
+
+  # convert to google earth mercator projection
+  data <- suppressWarnings(
+    as.data.frame(spTransform(data, CRS("+init=epsg:3857"))))
+  # bring z-values within plotting range
+  index <- which(data$z < zlim[1])
+  if (length(index) > 0) {
+    data[index,]$z <- zlim[1]
+  }
+  index <- which(data$z > zlim[2])
+  if (length(index) > 0) {
+    data[index,]$z <- zlim[2]
+  }
+
+  # rasterize
+  r <- raster::rasterize(data[,2:3], r, data[,1])
+  # assign colors
+  if (param %in% c("VRADH", "VRADV", "VRAD")) {
+    cols <- add_color_transparency(
+      colorRampPalette(colors = c("blue", "white", "red"),
+                       alpha = TRUE)(n.color), alpha = alpha)
+  } else {
+    cols <- add_color_transparency(
+      colorRampPalette(colors = c("lightblue", "darkblue", "green",
+                                  "yellow", "red", "magenta"),
+                       alpha = TRUE)(n.color), alpha = alpha)
+  }
+
+  color_scale <- function(value, lim){
+    output <- rep(0, length(value))
+    output <- round((value - lim[1])/(lim[2] - lim[1]) * n.color)
+    output[output > n.color] <- n.color
+    output[output < 1] < -1
     return(cols[output])
   }
-  r@data@values=color_scale(r@data@values,zlim)
-  # these declarations prevent generation of NOTE "no visible binding for global variable" during package Check
-  lon=lat=y=z=NA
+
+  r@data@values <- color_scale(r@data@values, zlim)
+  # these declarations prevent generation of NOTE "no visible binding for
+  # global variable" during package Check
+  lon = lat = y = z = NA
   # symbols for the radar position
-  # dummy is a hack to be able to include the ggplot2 color scale, radarpoint is the actual plotting of radar positions.
-  dummy=geom_point(aes(x = lon, y = lat, colour=z),size=0,data=data.frame(lon=x$geo$lon,lat=x$geo$lat,z=0))
-  radarpoint=geom_point(aes(x = lon, y = lat),colour=radar.color,size=radar.size,data=data.frame(lon=x$geo$lon,lat=x$geo$lat))
+  # dummy is a hack to be able to include the ggplot2 color scale,
+  # radarpoint is the actual plotting of radar positions.
+  dummy <- geom_point(aes(x = lon, y = lat, colour = z), size = 0,
+                      data = data.frame(lon = x$geo$lon,
+                                        lat = x$geo$lat,
+                                        z = 0))
+  radarpoint <- geom_point(aes(x = lon, y = lat), colour = radar.color,
+                           size = radar.size,
+                           data = data.frame(lon = x$geo$lon, lat = x$geo$lat))
   # colorscale
-  colorscale=color_scale(param,zlim)
+  colorscale <- color_scale(param, zlim)
   # bounding box
-  bboxlatlon=attributes(map)$geo$bbox
+  bboxlatlon <- attributes(map)$geo$bbox
   # remove dimnames, otherwise ggmap will give a warning message below:
-  dimnames(bboxlatlon)=NULL
-  if(missing(xlim)) xlim=bboxlatlon[1,]
-  if(missing(ylim)) ylim=bboxlatlon[2,]
+  dimnames(bboxlatlon) <- NULL
+  if (missing(xlim)) xlim <- bboxlatlon[1,]
+  if (missing(ylim)) ylim <- bboxlatlon[2,]
   # plot the data on the map
-  mymap = suppressMessages(ggmap(map)+inset_raster(raster::as.matrix(r),e@xmin,e@xmax,e@ymin,e@ymax) + dummy + colorscale + radarpoint + scale_x_continuous(limits = xlim, expand = c(0, 0)) + scale_y_continuous(limits = ylim, expand = c(0, 0)))
+  mymap <- suppressMessages(
+    ggmap(map) +
+      inset_raster(raster::as.matrix(r), e@xmin, e@xmax, e@ymin, e@ymax) +
+      dummy + colorscale +
+      radarpoint +
+      scale_x_continuous(limits = xlim, expand = c(0, 0)) +
+      scale_y_continuous(limits = ylim, expand = c(0, 0)))
   suppressWarnings(mymap)
 }
 
 
 get_zlim <- function(param) {
-  if (param %in% c("DBZH","DBZV","DBZ")) return(c(-20,30))
-  if (param %in% c("VRADH","VRADV","VRAD")) return(c(-20,20))
-  if (param == "RHOHV") return(c(0.4,1))
-  if (param == "ZDR") return(c(-5,8))
-  if (param == "PHIDP") return(c(-200,200))
+  if (param %in% c("DBZH","DBZV","DBZ")) return(c(-20, 30))
+  if (param %in% c("VRADH","VRADV","VRAD")) return(c(-20, 20))
+  if (param == "RHOHV") return(c(0.4, 1))
+  if (param == "ZDR") return(c(-5, 8))
+  if (param == "PHIDP") return(c(-200, 200))
 }
