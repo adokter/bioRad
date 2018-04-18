d5fac2c3308887d05cc231ba99be10c5c5385d4a
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-18 22:23:49 +0200

    Update code formatting plot.vp

--- R/plot.vp.R
+++ R/plot.vp.R
@@ -25,34 +25,38 @@
 #' data(VP)
 #' plot(VP)
 #' plot(VP, line.col = 'blue')
-plot.vp=function(x, quantity="dens", xlab=expression("volume density [#/km"^3*"]"),ylab="height [km]",line.col='red',line.lwd=1,...){
-  stopifnot(inherits(x,"vp"))
-  if(!(quantity %in% names(x$data))) stop(paste("unknown quantity '",quantity,"'",sep=""))
+plot.vp <- function(x, quantity="dens",
+                    xlab = expression("volume density [#/km"^3*"]"),
+                    ylab = "height [km]", line.col='red', line.lwd=1, ...){
+  stopifnot(inherits(x, "vp"))
+  if (!(quantity %in% names(x$data))) {
+    stop(paste("unknown quantity '", quantity, "'", sep = ""))
+  }
   # set up the plot labels
-  if(missing(xlab)){
-    if(quantity=="u") xlab="W->E ground speed component U [m/s]"
-    if(quantity=="v") xlab="N->S ground speed component V [m/s]"
-    if(quantity=="w") xlab="vertical speed W [m/s]"
-    if(quantity=="ff") xlab="ground speed [m/s]"
-    if(quantity=="dd") xlab="ground speed direction [deg]"
-    if(quantity=="sd_vvp") xlab="VVP-retrieved radial velocity standard deviation [m/s]"
-    if(quantity=="head_bl") xlab="heading baseline [unitless]"
-    if(quantity=="head_ff") xlab="heading amplitude [unitless]"
-    if(quantity=="head_dd") xlab="heading direction [deg]"
-    if(quantity=="head_sd") xlab="heading standard deviation [unitless]"
-    if(quantity=="dbz") xlab=expression("reflectivity factor [dBZ"[e] * "]")
-    if(quantity=="dens") xlab=expression("volume density [#/km"^3*"]")
-    if(quantity=="eta") xlab=expression("reflectivity "*eta*" [cm"^2*"/km"^3*"]")
-    if(quantity=="DBZH") xlab=expression("total reflectivity factor [dBZ"[e] * "]")
-    if(quantity=="gap") xlab="Angular data gap detected [logical]"
-    if(quantity=="n") xlab="# range gates in VVP velocity analysis"
-    if(quantity=="n_all") xlab="# range gates in sd_vvp estimate"
-    if(quantity=="n_dbz") xlab="# range gates in density estimates"
-    if(quantity=="n_dbz_all") xlab="# range gates in DBZH estimate"
+  if (missing(xlab)) {
+    if (quantity == "u") xlab = "W->E ground speed component U [m/s]"
+    if (quantity == "v") xlab = "N->S ground speed component V [m/s]"
+    if (quantity == "w") xlab = "vertical speed W [m/s]"
+    if (quantity == "ff") xlab = "ground speed [m/s]"
+    if (quantity == "dd") xlab = "ground speed direction [deg]"
+    if (quantity == "sd_vvp") xlab = "VVP-retrieved radial velocity standard deviation [m/s]"
+    if (quantity == "head_bl") xlab = "heading baseline [unitless]"
+    if (quantity == "head_ff") xlab = "heading amplitude [unitless]"
+    if (quantity == "head_dd") xlab = "heading direction [deg]"
+    if (quantity == "head_sd") xlab = "heading standard deviation [unitless]"
+    if (quantity == "dbz") xlab = expression("reflectivity factor [dBZ"[e] * "]")
+    if (quantity == "dens") xlab = expression("volume density [#/km"^3*"]")
+    if (quantity == "eta") xlab = expression("reflectivity "*eta*" [cm"^2*"/km"^3*"]")
+    if (quantity == "DBZH") xlab = expression("total reflectivity factor [dBZ"[e] * "]")
+    if (quantity == "gap") xlab = "Angular data gap detected [logical]"
+    if (quantity == "n") xlab = "# range gates in VVP velocity analysis"
+    if (quantity == "n_all") xlab = "# range gates in sd_vvp estimate"
+    if (quantity == "n_dbz") xlab = "# range gates in density estimates"
+    if (quantity == "n_dbz_all") xlab = "# range gates in DBZH estimate"
   }
 
   # extract the data from the time series object
-  pdat=get_quantity(x,quantity)
-  plot(pdat,x$data$HGHT/1000,xlab=xlab,ylab=ylab,...)
-  points(pdat,x$data$HGHT/1000, col=line.col,lwd=line.lwd,type="l")
+  pdat <- get_quantity(x, quantity)
+  plot(pdat, x$data$HGHT/1000, xlab = xlab, ylab = ylab, ...)
+  points(pdat, x$data$HGHT/1000, col = line.col, lwd = line.lwd, type = "l")
 }
