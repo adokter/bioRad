69f268ba89aea6ef2896825cf1e2b67718e0203e
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-18 15:28:49 +0200

    Improve code formatting

--- R/get_angles.R
+++ R/get_angles.R
@@ -1,9 +1,13 @@
 #' Elevation angle of scan(s)
 #'
-#' Gives the elevation angle of a scan, or the elevation angles within a polar volume
-#' @param x a \code{pvol} or \code{scan} object
-#' @export
+#' Gives the elevation angle of a scan, or the elevation angles within a
+#' polar volume
+#'
+#' @param x A \code{pvol} or \code{scan} object.
+#'
 #' @return elevation in degrees
+#'
+#' @export
 #' @examples
 #' # load a polar volume
 #' pvol <- system.file("extdata", "volume.h5", package="bioRad")
@@ -14,18 +18,20 @@
 #' scan=vol$scans[[1]]
 #' # elevation angle of the scan:
 #' elangle(scan)
-elangle <- function (x) UseMethod("elangle", x)
+elangle <- function(x) {
+  UseMethod("elangle", x)
+}
 
-#' @describeIn elangle elevation angle of a scan
+#' @describeIn elangle Elevation angle of a scan.
 #' @export
-elangle.scan = function(x){
-  stopifnot(inherits(x,"scan"))
+elangle.scan <- function(x) {
+  stopifnot(inherits(x, "scan"))
   x$attributes$where$elangle
 }
 
-#' @describeIn elangle elevation angles of all scans in a polar volume
+#' @describeIn elangle Elevation angles of all scans in a polar volume.
 #' @export
-elangle.pvol = function(x){
-  stopifnot(inherits(x,"pvol"))
-  sapply(x$scans,elangle.scan)
+elangle.pvol <- function(x) {
+  stopifnot(inherits(x, "pvol"))
+  sapply(x$scans, elangle.scan)
 }
