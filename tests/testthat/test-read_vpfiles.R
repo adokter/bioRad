987c284d15bcd64255fed6e12fdff4eff8aecdd7
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-19 17:29:39 +0200

    Change filenames within read_vpfiles tests

--- tests/testthat/test-read_vpfiles.R
+++ tests/testthat/test-read_vpfiles.R
@@ -1,19 +1,20 @@
 context("test-read_vpfiles.R")
 
 test_that("Read a vertical profile", {
-  prof <- system.file("extdata", "profile.h5", package = "bioRad")
-  expect_s3_class(readvp(prof), "vp")
-  expect_s3_class(read_vpfiles(prof), "vp")
+  vpfile <- system.file("extdata", "profile.h5", package = "bioRad")
+  expect_s3_class(readvp(vpfile), "vp")
+  expect_s3_class(read_vpfiles(vpfile), "vp")
 })
 
-test_that("Read a list of vertical profiles", {
-  prof <- system.file("extdata", "profile.h5", package = "bioRad")
+test_that("Read multiple vertical profiles", {
+  vpfile <- system.file("extdata", "profile.h5", package = "bioRad")
   # old function -> to vplist class; readvp.list was the function in the earlier
   # releases (not the S3 based method dispatching)
-  expect_s3_class(readvp.list(c(prof, prof)), "vplist")
-  expect_is(readvp.list(c(prof, prof)), "list")
+  expect_s3_class(readvp.list(c(vpfile, vpfile)), "vplist")
+  expect_is(readvp.list(c(vpfile, vpfile)), "list")
   # new function -> just a list of vp
-  expect_is(read_vpfiles(c(prof, prof)), "list")
-  expect_s3_class(read_vpfiles(c(prof, prof))[[1]], "vp")
-  expect_s3_class(read_vpfiles(c(prof, prof))[[2]], "vp")
+  expect_is(read_vpfiles(c(vpfile, vpfile)), "list")
+  #expect_error(is(read_vpfiles(c(vpfile, vpfile)), "vplist")) # to add at end of refactoring
+  expect_s3_class(read_vpfiles(c(vpfile, vpfile))[[1]], "vp")
+  expect_s3_class(read_vpfiles(c(vpfile, vpfile))[[2]], "vp")
 })
