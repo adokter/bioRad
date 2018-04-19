aaf4bbbfb57f79d0032d662b22677cb4ff1b43ab
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-19 10:25:01 +0200

    Update code formatting vpts

--- R/plot.vpts.R
+++ R/plot.vpts.R
@@ -1,157 +1,208 @@
 #' Plot a time series of vertical profiles
 #'
 #' Plot a time series of vertical profiles  of class \code{vpts}.
-#' @param x a vp class object inheriting from class \code{vpts}
-#' @param xlab a title for the x-axis
-#' @param ylab a title for the y-axis
-#' @param quantity character string with the quantity to plot, one of '\code{dens}','\code{eta}','\code{dbz}','\code{DBZH}' for density, reflectivity, reflectivity factor and total reflectivity factor, respectively.
-#' @param log logical. Whether to display \code{quantity} data on a logarithmic scale
-#' @param barbs logical. Whether to overlay speed barbs
-#' @param barbs.h integer. Number of barbs to plot in altitudinal dimension
-#' @param barbs.t integer. Number of barbs to plot in temporal dimension
-#' @param barbs.dens numeric. Lower threshold in aerial density of individuals for plotting speed barbs in individuals/km^3
-#' @param zlim optional numerical atomic vector of length 2, specifying the range of \code{quantity} values to plot
-#' @param legend.ticks numeric atomic vector specifying the ticks on the color bar
-#' @param main a title for the plot
-#' @param ... Additional arguments to be passed to the low level \link[graphics]{image} plotting function
-#' @export
-#' @method plot vpts
-#' @details
-#' Profile can be visualised in three related quantities, as specified by argument \code{quantity}:
-#' \describe{
-#'  \item{"\code{dens}"}{the aerial density of individuals. This quantity is dependent on the assumed radar cross section (RCS) in the \code{x$attributes$how$rcs_bird} attribute}
-#'  \item{"\code{eta}"}{reflectivity. This quantity is independent of the value of the \code{rcs_bird} attribute}
-#'  \item{"\code{dbz}"}{reflectivity factor. This quantity is independent of the value
-#'  of the \code{rcs_bird} attribute, and corresponds to the dBZ scale commonly used in weather radar meteorology.
-#'  Bioscatter by birds tends to occur at much higher reflectivity factors at S-band than at C-band}
-#'  \item{"\code{DBZH}"}{total reflectivity factor. This quantity equals the reflectivity factor of all scatterers (biological and meteorological scattering cambined)}
-#' }
 #'
-#' In the speed barbs, each half flag represents 2.5 m/s, each full flag 5 m/s, each pennant (triangle) 25 m/s
+#' @param x A vp class object inheriting from class \code{vpts}.
+#' @param xlab A title for the x-axis.
+#' @param ylab A title for the y-axis.
+#' @param quantity Character string with the quantity to plot,
+#' one of '\code{dens}','\code{eta}','\code{dbz}','\code{DBZH}' for density,
+#' reflectivity, reflectivity factor and total reflectivity factor,
+#' respectively.
+#' @param log Logical, whether to display \code{quantity} data on a
+#' logarithmic scale.
+#' @param barbs Logical, whether to overlay speed barbs.
+#' @param barbs.h Integer, number of barbs to plot in altitudinal dimension.
+#' @param barbs.t Integer, number of barbs to plot in temporal dimension.
+#' @param barbs.dens Numeric, lower threshold in aerial density of individuals
+#' for plotting speed barbs in individuals/km^3.
+#' @param zlim Optional numerical atomic vector of length 2, specifying the
+#' range of \code{quantity} values to plot.
+#' @param legend.ticks Numeric atomic vector specifying the ticks on the
+#' color bar.
+#' @param main A title for the plot.
+#' @param ... Additional arguments to be passed to the low level
+#' \link[graphics]{image} plotting function.
+#'
+#' @method plot vpts
+#'
+#' @export
+#'
+#' @details
+#' Profile can be visualised in three related quantities, as specified
+#' by argument \code{quantity}:
+#' \describe{
+#'  \item{"\code{dens}"}{the aerial density of individuals. This quantity is
+#'    dependent on the assumed radar cross section (RCS) in the
+#'    \code{x$attributes$how$rcs_bird} attribute}
+#'  \item{"\code{eta}"}{reflectivity. This quantity is independent of the
+#'    value of the \code{rcs_bird} attribute}
+#'  \item{"\code{dbz}"}{reflectivity factor. This quantity is independent
+#'    of the value of the \code{rcs_bird} attribute, and corresponds to the
+#'    dBZ scale commonly used in weather radar meteorology. Bioscatter by birds
+#'    tends to occur at much higher reflectivity factors at S-band
+#'    than at C-band}
+#'  \item{"\code{DBZH}"}{total reflectivity factor. This quantity equals the
+#'    reflectivity factor of all scatterers (biological and meteorological
+#'    scattering cambined)}
+#' }
+#' In the speed barbs, each half flag represents 2.5 m/s, each full flag 5 m/s,
+#' each pennant (triangle) 25 m/s
 #'
 #' @examples
 #' # locate example file:
-#' VPtable <- system.file("extdata", "VPtable.txt", package="bioRad")
+#' VPtable <- system.file("extdata", "VPtable.txt", package = "bioRad")
 #' # load and regularize time series of vertical profiles:
-#' ts=regularize(readvp.table(VPtable,radar="KBGM", wavelength='S'))
-#' # plot density of individuals for the first 500 time steps, in the altitude layer 0-3000 m.
-#' plot(ts[1:500], ylim=c(0,3000))
-#' # plot total reflectivity factor (rain,birds,insects together):
-#' plot(ts[1:500], ylim=c(0,3000), quantity="DBZH")
-plot.vpts = function(x, xlab="time",ylab="height [m]",quantity="dens",log=T, barbs=T, barbs.h=10, barbs.t=20, barbs.dens=5, zlim, legend.ticks, main, ...){
-  stopifnot(inherits(x,"vpts"))
-  stopifnot(quantity %in% c("dens","eta","dbz","DBZH"))
+#' ts <- regularize(readvp.table(VPtable, radar = "KBGM", wavelength = 'S'))
+#' # plot density of individuals for the first 500 time steps, in the altitude
+#' # layer 0-3000 m.
+#' plot(ts[1:500], ylim = c(0, 3000))
+#' # plot total reflectivity factor (rain, birds, insects together):
+#' plot(ts[1:500], ylim = c(0, 3000), quantity = "DBZH")
+plot.vpts <- function(x, xlab = "time", ylab = "height [m]", quantity = "dens",
+                      log = TRUE, barbs = TRUE, barbs.h = 10, barbs.t = 20,
+                      barbs.dens = 5, zlim, legend.ticks, main, ...) {
+  stopifnot(inherits(x, "vpts"))
+  stopifnot(quantity %in% c("dens", "eta", "dbz", "DBZH"))
   args <- list(...)
-  if(!x$regular) warning("Irregular time-series: missing profiles will not be visible. Use 'regularize' to make time series regular.")
+  if (!x$regular) {
+    warning("Irregular time-series: missing profiles will not be visible.",
+            "Use 'regularize' to make time series regular.")
+  }
 
   # prepare zlim, ticks and legendticks
-  if(missing(zlim)) {
-    if(quantity=="dens" & log){
-      ticks=legendticks=c(1,2,5,10,25,50,100,200,500,1000)
-      zlim=c(.5,1000)
+  if (missing(zlim)) {
+    if (quantity == "dens" & log) {
+      ticks <- legendticks <- c(1, 2, 5, 10, 25, 50, 100, 200, 500, 1000)
+      zlim <- c(.5, 1000)
     }
-    if(quantity=="dens" & !log){
-      ticks=legendticks=seq(0,500,20)
-      zlim=c(0,500)
+    if (quantity == "dens" & !log) {
+      ticks <- legendticks <- seq(0, 500, 20)
+      zlim <- c(0, 500)
     }
-    if(quantity=="eta" & log){
-      ticks=legendticks=10*c(1,2,5,10,25,50,100,200,500,1000)
-      zlim=c(5,10000)
+    if (quantity == "eta" & log) {
+      ticks <- legendticks <- 10*c(1, 2, 5, 10, 25, 50, 100, 200, 500, 1000)
+      zlim <- c(5,10000)
     }
-    if(quantity=="eta" & !log){
-      ticks=legendticks=seq(0,5000,500)
-      zlim=c(0,5000)
+    if (quantity == "eta" & !log) {
+      ticks <- legendticks <- seq(0, 5000, 500)
+      zlim <- c(0,5000)
     }
-    if(quantity=="dbz" || quantity=="DBZH"){
-      if(x$attributes$how$wavelength>10){
-        ticks=legendticks=seq(-5,30,5)
-        zlim=c(-5,30)
+    if (quantity == "dbz" || quantity == "DBZH") {
+      if (x$attributes$how$wavelength > 10) {
+        ticks <- legendticks <- seq(-5, 30, 5)
+        zlim <- c(-5, 30)
       }
       else{
-        ticks=legendticks=seq(-20,10,5)
-        zlim=c(-20,10)
+        ticks <- legendticks <- seq(-20, 10, 5)
+        zlim <- c(-20, 10)
       }
     }
+  } else {
+    ticks <- legendticks <- seq(zlim[1], zlim[2], length.out = 10)
   }
-  else{
-    ticks=legendticks=seq(zlim[1],zlim[2],length.out=10)
+  if (!missing(legend.ticks)) {
+    ticks <- legendticks <- legend.ticks
   }
-  if(!missing(legend.ticks)) ticks=legendticks=legend.ticks
 
   # set up the plot labels
-  if(missing(main)){
-    if(quantity=="dens") main=expression("volume density [#/km"^3*"]")
-    if(quantity=="eta") main=expression("reflectivity "*eta*" [cm"^2*"/km"^3*"]")
-    if(quantity=="dbz") main=expression("reflectivity factor [dBZ"[e] * "]")
-    if(quantity=="DBZH") main=expression("total reflectivity factor [dBZ"[e] * "]")
+  if (missing(main)) {
+    if (quantity == "dens") main = expression("volume density [#/km"^3*"]")
+    if (quantity == "eta") main = expression("reflectivity "*eta*" [cm"^2*"/km"^3*"]")
+    if (quantity == "dbz") main = expression("reflectivity factor [dBZ"[e] * "]")
+    if (quantity == "DBZH") main = expression("total reflectivity factor [dBZ"[e] * "]")
   }
 
   # extract the data from the time series object
-  if(quantity=="dens") plotdata=t(get_quantity(x,quantity))
-  if(quantity=="eta") plotdata=t(get_quantity(x,quantity))
-  if(quantity=="dbz"){
-    if(log){
-      if(!missing(log)) warning("reflectivity factor 'dbz' is already logarithmic, ignoring 'log' argument...")
-      log=F
+  if (quantity == "dens") plotdata = t(get_quantity(x,quantity))
+  if (quantity == "eta") plotdata = t(get_quantity(x,quantity))
+  if (quantity == "dbz") {
+    if (log) {
+      if (!missing(log)) {
+        warning("Reflectivity factor 'dbz' is already logarithmic,",
+                "ignoring 'log' argument...")
+      }
+      log <- FALSE
     }
-    plotdata=t(get_quantity(x,quantity))
+    plotdata <- t(get_quantity(x, quantity))
   }
-  if(quantity=="DBZH"){
-    if(log){
-      if(!missing(log)) warning("total reflectivity factor 'DBZH' is already logarithmic, ignoring 'log' argument...")
-      log=F
+  if (quantity == "DBZH") {
+    if (log) {
+      if (!missing(log)) {
+        warning("Total reflectivity factor 'DBZH' is already logarithmic,",
+                "ignoring 'log' argument...")
+      }
+      log <- FALSE
     }
-    plotdata=t(get_quantity(x,quantity))
+    plotdata <- t(get_quantity(x,quantity))
   }
 
   # do log-transformations:
-  if(log){
-    plotdata=log(plotdata)
-    legendticks=log(ticks)
-    zlim=log(zlim)
+  if (log) {
+    plotdata <- log(plotdata)
+    legendticks <- log(ticks)
+    zlim <- log(zlim)
   }
-  breaks=c(zlim[1]-(zlim[2]-zlim[1])/1000,seq(zlim[1],zlim[2],length.out=256))
+  breaks <- c(zlim[1] - (zlim[2] - zlim[1])/1000,
+              seq(zlim[1], zlim[2], length.out = 256))
   # move points out of zlim range into valid color range
-  plotdata[plotdata<(breaks[2]+breaks[3])/2]=(breaks[2]+breaks[3])/2
-  plotdata[plotdata>zlim[2]]=breaks[length(breaks)]
-  plotdata[is.na2(plotdata)]=(breaks[1]+breaks[2])/2
-  #
-  zlim[1]=breaks[1]
-  axis.args=list(at=legendticks,labels=ticks)
+  plotdata[plotdata < (breaks[2] + breaks[3])/2] <- (breaks[2] + breaks[3])/2
+  plotdata[plotdata > zlim[2]] <- breaks[length(breaks)]
+  plotdata[is.na2(plotdata)] <- (breaks[1] + breaks[2])/2
+
+  zlim[1] <- breaks[1]
+  axis.args <- list(at = legendticks, labels = ticks)
   # FIXME: want to change this to
   # plotdata[is.nan(plotdata)]=(breaks[2]+breaks[3])/2
   # when calculate_vp stdout also differentiates between NA and NaN:
-  plotdata[is.na(plotdata)]=(breaks[2]+breaks[3])/2
+  plotdata[is.na(plotdata)] <- (breaks[2] + breaks[3])/2
   # FIXME: want to change this to
   # plotdata[is.na2(plotdata)]=(breaks[1]+breaks[2])/2
   # when calculate_vp stdout also differentiates between NA and NaN:
-  plotdata[is.na(plotdata)]=(breaks[2]+breaks[3])/2
+  plotdata[is.na(plotdata)] <- (breaks[2] + breaks[3])/2
 
   #plot the image
-  image.plot(x$dates,x$heights,plotdata,col=plot_colors,xlab=xlab,ylab=ylab,axis.args=axis.args,breaks=breaks,zlim=zlim,main=main,...)
+  image.plot(x$dates, x$heights, plotdata, col = plot_colors, xlab = xlab,
+             ylab = ylab, axis.args = axis.args, breaks = breaks,
+             zlim = zlim, main = main, ...)
 
   # overlay speed barbs
-  if(barbs){
-    if("xlim" %in% names(args)) t.barbs=seq(min(args$xlim), max(args$xlim),length.out=barbs.t)
-    else t.barbs=seq(x$dates[1], tail(x$dates,1),length.out=barbs.t)
-    if("ylim" %in% names(args)) h.barbs=seq(min(args$ylim), max(args$ylim),length.out=barbs.h)
-    else h.barbs=seq(x$heights[1], tail(x$heights,1),length.out=barbs.h)
-    barbdata=expand.grid(date=t.barbs,height=h.barbs)
-    barbdata$indext=sapply(barbdata$date,function(y) which.min(abs(x$dates - y)))
-    barbdata$indexh=sapply(barbdata$height,function(y) which.min(abs(x$heights - y)))
-    barbdata$ff=mapply(function(xx,yy) x$data$ff[xx,yy],barbdata$indexh,barbdata$indext)
-    barbdata$dd=mapply(function(xx,yy) x$data$dd[xx,yy],barbdata$indexh,barbdata$indext)
-    barbdata$dens=mapply(function(xx,yy) x$data$dens[xx,yy],barbdata$indexh,barbdata$indext)
-    barbdata=barbdata[barbdata$dens>barbs.dens,]
-    plot_wind_barbs(barbdata$date,barbdata$height,180+barbdata$dd,2*barbdata$ff,cex=0.7)
+  if (barbs) {
+    if ("xlim" %in% names(args)) {
+      t.barbs <- seq(min(args$xlim), max(args$xlim), length.out = barbs.t)
+    } else {
+      t.barbs <- seq(x$dates[1], tail(x$dates, 1), length.out = barbs.t)
+    }
+    if ("ylim" %in% names(args)) {
+      h.barbs <- seq(min(args$ylim), max(args$ylim), length.out = barbs.h)
+    } else {
+      h.barbs <- seq(x$heights[1], tail(x$heights, 1), length.out = barbs.h)
+    }
+    barbdata <- expand.grid(date = t.barbs, height = h.barbs)
+    barbdata$indext <- sapply(barbdata$date,
+                              function(y) which.min(abs(x$dates - y)))
+    barbdata$indexh <- sapply(barbdata$height,
+                              function(y) which.min(abs(x$heights - y)))
+    barbdata$ff <- mapply(
+      function(xx,yy) x$data$ff[xx,yy], barbdata$indexh, barbdata$indext)
+    barbdata$dd <- mapply(
+      function(xx,yy) x$data$dd[xx,yy], barbdata$indexh, barbdata$indext)
+    barbdata$dens <- mapply(
+      function(xx,yy) x$data$dens[xx,yy], barbdata$indexh, barbdata$indext)
+    barbdata <- barbdata[barbdata$dens > barbs.dens,]
+    plot_wind_barbs(barbdata$date,barbdata$height, 180 + barbdata$dd,
+                    2*barbdata$ff, cex = 0.7)
   }
 }
 
-plot_wind_barbs = function(cx, cy, direction = 0, speed = NA, fill = rep(0, length(cx)), circle = FALSE, cex = 1, col = "black")
-{
+plot_wind_barbs <- function(cx, cy, direction = 0, speed = NA,
+                            fill = rep(0, length(cx)), circle = FALSE,
+                            cex = 1, col = "black") {
   ### press is actually height in upper air ###
-  ns = length(cx)
-  if (length(cy) != ns) stop("X AND Y COORDINATES SHOULD HAVE SAME LENGTH!")
+  ns <- length(cx)
+  if (length(cy) != ns) {
+    stop("X AND Y COORDINATES SHOULD HAVE SAME LENGTH!")
+  }
 
   msg = "ALL VARIABLES SHOULD HAVE SAME LENGTH AS COORDINATES, OR BE MISSING!!!"
   if (ns > 1) {
@@ -163,13 +214,13 @@ plot_wind_barbs = function(cx, cy, direction = 0, speed = NA, fill = rep(0, leng
     if (length(fill) > 1 & length(fill) != ns) stop(msg)
   }
 
-  tpar = par()
-  size = tpar$csi
-  scalex = (tpar$usr[2] - tpar$usr[1]) / tpar$pin[1]
-  scaley = (tpar$usr[4] - tpar$usr[3]) / tpar$pin[2]
-  scalex = (cex * (scalex * size)) / 5
-  scaley = (cex * (scaley * size)) / 5
-  for (i in 1 : ns) {
+  tpar <- par()
+  size <- tpar$csi
+  scalex <- (tpar$usr[2] - tpar$usr[1]) / tpar$pin[1]
+  scaley <- (tpar$usr[4] - tpar$usr[3]) / tpar$pin[2]
+  scalex <- (cex * (scalex * size)) / 5
+  scaley <- (cex * (scaley * size)) / 5
+  for (i in 1:ns) {
     x = cx[i]
     y = cy[i]
     if (is.na(is.na(x) || is.na(y))) next
@@ -189,7 +240,8 @@ plot_wind_barbs = function(cx, cy, direction = 0, speed = NA, fill = rep(0, leng
       }
       if (fill[i] > 0) {
         lim = c(51, 101, 151, 200)
-        polygon(c(x, X1[1 : lim[fill[i]]]), c(y, Y1[1 : lim[fill[i]]]), density = -1, col = col)
+        polygon(c(x, X1[1:lim[fill[i]]]), c(y, Y1[1:lim[fill[i]]]),
+                density = -1, col = col)
       }
       lines(RX + x, RY + y, col = col)
     } #end of circle
@@ -290,4 +342,6 @@ plot_wind_barbs = function(cx, cy, direction = 0, speed = NA, fill = rep(0, leng
 }
 
 # true when NA but not when NaN
-is.na2=function(x) is.na(x) & !is.nan(x)
+is.na2 <- function(x) {
+  is.na(x) & !is.nan(x)
+}
