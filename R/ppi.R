9a13f662caeacc65ca0e9ddcd3a38e9901e0d372
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-19 10:36:16 +0200

    Update code formatting ppi

--- R/ppi.R
+++ R/ppi.R
@@ -2,43 +2,56 @@
 #'
 #' Extract by index from a ppi
 #'
-#' @param x an object of class 'param' or 'scan'
-#' @param i indices specifying elements to extract
+#' @param x An object of class \code{param} or \code{scan}.
+#' @param i Indices specifying elements to extract.
 #'
 #' @export
-`[.ppi` <- function(x,i) {
-  stopifnot(inherits(x,"ppi"))
-  myppi=list(radar=x$radar,datetime=x$datetime,data=x$data[i],geo=x$geo)
-  class(myppi)="ppi"
+`[.ppi` <- function(x, i) {
+  stopifnot(inherits(x, "ppi"))
+  myppi <- list(radar = x$radar, datetime = x$datetime,
+                data = x$data[i], geo = x$geo)
+  class(myppi) <- "ppi"
   return(myppi)
 }
 
-#' print method for ppi
+#' Print method for ppi
 #'
-#' @param x An object of class \code{ppi}
+#' @param x An object of class \code{ppi}.
+#'
 #' @keywords internal
+#'
 #' @export
-print.ppi=function(x,digits = max(3L, getOption("digits") - 3L), ...){
+print.ppi <- function(x, digits = max(3L, getOption("digits") - 3L), ...){
   stopifnot(inherits(x, "ppi"))
   cat("               Plan position indicator (class ppi)\n\n")
-  cat("  quantities: ",names(x$data),"\n")
-  cat("        dims: ",x$data@grid@cells.dim[1],"x",x$data@grid@cells.dim[2],"pixels\n\n")
+  cat("  quantities: ", names(x$data), "\n")
+  cat("        dims: ", x$data@grid@cells.dim[1], "x",
+      x$data@grid@cells.dim[2], "pixels\n\n")
 }
 
 #' Class 'ppi': plan position indicator
-#' @param object object of class 'ppi'
-#' @param x object of class 'ppi'
-#' @param ... additional arguments affecting the summary produced.
-#' @export
+#'
+#' @param object Object of class \code{ppi}.
+#' @param x Object of class \code{ppi}.
+#' @param ... Additional arguments affecting the summary produced.
+#'
 #' @method summary ppi
+#'
+#' @export
+#'
 #' @details
 #' A PPI of class 'ppi' is a list containing:
 #' \describe{
-#'  \item{\code{data}}{an object of class \link[sp]{SpatialGridDataFrame} containing the georeferenced data. Commonly available parameters are:
+#'  \item{\code{data}}{an object of class \link[sp]{SpatialGridDataFrame}
+#'    containing the georeferenced data. Commonly available parameters are:
 #'     \describe{
 #'      \item{"\code{DBZH}", "\code{DBZ}"}{(Logged) reflectivity factor [dBZ]}
-#'      \item{"\code{VRADH}", "\code{VRAD}"}{Radial velocity [m/s]. Radial velocities towards the radar are negative, while radial velocities away from the radar are positive}
-#'      \item{"\code{RHOHV}"}{Correlation coefficient [unitless]. Correlation between vertically polarized and horizontally polarized reflectivity factor}
+#'      \item{"\code{VRADH}", "\code{VRAD}"}{Radial velocity [m/s]. Radial
+#'        velocities towards the radar are negative, while radial velocities
+#'        away from the radar are positive}
+#'      \item{"\code{RHOHV}"}{Correlation coefficient [unitless]. Correlation
+#'        between vertically polarized and horizontally polarized reflectivity
+#'        factor}
 #'      \item{"\code{PHIDP}"}{Differential phase [degrees]}
 #'      \item{"\code{ZDR}"}{(Logged) differential reflectivity [dB]}
 #'        }
@@ -47,26 +60,35 @@ print.ppi=function(x,digits = max(3L, getOption("digits") - 3L), ...){
 #'     \describe{
 #'      \item{\code{lat}}{latitude of the radar [decimal degrees]}
 #'      \item{\code{lon}}{longitude of the radar [decimal degrees]}
-#'      \item{\code{height}}{height of the radar antenna [metres above sea level]}
+#'      \item{\code{height}}{height of the radar
+#'        antenna [metres above sea level]}
 #'      \item{\code{elangle}}{radar beam elevation [degrees]}
 #'      \item{\code{rscale}}{range bin size [m]}
 #'      \item{\code{ascale}}{azimuth bin size [deg]}
 #'     }
-#'     The \code{geo} element of a 'scan' object is a copy of the \code{geo} element of its parent scan or scan parameter.
+#'     The \code{geo} element of a 'scan' object is a copy of the \code{geo}
+#'     element of its parent scan or scan parameter.
 #'   }
 #' }
-summary.ppi=function(object, ...) print.ppi(object)
+summary.ppi <- function(object, ...) {
+  print.ppi(object)
+}
 
 #' @rdname summary.ppi
+#'
 #' @export
-#' @return for \code{is.ppi}: \code{TRUE} if its argument is of class "\code{ppi}"
+#'
+#' @return For \code{is.ppi}: \code{TRUE} if its argument is of
+#' class \code{ppi}.
 is.ppi <- function(x) inherits(x, "ppi")
 
 #' @rdname summary.ppi
+#'
 #' @export
-#' @return for \code{dim.ppi}: dimensions of the ppi
+#'
+#' @return For \code{dim.ppi}: dimensions of the ppi.
 dim.ppi <- function(x) {
-  stopifnot(inherits(x,"ppi"))
-  c(dim(x$data)[2],x$data@grid@cells.dim)
+  stopifnot(inherits(x, "ppi"))
+  c(dim(x$data)[2], x$data@grid@cells.dim)
 }
 
