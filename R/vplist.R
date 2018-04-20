90e89a63e03e317568b313e15f9fff1e83551855
stijnvanhoey <stijnvanhoey@gmail.com>
2018-04-20 15:28:50 +0200

    Rewrite vp concat fucntion

--- R/vplist.R
+++ R/vplist.R
@@ -1,19 +1,23 @@
-#' Concatenate vertical profiles (\code{vp}) into a list of vertical profiles (\code{vplist})
+#' Concatenate vertical profiles (\code{vp}) into a list of vertical profiles
 #'
 #' @param ... objects of class \code{vp}
+#'
+#' @return an object of class \code{list}
+#'
 #' @export
-#' @return an object of class \code{vplist}, see \link{readvp.list} for details
-c.vp = function(...){
-  vps=list(...)
-  vptest=sapply(vps,function(x) is(x,"vp"))
-  if(FALSE %in% vptest) {
-    warning("non-vp objects found, returning a standard list...")
+c.vp <- function(...) {
+  vps <- list(...)
+  vptest <- sapply(vps, function(x) is(x, "vp"))
+  if (FALSE %in% vptest) {
+    warning("Non-vp objects found!")
     return(vps)
   }
   # extract radar identifiers
-  radars=unique(sapply(vps,'[[',"radar"))
-  if(length(radars)>1) warning("Vertical profiles are not from a single radar")
-  output=vps
-  class(output)=c("vplist","list")
+  radars <- unique(sapply(vps, '[[', "radar"))
+  if (length(radars) > 1) {
+    warning("Vertical profiles are not from a single radar!")
+  }
+  output <- vps
+  class(output) <- c("list")
   output
 }
