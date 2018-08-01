8ce1507e2fe3ecd10db7ed8ec8ba0a58a38e2db2
stijnvanhoey <stijnvanhoey@gmail.com>
2018-08-01 22:32:38 +0200

    update function arguments plot.ppi

--- R/plot.ppi.R
+++ R/plot.ppi.R
@@ -4,7 +4,7 @@
 #' using \link[ggplot2]{ggplot}
 #'
 #' @param x An object of class \code{ppi}.
-#' @param param The scan parameter to plot, see details below.
+#' @param quantity The scan quantity to plot, see details below.
 #' @param xlim Range of x values to plot.
 #' @param ylim Range of y values to plot.
 #' @param ratio Aspect ratio between x and y scale.
@@ -16,8 +16,8 @@
 #' @export
 #'
 #' @details
-#' Available scan parameters for plotting can by printed to screen
-#' by \code{summary(x)}. Commonly available parameters are:
+#' Available scan quantities for plotting can by printed to screen
+#' by \code{summary(x)}. Commonly available quantities are:
 #' \describe{
 #'  \item{"\code{DBZH}", "\code{DBZ}"}{(Logged) reflectivity factor [dBZ]}
 #'  \item{"\code{VRADH}", "\code{VRAD}"}{Radial velocity [m/s]. Radial
@@ -28,43 +28,51 @@
 #'  \item{"\code{PHIDP}"}{Differential phase [degrees]}
 #'  \item{"\code{ZDR}"}{(Logged) differential reflectivity [dB]}
 #' }
-#' The scan parameters are named according to the OPERA data information
+#' The scan quantities are named according to the OPERA data information
 #' model (ODIM), see Table 16 in the
 #' \href{https://github.com/adokter/vol2bird/blob/master/doc/OPERA2014_O4_ODIM_H5-v2.2.pdf}{ODIM specification}.
 #'
 #' @examples
 #' # load an example scan:
 #' data(example_scan)
-#' # print to screen the available scan parameters:
+#' # print to screen the available scan quantities:
 #' summary(example_scan)
 #' # make ppi for the scan
 #' ppi <- project_as_ppi(example_scan)
-#' # plot the first scan parameter, which in this case is "VRADH":
+#' # plot the first scan quantity, which in this case is "VRADH":
 #' plot(ppi)
 #' # plot the reflectivity quantity:
-#' plot(ppi, param = "DBZH")
+#' plot(ppi, quantity = "DBZH")
 #' # change the range of reflectivities to plot to -30 to 50 dBZ:
-#' plot(ppi, param = "DBZH",zlim = c(-30, 50))
-plot.ppi <- function(x, param, xlim, ylim, zlim = c(-20, 20), ratio = 1, ...) {
+#' plot(ppi, quantity = "DBZH",zlim = c(-30, 50))
+plot.ppi <- function(x, quantity, param, xlim, ylim, zlim = c(-20, 20),
+                     ratio = 1, ...) {
   stopifnot(inherits(x, "ppi"))
-  if (missing(param)) {
+
+  # deprecate function argument
+  if (!missing(param)) {
+    warning("argument param is deprecated; please use quantity instead.", call. = FALSE)
+    quantity <- param
+  }
+
+  if (missing(quantity)) {
     if ("DBZH" %in% names(x$data)) {
-      param <- "DBZH"
+      quantity <- "DBZH"
     } else {
-      param <- names(x$data)[1]
+      quantity <- names(x$data)[1]
     }
-  } else if (!is.character(param)) {
-    stop("'param' should be a character string with a valid scan",
+  } else if (!is.character(quantity)) {
+    stop("'quantity' should be a character string with a valid scan",
          "parameter name")
   }
 
   if (missing(zlim)) {
-    zlim <- get_zlim(param)
+    zlim <- get_zlim(quantity)
   }
-  colorscale <- color_scale_fill(param,zlim)
+  colorscale <- color_scale_fill(quantity, zlim)
   # extract the scan parameter
   y <- NULL #dummy asignment to suppress devtools check warning
-  data <- do.call(function(y) x$data[y], list(param))
+  data <- do.call(function(y) x$data[y], list(quantity))
   # convert to points
   data <- data.frame(rasterToPoints(raster(data)))
   # bring z-values within plotting range
@@ -85,7 +93,7 @@ plot.ppi <- function(x, param, xlim, ylim, zlim = c(-20, 20), ratio = 1, ...) {
   }
   bbox <- coord_fixed(xlim = xlim, ylim = ylim, ratio = ratio)
   ggplot(data = data,...) +
-    geom_raster(aes(x, y, fill = eval(parse(text = param)))) +
+    geom_raster(aes(x, y, fill = eval(parse(text = quantity)))) +
     colorscale +
     bbox
 }
