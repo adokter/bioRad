243129220741df659844a49962b76b2e7e66be6c
adokter <adriaandokter@gmail.com>
2018-09-22 17:14:06 -0400

    fix issue #132

---
+++ R/get_param.R
@@ -0,0 +1,18 @@
+#' Get a scan parameter (\code{param}) from a scan (\code{scan})
+#'
+#' @param x An object of class \code{scan}.
+#' @param param a scan parameter
+#'
+#' @return An object of class '\link[=summary.param]{param}'.
+#'
+#' @export
+#' @examples
+#' # we will extract a scan parameter from the example scan object:
+#' example_scan
+#' # extract the VRADH scan parameter
+#' myparam <- get_param(example_scan,"VRADH")
+get_param <- function(x, param) {
+  stopifnot(inherits(x, "scan"))
+  if(!(param %in% names(x$params))) stop(paste("scan parameter",param,"not found"))
+  x$params[[param]]
+}
