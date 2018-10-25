55a8811a88b0ca9306c927ec26a5bbcb291af0d7
adokter <adriaandokter@gmail.com>
2018-10-25 13:55:48 -0400

    corrected update time update_docker to UTC. Added docker_check to update_docker

--- R/read_vpts.R
+++ R/read_vpts.R
@@ -4,7 +4,7 @@
 #' by vol2bird (or the package function \code{calculate_vp}).
 #' @param radar A string containing a radar identifier.
 #' @param wavelength Radar wavelength in cm, or one of 'C' or 'S' for C-band
-#' and S-band radar, respectively, in which case C-band wavelenvth is assumed
+#' and S-band radar, respectively, in which case C-band wavelength is assumed
 #' to be 5.3 cm and S-band wavelength 10.6 cm
 #'
 #' @return An object inhereting from class \code{vpts}, see
