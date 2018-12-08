8fd350a1963cad8888f263d293f0041fbb63200b
Peter Desmet <peter.desmet.work@gmail.com>
2018-12-08 22:47:53 +0100

    Update descriptions of parameters

--- R/download_vpfiles.R
+++ R/download_vpfiles.R
@@ -4,14 +4,14 @@
 #' \href{http://enram.github.io/data-repository/}{ENRAM data repository}, where
 #' these are stored as monthly zips per radar.
 #'
-#' @param date_min character. ISO 8601 start date of data selection. Days will
+#' @param date_min character. YYYY-MM-DD start date of file selection. Days will
 #'   be ignored.
-#' @param date_max character. ISO 8601 end date of data selection. Days will be
-#'   ignored.
-#' @param radar character (vector). 5-letter country/radar code(s)
-#'   (e.g. "bejab") of radars to include in data selection.
-#' @param directory character. Path to local directory for downloading and
-#'   unzipping files.
+#' @param date_max character. YYYY-MM-DD end date of file selection. Days will
+#'   be ignored.
+#' @param radars character (vector). 5-letter country/radar code(s)
+#'   (e.g. "bejab") of radars to include in file selection.
+#' @param directory character. Path to local directory where files should be
+#'   downloaded and unzipped.
 #' @param overwrite logical. TRUE for redownloading and overwriting previously
 #'   downloaded files of the same names.
 #'
@@ -41,11 +41,11 @@ download_vpfiles <- function(date_min, date_max, radars, directory = ".",
          paste(wrong_codes, collapse = ", "))
   }
 
-  # Split 5 letter radar codes into form be_jab
+  # Split 5 letter radar codes into format be_jab
   ra_dars <- paste(substring(radars, 1, 2), substring(radars, 3, 5),
                        sep = "_")
 
-  # Create series of year/month based on date_min/max: 2016/10, 2016/11, 2016/12
+  # Create series of yyyy/mm based on date_min/max: 2016/10, 2016/11, 2016/12
   dates <- seq(
     as.Date(date_min, tz = NULL),
     as.Date(date_max, tz = NULL),
@@ -53,7 +53,7 @@ download_vpfiles <- function(date_min, date_max, radars, directory = ".",
   )
   year_months <- format(dates, "%Y/%m")
 
-  # Expand to series of radar/year/month: be_jab/2016/10, be_wid/2016/10, ...
+  # Expand to series of radar/yyyy/mm: be_jab/2016/10, be_wid/2016/10, ...
   radar_year_months <- apply(expand.grid(ra_dars, year_months), 1, paste,
                              collapse = "/")
 
@@ -64,16 +64,16 @@ download_vpfiles <- function(date_min, date_max, radars, directory = ".",
   message(paste("Downloading data from", base_url))
 
   for (radar_year_month in radar_year_months) {
-    # Create filename of form bejab201610.zip (removing _ and /)
+    # Create filename of format bejab201610.zip (removing _ and /)
     file_name <- paste0(gsub("/", "", gsub("_", "", radar_year_month)), ".zip")
-    # Create filepath of form directory/bejab201610.zip
+    # Create filepath of format directory/bejab201610.zip
     file_path <- file.path(directory, file_name)
-    # Create url of form base_url/be/jab/2016/bejab201610.zip (removing month)
+    # Create url of format base_url/be/jab/2016/bejab201610.zip (removing month)
     url <- paste(base_url, gsub("_", "/",
                 substring(radar_year_month, 1, nchar(radar_year_month) - 3)),
                 file_name,
                 sep = "/")
-    # Create local unzip directory of form directory/bejab/2016/10
+    # Create local unzip directory of format directory/bejab/2016/10
     unzip_dir <- gsub("_", "", radar_year_month)
 
     # Skip download if overwrite = FALSE and file already exists locally
