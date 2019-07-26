cb3530317513603aa77a2c446d5403e20eb94168
adokter <adriaandokter@gmail.com>
2019-07-26 17:24:27 -0400

    adding nearest argument, and allowing string input of dates

--- R/filter.R
+++ R/filter.R
@@ -1,8 +1,10 @@
 #' Time selection in a time series of vertical profiles ('vpts')
 #'
 #' @param x A \code{vpts} object.
-#' @param min POSIXct value. Minimum datetime to be included
-#' @param max POSIXct value. Maximum datetime to be included
+#' @param min Minimum datetime to be included. POSIXct value or charater string convertable to POSIXct.
+#' @param max Maximum datetime to be included. POSIXct value or charater string convertable to POSIXct.
+#' @param nearest If specified, \code{min} and \code{max} are ignored and the profile nearest to the
+#' specified datetime is returned. POSIXct value or charater string convertable to POSIXct.
 #' @return An object of class '\link[=summary.vpts]{vpts}'.
 #'
 #' @export
@@ -12,16 +14,26 @@
 #' example_vpts
 #' # select profiles later than 02-Sep-2016
 #' filter_datetime(example_vpts, min=as.POSIXct("2016-09-02"))
-filter_datetime = function(x, min, max){
+filter_datetime = function(x, min, max, nearest){
   assert_that(is.vpts(x))
+  errorf=function(e){min}
   if(!missing(min)){
+    if(is.string(min)) min=tryCatch(as.POSIXct(min),errorf=function(e){min})
     assert_that(is.time(min))
     assert_that(length(min) == 1)
 
   }
   if(!missing(max)){
+    if(is.string(max)) max=tryCatch(as.POSIXct(max),errorf=function(e){max})
     assert_that(is.time(max))
     assert_that(length(max) == 1)
+  }
+  if(!missing(nearest)){
+    if(is.string(nearest)) nearest=tryCatch(as.POSIXct(nearest),errorf=function(e){nearest})
+    assert_that(is.time(nearest))
+    assert_that(length(nearest) == 1)
+    idx=which.min(abs(difftime(x$datetime,nearest)))
+    return(x[idx])
   }
   if(missing(min) & missing(max)) return(x)
   if(missing(min) & !missing(max)) idx=x$datetime<max
