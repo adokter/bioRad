bba8c866765590c8fe8d7e2d06cb6ac4291ca477
adokter <adriaandokter@gmail.com>
2019-10-17 16:47:15 -0400

    fix issue #211

--- R/docker.R
+++ R/docker.R
@@ -102,7 +102,7 @@ mount_docker_container <- function(mount = "~/") {
   if (.Platform$OS.type == "unix") {
     result <- system(
       paste("docker run -v ",
-        system(paste("printf %q ", shQuote(normalizePath(mount)), sep = ""), intern = T),
+        system(paste("/bin/bash -c",shQuote(paste("printf %q", shQuote(normalizePath(mount))))), intern = T),
         ":/data -d --name vol2bird adokter/vol2bird sleep infinity",
         sep = ""
       ),
