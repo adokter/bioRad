48b8fd03bc2f8af7ca4926a8a15222040194e693
adokter <adriaandokter@gmail.com>
2020-01-21 17:01:15 -0500

    fixes following devtools check

--- tests/testthat/test-raster_input.R
+++ tests/testthat/test-raster_input.R
@@ -45,19 +45,18 @@ test_that('check if other projection gives same result',{
   expect_equal(values(raster(integrate_to_ppi(example_pvol, example_vp, res=res)$data))[!is.na(values(res))],
                values(res)[!is.na(values(res))])
 })
+
 context('check raster input for project_as_ppi')
 test_that('sample_polar works',{
   data("example_scan")
   a<-example_scan$params[[3]]
-
-  expect_s4_class(b<-bioRad::sample_polar(example_scan$params[[3]],500,10000, xlim=12.9+c(-1,1), ylim=56.4+c(-1,1), project = F), 'SpatialGridDataFrame')
-  expect_equivalent(b,sample_polar(example_scan$params[[3]],raster(b), project = F))
-  expect_s4_class(bb<-bioRad::sample_polar(example_scan$params[[3]],5000,10000, xlim=12.9+c(-1,1), ylim=56.4+c(-1,1), project = T), 'SpatialGridDataFrame')
-  expect_equivalent(bb,sample_polar(example_scan$params[[3]],raster(b), project = T))
-
+  expect_s4_class(b<-bioRad:::sample_polar(example_scan$params[[3]],500,10000, xlim=12.9+c(-1,1), ylim=56.4+c(-1,1), project = F), 'SpatialGridDataFrame')
+  expect_equivalent(b,bioRad:::sample_polar(example_scan$params[[3]],raster(b), project = F))
+  expect_s4_class(bb<-bioRad:::sample_polar(example_scan$params[[3]],5000,10000, xlim=12.9+c(-1,1), ylim=56.4+c(-1,1), project = T), 'SpatialGridDataFrame')
+  expect_equivalent(bb,bioRad:::sample_polar(example_scan$params[[3]],raster(bb), project = T))
 })
 
-test_that('sample_polar works',{
+test_that('project_as_ppi works',{
   data("example_scan")
   expect_s3_class(b<-project_as_ppi(example_scan,500,10000, project = F), 'ppi')
   expect_s3_class(bb<-project_as_ppi(example_scan,raster(b$data), project = F), 'ppi')
@@ -72,11 +71,10 @@ test_that('sample_polar works',{
 })
 
 
-test_that('sample_polar works from different projection',{
+test_that('project_as_ppi works from different projection',{
   data("example_scan")
   expect_s3_class(b<-project_as_ppi(example_scan,3000,5000, project = F), 'ppi')
-  expect_s4_class(r<-rasterFromXYZ(spTransform(as(b$data,'SpatialPoints')[s<-c(1,11),],'+proj=longlat'),crs = '+proj=longlat'), 'RasterLayer')
+  expect_s4_class(r<-raster::rasterFromXYZ(spTransform(as(b$data,'SpatialPoints')[s<-c(1,11),],'+proj=longlat'),crs = '+proj=longlat'), 'RasterLayer')
   expect_s3_class(bb<-project_as_ppi(example_scan,r, project = F), 'ppi')
   expect_equivalent(b$data@data[s,], bb$data@data[c(1,4),])
 })
-
