6cd096731b95b4901328ea7473b3b08bb2155499
peterdesmet <peter.desmet@inbo.be>
2020-04-08 22:11:16 +0200

    Test that density/mtr/vid is updated
    And assignment of sd_vvp_threshold to 2

--- tests/testthat/test-rcs.R
+++ tests/testthat/test-rcs.R
@@ -44,6 +44,44 @@ test_that("rcs assignment works", {
   expect_equal(rcs(vpi), 5.5)
 })
 
+test_that("rcs assignments updates density", {
+  # Not tested for vp_list as that is a repetition of vp method
 
+  # Precondition: at least some of the dens > 0 below sd_vvp_threshold
+  # This is not the case for sd_vvp_threshold = 2, which is why it is set to 3
+  vp$attributes$how$sd_vvp_thresh <- 3
+  vpts$attributes$how$sd_vvp_thresh <- 3
+  expect_true(any(vp$data$dens[vp$data$sd_vvp < 3] > 0, na.rm = TRUE))
+  expect_true(any(vpts$data$dens[vpts$data$sd_vvp < 3] > 0, na.rm = TRUE))
 
+  rcs(vp) <- 6
+  rcs(vpts) <- 6
+
+  # dens = eta/rcs when above sd_vvp_threshold
+  expect_equal(
+    vp$data$dens[vp$data$sd_vvp >= 3], vp$data$eta[vp$data$sd_vvp >= 3] / 6
+  )
+  expect_equal(
+    vpts$data$dens[vpts$data$sd_vvp >= 2], vpts$data$eta[vpts$data$sd_vvp >= 3] / 6
+  )
+  # dens = 0 when below sd_vvp_threshold and not NA
+  expect_true(all(vp$data$dens[vp$data$sd_vvp < 3] == 0, na.rm = TRUE))
+  expect_true(all(vpts$data$dens[vpts$data$sd_vvp < 3] == 0, na.rm = TRUE))
+})
+
+test_that("rcs assignment sets sd_vvp_threshold to 2 when NULL", {
+  # Not tested for vp_list as that is a repetition of vp method
+
+  vp$attributes$how$sd_vvp_thresh <- NULL
+  vpts$attributes$how$sd_vvp_thresh <- NULL
+  expect_warning(rcs(vp) <- 6, "Threshold for sd_vvp not set, defaulting to 2 m/s.")
+  expect_warning(rcs(vpts) <- 6, "Threshold for sd_vvp not set, defaulting to 2 m/s.")
+  expect_equal(sd_vvp_threshold(vp), 2)
+  expect_equal(sd_vvp_threshold(vpts), 2)
+})
+
+test_that("rcs assignment updates mtr and vid in vpi", {
+  rcs(vpi) <- 5.5
+  expect_equal(vpi$mtr, vpi$rtr / 5.5)
+  expect_equal(vpi$vid, vpi$vir / 5.5)
 })
