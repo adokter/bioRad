c1e12ae727cea170939334b825be05f8d586654c
Cecilia Nilsson <cecilia.nilsson@biol.lu.se>
2020-04-15 20:05:17 +0200

    get_scan unit tests

--- tests/testthat/test-get_scan.R
+++ tests/testthat/test-get_scan.R
@@ -1,3 +1,19 @@
-test_that("returns error on incorrect parameters", {
+pvolfile <- system.file("extdata", "volume.h5", package = "bioRad")
+pvol <- read_pvolfile(pvolfile)
+scan <- pvol[[1]]
 
+test_that("returns error on incorrect parameters", {
+  expect_error(get_scan("not_a_pvol", 5), "`x` must be a pvol object.")
+})
+
+test_that("get_scan() returns a object of class scan", {
+  expect_s3_class(get_scan(pvol, 5), "scan")
+})
+
+test_that("get_scan() returns the scan closest to elev parameter", {
+  scan_2 <- get_scan(pvol, 2)
+  all_angels <- get_elevation_angles(pvol)
+
+  expect_equal(scan2$geo$elangle,
+               angels[which.min(abs(get_elevation_angles(pvol)-2))])
 })
