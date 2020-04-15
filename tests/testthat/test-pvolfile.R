e5404050f2d5782581eb2e5d78c4bd793997ed27
peterdesmet <peter.desmet@inbo.be>
2020-04-15 13:04:09 +0200

    Review tests
    - Use expect_true/false()
    - expect_warning() will take into account x + actual warning: https://testthat.r-lib.org/reference/expect_message.html

--- tests/testthat/test-pvolfile.R
+++ tests/testthat/test-pvolfile.R
@@ -1,29 +1,29 @@
-#locate a polar volume file:
+# Locate polar volume file
 pvolfile <- system.file("extdata", "volume.h5", package = "bioRad")
 vpfile <- system.file("extdata", "profile.h5", package = "bioRad")
 textfile <- system.file("extdata", "example_vpts.txt", package = "bioRad")
 ghostfile <- paste(tempdir(),"/garbage",sep="")
 
 test_that("is.odimfile() can identify ODIM hdf5 files", {
-  expect_equal(bioRad:::is.odimfile(pvolfile), TRUE)
-  expect_equal(bioRad:::is.odimfile(vpfile), TRUE)
-  expect_equal(suppressWarnings(bioRad:::is.odimfile(textfile)), FALSE)
+  expect_true(bioRad::is.odimfile(pvolfile))
+  expect_true(bioRad::is.odimfile(vpfile))
+  expect_false(suppressWarnings(bioRad::is.odimfile(textfile)))
   # TODO: test for non-complient ODIM hdf5 files with missing groups/attributes
 })
 
 test_that("get_odim_object_type() returns a valid ODIM type", {
-  expect_equal(get_odim_object_type(pvolfile),"PVOL")
-  expect_equal(get_odim_object_type(vpfile),"VP")
-  expect_equal(suppressWarnings(get_odim_object_type(textfile)),NA)
-  expect_warning(get_odim_object_type(textfile),paste(textfile, "is not a HDF5 file"))
-  expect_equal(suppressWarnings(get_odim_object_type(ghostfile)),NA)
-  expect_warning(get_odim_object_type(ghostfile),paste(ghostfile, "does not exist"))
+  expect_equal(get_odim_object_type(pvolfile), "PVOL")
+  expect_equal(get_odim_object_type(vpfile), "VP")
+  expect_equal(suppressWarnings(get_odim_object_type(textfile)), NA)
+  expect_warning(get_odim_object_type(textfile), "is not a HDF5 file.")
+  expect_equal(suppressWarnings(get_odim_object_type(ghostfile)), NA)
+  expect_warning(get_odim_object_type(ghostfile), "does not exist.")
 })
 
 test_that("is.pvolfile() can identify ODIM hdf5 polar volume files", {
-  expect_equal(is.pvolfile(pvolfile), TRUE)
-  expect_equal(is.pvolfile(vpfile), FALSE)
-  expect_equal(is.pvolfile(textfile), FALSE)
-  expect_warning(is.pvolfile(ghostfile),paste(ghostfile, "does not exist"))
+  expect_true(is.pvolfile(pvolfile))
+  expect_false(is.pvolfile(vpfile))
+  expect_false(suppressWarnings(is.pvolfile(textfile)))
+  expect_warning(is.pvolfile(ghostfile), "does not exist.")
   # TODO: test and identify NEXRAD and IRIS polar volume files
 })
