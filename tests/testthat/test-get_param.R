cc4ad27b3e9f1ef4394090cd690f280f7d783b83
Cecilia Nilsson <cecilia.nilsson@biol.lu.se>
2020-04-16 17:31:06 +0200

    Unit tests for get_parm

--- tests/testthat/test-get_param.R
+++ tests/testthat/test-get_param.R
@@ -1,3 +1,17 @@
-test_that("returns error on incorrect parameters", {
+pvolfile <- system.file("extdata", "volume.h5", package = "bioRad")
+pvol <- read_pvolfile(pvolfile)
+scan <- pvol$scans[[1]]
 
+test_that("returns error on incorrect parameters", {
+  expect_error(get_param("not_a_scan"), "`x` must be a scan object.")
+  expect_error(get_param(vp),"`x` must be a scan object.")
+  expect_error(get_param(pvol),"`x` must be a scan object.")
+  expect_error(get_param(scan))
+  expect_error(get_param(scan, "not_a_param"), "Scan parameter not_a_param not found in `x`.")
 })
+
+test_that("get_param returns correct parameters", {
+  expect_equal(get_param(scan, names(scan$params[1])), scan$params[[1]])
+  expect_equal(get_param(scan, names(scan$params[2])), scan$params[[2]])
+})
+
