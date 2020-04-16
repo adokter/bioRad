a259f721593b8734fc5f4f29fb01f2213b88285a
peterdesmet <peter.desmet@inbo.be>
2020-04-16 17:02:10 +0200

    Update pvol doc

--- R/pvol.R
+++ R/pvol.R
@@ -10,42 +10,37 @@
 #' @export
 #'
 #' @details
-#' A polar volume (`pvol`) object is a list containing:
+#' A polar volume consists of a number of scans (or sweeps) made by the radar at
+#' different elevation angles. A polar volume (`pvol`) object is a list
+#' containing:
 #' * `radar`: Radar identifier.
-#' * `datetime`: Nominal time of the volume (UTC).
+#' * `datetime`: Nominal time of the volume in UTC.
 #' * `scans`: List of scans (`scan`) at different elevation angles.
-#' * `attributes`: List of the the volume's `what`, `where` and `how`
+#' * `attributes`: List of the volume's `what`, `where` and `how`
 #' attributes.
 #' * `geo`: List of the volume's geographic properties:
-#'   * `lat`: Latitude of the radar (decimal degrees).
-#'   * `lon`: Longitude of the radar (decimal degrees).
-#'   * `height`: Height of the radar antenna (meters above sea level).
+#'   * `lat`: Latitude of the radar in decimal degrees.
+#'   * `lon`: Longitude of the radar in decimal degrees.
+#'   * `height`: Height of the radar antenna in meters above sea level.
 #'
 #' @seealso
-#' * [read_pvolfile()] to read polar volume files.
-#' * [summary.scan()] to inspect scans.
+#' * [read_pvolfile()]
+#' * [get_elevation_angles()]
+#' * [get_scan()]
 #'
 #' @examples
-#' # Locate the polar volume example file:
+#' # Locate and read the polar volume example file:
 #' pvolfile <- system.file("extdata", "volume.h5", package = "bioRad")
-#'
-#' # Read the file:
 #' example_pvol <- read_pvolfile(pvolfile)
 #'
-#' # Verify that it is a pvol object:
+#' # Verify that is an object of class pvol:
 #' is.pvol(example_pvol)
 #'
-#' # Print summary info:
+#' # Get summary info:
 #' example_pvol # Same as summary(example_pvol) or print(example_pvol)
 #'
-#' # Print summary info for the scans in the polar volume:
+#' # Get summary info for the scans in the polar volume:
 #' example_pvol$scans
-#'
-#' # Copy the first scan to a new object `scan`:
-#' scan <- example_pvol$scans[[1]]
-#'
-#' # Print summary info for the scan:
-#' scan # Same as summary(scan) or print(scan)
 summary.pvol <- function(x, ...) {
   print.pvol(x)
 }
@@ -54,13 +49,15 @@ summary.pvol <- function(x, ...) {
 #'
 #' @inheritParams summary.pvol
 #'
-#' @return For `is.pvol(x)`: `TRUE` if `x` is an object of class `pvol`,
-#'   otherwise `FALSE`.
+#' @return For [is.pvol()]: `TRUE` for an object of class `pvol`, otherwise
+#'   `FALSE`.
 #'
 #' @rdname summary.pvol
 #'
 #' @export
-is.pvol <- function(x) inherits(x, "pvol")
+is.pvol <- function(x) {
+  inherits(x, "pvol")
+}
 
 #' Print summary for an object of class `pvol`
 #'
