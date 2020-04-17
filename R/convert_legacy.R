e9191bb36953f67e5745526261c37147ad0d6475
adokter <adriaandokter@gmail.com>
2020-04-17 10:47:29 -0400

    fix punctuation

--- R/convert_legacy.R
+++ R/convert_legacy.R
@@ -1,11 +1,11 @@
-#' Convert legacy bioRad objects to latest version
+#' Convert legacy bioRad objects to current version
 #'
 #' Convert legacy bioRad objects (vp, vpts) that have become
-#' obsolete and make them compatible with latest bioRad version
+#' obsolete and make them compatible with the current bioRad version.
 #'
 #' @param x A `vp`, or `vpts` object.
 #'
-#' @return An updated object of the same class as the input
+#' @return An updated object of the same class as the input.
 #'
 #' @export
 #'
@@ -13,7 +13,7 @@
 #' # convert a vp object:
 #' convert_legacy(example_vp)
 #'
-#' # convert a vpts object
+#' # convert a vpts object:
 #' convert_legacy(example_vpts)
 convert_legacy <- function(x) {
   UseMethod("convert_legacy", x)
