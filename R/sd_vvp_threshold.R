f3affa4ac4522f26730bb743f3f09b9a1dac7e0c
adokter <adriaandokter@gmail.com>
2020-04-19 17:29:00 -0400

    fix that sd_vvp_threshold can be zero

--- R/sd_vvp_threshold.R
+++ R/sd_vvp_threshold.R
@@ -85,7 +85,7 @@ sd_vvp_threshold.vpts <- function(x) {
 `sd_vvp_threshold<-.vp` <- function(x, value) {
   stopifnot(inherits(x, "vp"))
   assert_that(is.numeric(value))
-  assert_that(value > 0)
+  assert_that(value >= 0)
   x$attributes$how$sd_vvp_thresh <- value
   if (is.numeric(x$attributes$how$rcs_bird)) {
     x$data$dens <- x$data$eta / x$attributes$how$rcs_bird
@@ -118,7 +118,7 @@ sd_vvp_threshold.vpts <- function(x) {
 `sd_vvp_threshold<-.vpts` <- function(x, value) {
   stopifnot(inherits(x, "vpts"))
   assert_that(is.numeric(value))
-  assert_that(value > 0)
+  assert_that(value >= 0)
   x$attributes$how$sd_vvp_thresh <- value
   if (is.numeric(x$attributes$how$rcs_bird)) {
     x$data$dens <- x$data$eta / x$attributes$how$rcs_bird
