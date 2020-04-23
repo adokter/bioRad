23a087f0c86521349060e8f29d6edd1f28a70f42
adokter <adriaandokter@gmail.com>
2020-04-23 12:56:27 -0400

    additional warning for #368

--- R/read_pvolfile.R
+++ R/read_pvolfile.R
@@ -234,6 +234,9 @@ read_pvolfile_body <- function(file, param = c(
   # filter out NULL output from read_pvolfile_scan
   valid_scans <- which(!sapply(data, is.null))
   assert_that(length(valid_scans) > 0, msg = paste("none of the requested scan parameters found in file", file))
+  if(length(valid_scans) < length(scans)){
+    warning(paste("ignoring",length(scans)-length(valid_scans),"scan(s) in file",file,"because requested scan parameter(s) are missing."))
+  }
   data <- data[valid_scans]
 
   # order by elevation
