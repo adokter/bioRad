186b73d76c71a143e50086d01754e32251d82a4a
peterdesmet <peter.desmet@inbo.be>
2020-04-27 11:38:56 +0200

    Write summary.pvol, print.pvol, is.pvol tests

--- tests/testthat/test-pvol.R
+++ tests/testthat/test-pvol.R
@@ -1,3 +1,24 @@
-test_that("... returns error on incorrect parameters", {
+pvolfile <- system.file("extdata", "volume.h5", package = "bioRad")
+pvol <- read_pvolfile(pvolfile)
+vp <- example_vp
 
+# No tests for error on incorrect parameters:
+# summary(), print() are generic and work for every input
+# is.pvol() returns TRUE/FALSE and works for every input
+
+# print.pvol() is not tested as it is the same as and called from summary.pvol()
+
+test_that("summary.pvol() prints metadata to the console", {
+  expect_output(summary(pvol), "Polar volume (class pvol)", fixed = TRUE)
+  expect_output(summary(pvol), "# scans:", fixed = TRUE)
+  expect_output(summary(pvol), "radar:", fixed = TRUE)
+  expect_output(summary(pvol), "source:", fixed = TRUE)
+  expect_output(summary(pvol), "nominal time:", fixed = TRUE)
+
+})
+
+test_that("is.pvol() returns TRUE/FALSE correctly", {
+  expect_true(is.pvol(pvol))
+  expect_false(is.pvol("not_a_pvol"))
+  expect_false(is.pvol(vp))
 })
