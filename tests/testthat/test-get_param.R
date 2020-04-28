807fa8546743ae7abcf76d15bacc05983378baf3
peterdesmet <peter.desmet@inbo.be>
2020-04-28 17:03:34 +0200

    Test for literal parameters + test class

--- tests/testthat/test-get_param.R
+++ tests/testthat/test-get_param.R
@@ -1,6 +1,6 @@
 pvolfile <- system.file("extdata", "volume.h5", package = "bioRad")
 pvol <- read_pvolfile(pvolfile)
-scan <- pvol$scans[[1]]
+scan <- example_scan
 
 test_that("get_param() returns error on incorrect parameters", {
   expect_error(get_param("not_a_scan", "DBZH"), "`x` must be a scan object.", fixed = TRUE)
@@ -9,7 +9,9 @@ test_that("get_param() returns error on incorrect parameters", {
   expect_error(get_param(scan, "not_a_param"), "Can't find parameter `not_a_param` in `x`", fixed = TRUE)
 })
 
-test_that("get_param() returns correct parameters", {
-  expect_equal(get_param(scan, names(scan$params[1])), scan$params[[1]])
-  expect_equal(get_param(scan, names(scan$params[2])), scan$params[[2]])
+test_that("get_param() returns correct parameter", {
+  # Parameters: VRADH DBZH ZDR RHOHV PHIDP
+  expect_equal(get_param(scan, "DBZH"), scan$params[["DBZH"]])
+  expect_equal(get_param(scan, "PHIDP"), scan$params[["PHIDP"]])
+  expect_s3_class(get_param(scan, "RHOHV"), "param")
 })
