d992288f0cb9b80f728b517b3355c95dd7f8da9b
bart <bart@example.com>
2020-04-30 12:10:57 +0200

    some documentation updates

--- R/vcp.R
+++ R/vcp.R
@@ -1,13 +1,15 @@
-#' Extract a volume coverage pattern table
+#' Extract a volume coverage pattern dataframe
 #'
 #' @param x Either a pvol or scan for which the VCP should be created.
-#' @param add_params A logical wether a column with parameters be added.
-#' @param select A charcter vector which the column names that should be returned when NULL all attributes are returned
+#' @param add_params A logical wether a column with with the parameters present is to be added.
+#' @param select A charcter vector which the column names that should be returned when NULL all attributes are to be returned
 #' @param ... Currently not used
 #'
-#' This function tabulates the attributes of one scan or all scans of a pvol. Attributes that have a length longer then one are presented as a list column.
-#' By default the function returns a limited set of column to keep the output clear.
-#' It is important to note thad attributes of the polar volume can contain additional information on processing that is not included in the resulting table.
+#' This function tabulates the attributes of one scan or all scans of a pvol.
+#' Attributes that have a length longer then one are presented as a list column.
+#' By default the function returns a limited set of columns to keep the output clear.
+#' It is important to note that attributes of the full polar volume can contain additional information on processing that is not included in the resulting table.
+#' This function only tabulates attributes of the scans.
 #'
 #' @export
 #'
@@ -35,7 +37,7 @@ vcp <-
     assert_that(inherits(x, "scan") | inherits(x, "pvol"))
     assert_that(is.flag(add_params))
     assert_that(is.character(select) | is.null(select))
-    if (class(x) == "pvol") {
+    if (inherits(x, "pvol")) {
       df <-
         do.call(
           "rbind",
@@ -49,7 +51,6 @@ vcp <-
         )
       return(df)
     }
-
     t <- unlist(x$attributes, F)
     g <- lapply(t, function(x) {
       ifelse(length(x) != 1, (list(x)), x)
