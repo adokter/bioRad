11662317c7e9f2184a33a1e261f611f5d3eacef1
peterdesmet <peter.desmet@inbo.be>
2021-06-07 20:31:56 +0200

    Test for as.POSIXct

--- tests/testthat/test-doy_noy.R
+++ tests/testthat/test-doy_noy.R
@@ -7,15 +7,16 @@ pvol <- read_pvolfile(pvolfile)
 test_that("doy_noy() returns error on incorrect parameters", {
   expect_error(doy("not_a_object"))
   expect_error(noy("not_a_object"))
-
 })
 
 test_that("doy_noy() returns a number", {
+  expect_type(doy(as.POSIXct("2015-01-01 18:00:00 UTC"), 12.8517, 56.3675), "integer")
   expect_type(doy(vp), "integer")
   expect_type(doy(vpts), "integer")
   expect_type(doy(vpi), "integer")
   expect_type(doy(pvol), "integer")
 
+  expect_type(noy(as.POSIXct("2015-01-01 18:00:00 UTC"), 12.8517, 56.3675), "integer")
   expect_type(noy(vp), "integer")
   expect_type(noy(vpts), "integer")
   expect_type(noy(vpi), "integer")
