3b53e3b9350bd7cefc72b730d549d75c0c5f9bfe
peterdesmet <peter.desmet@inbo.be>
2021-06-08 12:39:18 +0200

    Name legacy objects clearly

--- tests/testthat/test-convert_legacy.R
+++ tests/testthat/test-convert_legacy.R
@@ -1,20 +1,20 @@
 # Make legacy objects
-vp <- example_vp
-vpts <- example_vpts
-names(vp$data) <- sub("height", "HGHT", names(vp$data))
-names(vpts$data) <- sub("height", "heights", names(vpts$data))
-names(vpts$data) <- sub("dates", "datetime", names(vpts$data))
+legacy_vp <- example_vp
+legacy_vpts <- example_vpts
+names(legacy_vp$data) <- sub("height", "HGHT", names(legacy_vp$data))
+names(legacy_vpts$data) <- sub("height", "heights", names(legacy_vpts$data))
+names(legacy_vpts$data) <- sub("dates", "datetime", names(legacy_vpts$data))
 
 test_that("convert_legacy() returns error on incorrect parameters", {
   expect_error(convert_legacy("not_a_object"))
 })
 
 test_that("convert_legacy() returns same object type", {
-  expect_s3_class(convert_legacy(vp), "vp")
-  expect_s3_class(convert_legacy(vpts), "vpts")
+  expect_s3_class(convert_legacy(legacy_vp), "vp")
+  expect_s3_class(convert_legacy(legacy_vpts), "vpts")
 })
 
 test_that("convert_legacy() renames certain data colnames", {
-  expect_identical(convert_legacy(vp), example_vp)
-  expect_identical(convert_legacy(vpts), example_vpts)
+  expect_identical(convert_legacy(legacy_vp), example_vp)
+  expect_identical(convert_legacy(legacy_vpts), example_vpts)
 })
