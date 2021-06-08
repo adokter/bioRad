7a0778cb89478182dabcc5a25cc8213b718b2521
Cecilia Nilsson <cecilia.nilsson@biol.lu.se>
2021-06-08 17:05:34 +0200

    start on plot tests

--- tests/testthat/test-plot.ppi.R
+++ tests/testthat/test-plot.ppi.R
@@ -1,3 +1,5 @@
-test_that("returns error on incorrect parameters", {
 
+test_that("plot.ppi() returns error on incorrect parameters", {
+  expect_error(plot.ppi("not_a_ppi"))
+  expect_error(plot.ppi("not_a_object"))
 })
