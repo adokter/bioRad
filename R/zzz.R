53e89fd187b0a060f1ba98e0be857aaf57289db8
peterdesmet <peter.desmet@inbo.be>
2021-06-09 18:29:20 +0200

    Create skip_if_no_docker() function

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-#' Identify `NaN` in a dataframe.
+#' Identify `NaN` in a dataframe
 #'
 #' Identify cells with `NaN` (not a number) in a data frame. Improves on the
 #' default[base::is.nan()] function, which only works on vectors, by allowing
@@ -11,3 +11,17 @@
 #'
 #' @keywords internal
 is.nan.data.frame <- function(x) do.call(cbind, lapply(x, is.nan))
+
+#' Skip test if no docker
+#'
+#' Some functions require Docker. This helper function allows to skip a test if
+#' the Docker container is not available, e.g. when running in CI.
+#' Inspired by <https://testthat.r-lib.org/articles/skipping.html#helpers>.
+#'
+#' @keywords internal
+skip_if_no_docker <- function() {
+  if (check_docker(verbose = FALSE) == 0) {
+    return(invisible(TRUE))
+  }
+  skip("No docker")
+}
