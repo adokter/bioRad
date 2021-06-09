6a80ae60d46cede0ba0a59a656342d94d2eeb61c
bart <bart@example.com>
2021-06-09 16:17:58 +0200

    update tests accordingly

--- tests/testthat/test-attribute_table.R
+++ tests/testthat/test-attribute_table.R
@@ -3,26 +3,23 @@ example_pvol <- read_pvolfile(pvolfile)
 data(example_scan)
 test_that("returns error on incorrect parameters", {
   expect_error(attribute_table("not_a_vp"))
-  expect_error(attribute_table(example_pvol, add_param = "a"))
   expect_error(attribute_table(example_pvol, select = 1L))
 })
 test_that("result is correct", {
   expect_equal(nrow(attribute_table(example_pvol)), length(example_pvol$scans))
-  expect_equal(attribute_table(example_pvol)$params[[2]], names(example_pvol$scans[[2]]$params))
+  expect_equal(attribute_table(example_pvol)$param[[2]], names(example_pvol$scans[[2]]$param))
   expect_equal(attribute_table(example_pvol)$where.elangle, get_elevation_angles(example_pvol))
-  expect_equal(attribute_table(example_pvol)$where.nbins[[3]], nrow(example_pvol$scans[[3]]$params[[1]]))
+  expect_equal(attribute_table(example_pvol)$where.nbins[[3]], nrow(example_pvol$scans[[3]]$param[[1]]))
   expect_equal(attribute_table(example_scan), attribute_table(example_pvol)[1, ])
   expect_equal(attribute_table(example_pvol, select = "how.stopazT", F)[, 1], lapply(example_pvol$scans, function(x) x$attributes$how$stopazT))
 })
 test_that("add_params works", {
-  expect_false("params" %in% colnames(attribute_table(example_pvol, add_params = F)))
-  expect_true("params" %in% colnames(attribute_table(example_pvol, add_params = T)))
-  expect_true("params" %in% colnames(attribute_table(example_pvol)))
-  expect_type(attribute_table(example_pvol)$params, "list")
-  expect_type(attribute_table(example_pvol)$params[[2]], "character")
+  expect_true("param" %in% colnames(attribute_table(example_pvol)))
+  expect_type(attribute_table(example_pvol)$param, "list")
+  expect_type(attribute_table(example_pvol)$param[[2]], "character")
 })
 test_that("select argument works", {
-  expect_equal(setdiff(colnames(attribute_table(example_scan, add_params = F)), c(
+  expect_equal(setdiff(colnames(attribute_table(example_scan)), c(
     "how.lowprf",
     "how.midprf",
     "how.highprf",
@@ -30,18 +27,20 @@ test_that("select argument works", {
     "where.nbins",
     "where.nrays",
     "where.rscale",
-    "how.NI"
+    "how.NI",
+    "param"
   )), character(0))
-  expect_equal(colnames(attribute_table(example_pvol, select = NULL, add_params = F)), names(unlist(example_pvol$scans[[1]]$attributes, recursive = F)))
+  expect_equal(colnames(attribute_table(example_pvol, select = NULL)), c(names(unlist(example_pvol$scans[[1]]$attributes, recursive = F)), 'param'))
 })
 
-test_that("silently isnores non existant colums", {
+test_that("silently ignores non existant colums", {
   expect_equal(setdiff(colnames(attribute_table(example_scan, select = c(
     "how.lowprf",
     "how.NI", "asdf"
-  ), add_params = F)), c(
+  ))), c(
     "how.lowprf",
-    "how.NI"
+    "how.NI",
+    "param"
   )), character(0))
   expect_silent(attribute_table(example_scan, select = c(
     "how.lowprf",
