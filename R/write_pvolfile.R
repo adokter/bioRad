1b6f089e57a9d126ba1f8cb05e3db4a1a89f6fa9
Bart Hoekstra <barthoekstra@gmail.com>
2021-06-10 18:08:40 +0200

    Moved attribute writing to separate function

--- R/write_pvolfile.R
+++ R/write_pvolfile.R
@@ -35,35 +35,117 @@ write_pvolfile <- function(pvol, file, overwrite = FALSE, same.type = TRUE) {
 
     # write scan attributes
     attrgroupnames <- names(pvol$scans[[i]]$attributes)
-    for(k in seq_along(attrgroupnames)){
-      group <- paste0("dataset", i, "/", attrgroupnames[k])
-      h5createGroup(fid, group)
-      gid <- H5Gopen(fid, group)
-      attrgroup <- pvol$scans[[i]]$attributes[[k]]
-      attribnames <- names(attrgroup)
-      for (l in seq_along(attribnames)){
-        h5writeAttribute(attrgroup[[l]], gid, attribnames[l])
-      }
-      H5Gclose(gid)
-    }
+    attrgroupnames <- paste0("dataset", i, "/", attrgroupnames)
+    write_group_attributes(fid, attrgroupnames, pvol$scans[[i]]$attributes)
+    # for(k in seq_along(attrgroupnames)){
+    #   group <- paste0("dataset", i, "/", attrgroupnames[k])
+    #   h5createGroup(fid, group)
+    #   gid <- H5Gopen(fid, group)
+    #   attrgroup <- pvol$scans[[i]]$attributes[[k]]
+    #   attribnames <- names(attrgroup)
+    #   for (l in seq_along(attribnames)){
+    #     h5writeAttribute(attrgroup[[l]], gid, attribnames[l])
+    #   }
+    #   H5Gclose(gid)
+    # }
   }
 
   # write volume attributes
   attrgroupnames <- names(pvol$attributes)
-  for(k in seq_along(attrgroupnames)){
-    group <- paste0(attrgroupnames[k])
-    h5createGroup(fid, group)
-    gid <- H5Gopen(fid, group)
-    attrgroup <- pvol$attributes[[k]]
-    attribnames <- names(attrgroup)
-    for (l in seq_along(attribnames)){
-      h5writeAttribute(attrgroup[[l]], gid, attribnames[l])
-      # # g <- H5Gopen(fid, paste(group, "/", attrgroup[[l]], sep = ""))
-      # aid <- H5Acreate(gid, attribnames[l], h5space = H5Screate(type = "H5S_SCALAR"))
-      # H5Awrite(aid)
-    }
-    H5Gclose(gid)
-  }
+  # groups <- paste0(attrgroupnames)
+  write_group_attributes(fid, attrgroupnames, pvol$attributes)
+
+  # for(k in seq_along(attrgroupnames)){
+  #   group <- paste0(attrgroupnames[k])
+  #   print(group)
+  #   h5createGroup(fid, group)
+  #   gid <- H5Gopen(fid, group)
+  #   attrgroup <- pvol$attributes[[k]]
+  #   attribnames <- names(attrgroup)
+  #   for (l in seq_along(attribnames)){
+  #     h5writeAttribute(attrgroup[[l]], gid, attribnames[l])
+  #     # # g <- H5Gopen(fid, paste(group, "/", attrgroup[[l]], sep = ""))
+  #     # aid <- H5Acreate(gid, attribnames[l], h5space = H5Screate(type = "H5S_SCALAR"))
+  #     # H5Awrite(aid)
+  #   }
+  #   H5Gclose(gid)
+  # }
 
   H5Fclose(fid)
 }
+
+write_group_attributes <- function(fid, group, attrgroups) {
+  for (k in seq_along(attrgroups)) {
+    print(group)
+    h5createGroup(fid, group[k])
+    gid <- H5Gopen(fid, group[k])
+
+    attrgroup <- attrgroups[k][[1]]
+    attribnames <- names(attrgroup)
+
+    for (l in seq_along(attribnames)) {
+      # print(dim(attrgroup[[l]]))
+      # sid <- H5Screate_simple(c(1,1))
+      # tid <- H5Tcopy("H5T_NATIVE_CHAR")
+      # aid <- H5Acreate(gid, attribnames[l], tid, sid)
+      # # aid <- H5Acreate(gid, attribnames[l], dtype_id = "H5T_NATIVE_CHAR",
+      # #                  h5space = H5Screate(type = "H5S_SCALAR"))
+      # H5Awrite(aid, attrgroup[[l]])
+      # h5writeAttribute(attrgroup[[l]][[1]], gid, attribnames[l])
+      h5wr(attrgroup[[l]][[1]], gid, attribnames[l])
+    }
+    H5Gclose(gid)
+  }
+}
+
+h5wr <- function(attr, h5obj, name, size=NULL) {
+  if (is.null(dim(attr))) {
+    # dim(attr) = length(attr)
+    d = length(attr)
+  } else {
+    d = dim(attr)
+  }
+  if (H5Aexists(h5obj, name)) {
+    H5Adelete(h5obj, name)
+  }
+  size = NULL
+  if (storage.mode(attr) == "character") {
+    size = max(nchar(attr))+1
+  }
+  h5createAttribute(h5obj, name, dims = d, storage.mode = storage.mode(attr), size=size)
+  h5attr <- H5Aopen(h5obj, name)
+
+  DimMem <- dim(attr)
+  DimMem <- d
+  h5spaceMem <- H5Screate_simple(DimMem)
+
+  try( { res <- H5Awrite(h5attr, attr) } )
+
+  try( { H5Sclose(h5spaceMem) } )
+  try( { H5Aclose(h5attr) } )
+  invisible(res)
+}
+
+# h5wr <- function(attr, h5obj, name, size=NULL) {
+#   if (is.null(dim(attr))) {
+#     dim(attr) = length(attr)
+#   }
+#   if (H5Aexists(h5obj, name)) {
+#     H5Adelete(h5obj, name)
+#   }
+#   size = NULL
+#   if (storage.mode(attr) == "character") {
+#     size = max(nchar(attr))+1
+#   }
+#   h5createAttribute(h5obj, name, dims = dim(attr), storage.mode = storage.mode(attr), size=size)
+#   h5attr <- H5Aopen(h5obj, name)
+#
+#   DimMem <- dim(attr)
+#   h5spaceMem <- H5Screate_simple(DimMem,NULL)
+#
+#   try( { res <- H5Awrite(h5attr, as.vector(attr)) } )
+#
+#   try( { H5Sclose(h5spaceMem) } )
+#   try( { H5Aclose(h5attr) } )
+#   invisible(res)
+# }
