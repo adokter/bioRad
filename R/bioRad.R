f7d63b3a2e689e057d2e0289ec15af64d9080bff
peterdesmet <peter.desmet@inbo.be>
2021-06-10 17:42:17 +0200

    Write bioRad.R in Markdown

--- R/bioRad.R
+++ R/bioRad.R
@@ -1,14 +1,12 @@
 #' @details
 #' To get started, see:
 #'
-#' \itemize{
-#'   \item \href{https://doi.org/10.1111/ecog.04028}{Dokter et al. (2019)}: a
-#'   paper describing the package.
-#'   \item \href{https://adokter.github.io/bioRad/articles/bioRad.html}{bioRad
-#'   vignette}: an introduction to bioRad's main functionalities.
-#'   \item \href{https://adokter.github.io/bioRad/reference/index.html}{Function
-#'   reference}: an overview of all bioRad functions.
-#' }
+#' * [Dokter et al. (2019)](https://doi.org/10.1111/ecog.04028): a paper describing the
+#' package.
+#' * [bioRad vignette](https://adokter.github.io/bioRad/articles/bioRad.html):
+#' an introduction to bioRad's main functionalities.
+#' * [Function reference](https://adokter.github.io/bioRad/reference/index.html)
+#' : an overview of all bioRad functions.
 #'
 #' @keywords internal
 #'
