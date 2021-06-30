d729c0785a25149e53e9617d9655c67c6c9c81e1
peterdesmet <peter.desmet@inbo.be>
2021-06-30 14:00:39 +0200

    Use Markdown for nyquist_velocity

--- R/nyquist_velocity.R
+++ R/nyquist_velocity.R
@@ -1,23 +1,24 @@
-#' Nyquist velocity for a given pulse repetition frequency (PRF)
+#' Calculate Nyquist velocity for a given pulse repetition frequency (PRF)
 #'
-#' Calculates the Nyquist velocity given a radar's pulse repetition frequency (PRF)
-#' and wavelength. When specifying two PRFs, the extended Nyquist
-#' velocity is given for a radar using the dual-PRF technique.
+#' Calculates the Nyquist velocity given a radar's pulse repetition frequency
+#' (PRF) and wavelength. When specifying two PRFs, the extended Nyquist velocity
+#' is given for a radar using the dual-PRF technique.
 #'
-#' @param wavelength radar wavelength in cm
-#' @param prf1 radar pulse repetition frequency in Hz
-#' @param prf2 alternate radar pulse repetition frequency in Hz (for a radar operating in dual-PRF mode)
+#' @param wavelength Numeric. Radar wavelength, in cm.
+#' @param prf1 Numeric. Radar pulse repetition frequency, in Hz.
+#' @param prf2 Numeric. Alternate radar pulse repetition frequency for a radar
+#'   operating in dual-PRF mode, in Hz.
 #'
-#' @return Nyquist velocity in m/s.
+#' @return Nyquist velocity, in m/s.
 #'
 #' @export
 #'
 #' @examples
-#' # at C-band (5.3 cm wavelength) and a PRF of 2000 Hz
+#' # Get Nyquist velocity at C-band (5.3 cm wavelength) and a PRF of 2000 Hz
 #' nyquist_velocity(5.3, 2000)
 #'
-#' # extended Nyquist velocity in a dual-PRF scheme
-#' # using 2000 Hz and 1500 Hz PRFs:
+#' # Get extended Nyquist velocity in a dual-PRF scheme using 2000 Hz and
+#' # 1500 Hz PRFs
 #' nyquist_velocity(5.3, 2000, 1500)
 nyquist_velocity <- function(wavelength, prf1, prf2) {
   assert_that(is.number(wavelength))
