a225f3a765e76b263d714cb860979bb621cfe93b
bart <bart@example.com>
2021-07-12 15:50:36 +0200

    expand testing

--- tests/testthat/test-operators.R
+++ tests/testthat/test-operators.R
@@ -18,23 +18,29 @@ test_that("param errors", {
   expect_silent(p2 + p3)
   expect_warning(p2 + p4, "*of the parameter.s. differ .e.g., location, range gate location. meaning you are likly to combine data that has been observed in different places.")
   data("example_scan")
-  s4<-example_scan
-  s2<-example_scan
-  s2$params<-list(DB=p2)
-  s4$params<-list(DB=p4)
+  s4 <- example_scan
+  s2 <- example_scan
+  s2$params <- list(DB = p2)
+  s4$params <- list(DB = p4)
   expect_warning(s2 + s4, "*of the parameter.s. differ .e.g., location, range gate location. meaning you are likly to combine data that has been observed in different places.")
-  s5<-example_scan
-  s6<-example_scan
-  s5$params<-list(DDB=p2, DF=p2)
-  s6$params<-list(DB=p2, DF=p2)
-  expect_warning(s5+s6,'The names of parameters do not match')
+  s5 <- example_scan
+  s6 <- example_scan
+  s5$params <- list(DDB = p2, DF = p2)
+  s6$params <- list(DB = p2, DF = p2)
+  expect_warning(s5 + s6, "The names of parameters do not match")
 })
 
 test_that("scan operators", {
   data(example_scan)
   expect_equal(example_scan + 1, 1 + example_scan)
-  expect_equal(matrix(get_param(example_scan - 1,'VRADH')), matrix(get_param(example_scan,'VRADH'))-1)
-  expect_equal(matrix(get_param(1-example_scan ,'VRADH')), 1-matrix(get_param(example_scan,'VRADH')))
+  expect_equal(
+    matrix(get_param(example_scan - 1, "VRADH")),
+    matrix(get_param(example_scan, "VRADH")) - 1
+  )
+  expect_equal(
+    matrix(get_param(1 - example_scan, "VRADH")),
+    1 - matrix(get_param(example_scan, "VRADH"))
+  )
   expect_equal(example_scan * 1, example_scan)
   expect_equal(example_scan, example_scan * 1)
   expect_equal(2 * example_scan, example_scan * 2)
@@ -44,8 +50,11 @@ test_that("scan operators", {
   expect_equal(example_scan * 1:480, 1:480 * example_scan)
   r <- matrix(runif(prod(dim(example_scan)[-1])), nrow = dim(example_scan)[2])
   expect_equal(example_scan * r, r * example_scan)
-  expect_equal(example_scan$params[["DBZH"]][4, 1:54] * r[4, 1:54],
-               get_param(r * example_scan, "DBZH")[4, 1:54])
+  expect_equal(
+    example_scan$params[["DBZH"]][4, 1:54] * r[4, 1:54],
+    get_param(r * example_scan, "DBZH")[4, 1:54]
+  )
+  expect_error(cumsum(example_scan), "not meaningful for scan objects")
 })
 
 
@@ -53,8 +62,14 @@ test_that("pvol operators", {
   pvolfile <- system.file("extdata", "volume.h5", package = "bioRad")
   example_pvol <- read_pvolfile(pvolfile)
   expect_equal(example_pvol + 1, 1 + example_pvol)
-  expect_equal(matrix(get_param(get_scan(example_pvol - 1,3),'DBZH')), matrix(get_param(get_scan(example_pvol,3),'DBZH'))-1)
-  expect_equal(matrix(get_param(get_scan(1-example_pvol,3 ),'DBZH')), 1-matrix(get_param(get_scan(example_pvol,3),'DBZH')))
+  expect_equal(
+    matrix(get_param(get_scan(example_pvol - 1, 3), "DBZH")),
+    matrix(get_param(get_scan(example_pvol, 3), "DBZH")) - 1
+  )
+  expect_equal(
+    matrix(get_param(get_scan(1 - example_pvol, 3), "DBZH")),
+    1 - matrix(get_param(get_scan(example_pvol, 3), "DBZH"))
+  )
   expect_equal(example_pvol * 1, example_pvol)
   expect_equal(example_pvol, example_pvol * 1)
   expect_equal(2 * example_pvol, example_pvol * 2)
@@ -69,10 +84,35 @@ test_that("pvol operators", {
   expect_equal(example_pvol, exp(log(example_pvol)))
   expect_equal(example_pvol, sqrt((example_pvol^2)))
   expect_equal(log10(example_pvol), log(example_pvol, base = 10))
-  expect_equal(Reduce("+", lapply(list(example_pvol, example_pvol), exp)),
-               exp(example_pvol) + exp(example_pvol))
-  expect_equal(get_scan(example_pvol * list(1.4, 1.7, 1.8), 1.5),
-               get_scan(example_pvol, 1.5) * 1.7)
-  expect_equal(get_scan(example_pvol * list(1.4 + 1:480, 1.7 + 1:480, 1.8 + 1:480), 1.5),
-               get_scan(example_pvol, 1.5) * (1.7 + 1:480))
+  expect_equal(
+    Reduce("+", lapply(list(example_pvol, example_pvol), exp)),
+    exp(example_pvol) + exp(example_pvol)
+  )
+  expect_equal(
+    get_scan(example_pvol * list(1.4, 1.7, 1.8), 1.5),
+    get_scan(example_pvol, 1.5) * 1.7
+  )
+  expect_equal(
+    get_scan(example_pvol * list(1.4 + 1:480, 1.7 + 1:480, 1.8 + 1:480), 1.5),
+    get_scan(example_pvol, 1.5) * (1.7 + 1:480)
+  )
+  expect_equal(
+    get_scan(list(1.4 + 1:480, 1.7 + 1:480, 1.8 + 1:480) * example_pvol, 2.5),
+    get_scan(example_pvol, 2.5) * (1.8 + 1:480)
+  )
+  expect_error(
+    example_pvol * list(1, 2), "only works if the list is equally long to the number"
+  )
+  expect_error(
+    list(1, 2) * example_pvol, "only works if the list is equally long to the number"
+  )
+  expect_error(cumsum(example_pvol), "not meaningful for pvol objects")
+})
+test_that("Compare calculate_param and ops", {
+  require(dplyr)
+  data(example_scan)
+  expect_equal(
+    select(example_scan, "DBZH") - select(example_scan, "VRADH"),
+    select(calculate_param(example_scan, DBZH = DBZH - VRADH), DBZH)
+  )
 })
