75187157fa5309dd5189b8a6401b048cb2c55b96
rafnuss <rafnuss@gmail.com>
2022-04-05 21:13:31 -0400

    Fix for date and lintr of the file.

--- R/download_pvolfiles.R
+++ R/download_pvolfiles.R
@@ -1,18 +1,17 @@
 #' Download polar volume (\code{pvol}) files from the NEXRAD archive
 #'
 #' Download a selection of polar volume (\code{pvol}) files from the
-#' \href{https://registry.opendata.aws/noaa-nexrad/}{NEXRAD Level II archive data}.
+#' \href{https://registry.opendata.aws/noaa-nexrad/}{NEXRAD Level II archive
+#'  data}.
 #'
-#' @param date_min character. YYYY-MM-DD start date of file selection. Days will
-#'   be ignored.
-#' @param date_max character. YYYY-MM-DD end date of file selection. Days will
-#'   be ignored.
-#' @param radars character (vector). 5-letter country/radar code(s)
-#'   (e.g. "bejab") of radars to include in file selection.
+#' @param date_min character. YYYY-MM-DD HH:MM start date of file selection.
+#' @param date_max character. YYYY-MM-DD HH:MM end date of file selection.
+#' @param radar character (vector). 5-letter radar code(s) (e.g. "KAMA")
 #' @param directory character. Path to local directory where files should be
-#'   downloaded and unzipped.
+#'   downloaded
 #' @param overwrite logical. TRUE for re-downloading and overwriting previously
 #'   downloaded files of the same names.
+#' @param bucket character. Bucket name to use.
 #'
 #' @export
 #'
@@ -23,22 +22,45 @@
 #' dir.create("~/bioRad_tmp_files")
 #' download_pvolfiles(
 #'   date_min = "2016-10-02 20:00",
-#'   date_max = "2016-10-03 06:00",
+#'   date_max = "2016-10-02 20:05",
 #'   radar = "KBBX",
 #'   directory = "~/bioRad_tmp_files",
 #'   overwrite = TRUE
 #' )
-download_pvolfiles <- function(date_min, date_max, radar, directory = ".",
-                             overwrite = FALSE, bucket = "noaa-nexrad-level2") {
+#' }
+download_pvolfiles <- function(date_min, date_max, radar,
+                               directory = ".", overwrite = FALSE,
+                               bucket = "noaa-nexrad-level2") {
 
   # Ensure directory exists
   assert_that(is.dir(directory))
 
   # Stop if radar codes are not exactly 5 characters
   assert_that(is.character(radar))
-  assert_that(length(radar)==1, msg = paste0("radar is not of length 1"))
-  possible_radar = c('KABR','KABX','KAKQ','KAMA','KAMX','KAPX','KARX','KATX','KBBX','KBGM','KBHX','KBIS','KBLX','KBMX','KBOX','KBRO','KBUF','KBYX','KCAE','KCBW','KCBX','KCCX','KCLE','KCLX','KCRP','KCXX','KCYS','KDAX','KDDC','KDFX','KDGX','KDIX','KDLH','KDMX','KDOX','KDTX','KDVN','KDYX','KEAX','KEMX','KENX','KEOX','KEPZ','KESX','KEVX','KEWX','KEYX','KFCX','KFDR','KFDX','KFFC','KFSD','KFSX','KFTG','KFWS','KGGW','KGJX','KGLD','KGRB','KGRK','KGRR','KGSP','KGWX','KGYX','KHDX','KHGX','KHNX','KHPX','KHTX','KICT','KICX','KILN','KILX','KIND','KINX','KIWA','KIWX','KJAN','KJAX','KJGX','KJKL','KLBB','KLCH','KLGX','KLIX','KLNX','KLOT','KLRX','KLSX','KLTX','KLVX','KLWX','KLZK','KMAF','KMAX','KMBX','KMHX','KMKX','KMLB','KMOB','KMPX','KMQT','KMRX','KMSX','KMTX','KMUX','KMVX','KMXX','KNKX','KNQA','KOAX','KOHX','KOKX','KOTX','KOUN','KPAH','KPBZ','KPDT','KPOE','KPUX','KRAX','KRGX','KRIW','KRLX','KRMX','KRTX','KSFX','KSGF','KSHV','KSJT','KSOX','KSRX','KTBW','KTFX','KTLH','KTLX','KTWX','KTYX','KUDX','KUEX','KVAX','KVBX','KVNX','KVTX','KVWX','KYUX')
-  assert_that(radar %in% possible_radar, msg = paste0("radar ", radar, " doesn't exist"))
+  assert_that(length(radar) == 1, msg = paste0("radar is not of length 1"))
+  possible_radar <- c("KABR", "KABX", "KAKQ", "KAMA", "KAMX", "KAPX", "KARX",
+                      "KATX", "KBBX", "KBGM", "KBHX", "KBIS", "KBLX", "KBMX",
+                      "KBOX", "KBRO", "KBUF", "KBYX", "KCAE", "KCBW", "KCBX",
+                      "KCCX", "KCLE", "KCLX", "KCRP", "KCXX", "KCYS", "KDAX",
+                      "KDDC", "KDFX", "KDGX", "KDIX", "KDLH", "KDMX", "KDOX",
+                      "KDTX", "KDVN", "KDYX", "KEAX", "KEMX", "KENX", "KEOX",
+                      "KEPZ", "KESX", "KEVX", "KEWX", "KEYX", "KFCX", "KFDR",
+                      "KFDX", "KFFC", "KFSD", "KFSX", "KFTG", "KFWS", "KGGW",
+                      "KGJX", "KGLD", "KGRB", "KGRK", "KGRR", "KGSP", "KGWX",
+                      "KGYX", "KHDX", "KHGX", "KHNX", "KHPX", "KHTX", "KICT",
+                      "KICX", "KILN", "KILX", "KIND", "KINX", "KIWA", "KIWX",
+                      "KJAN", "KJAX", "KJGX", "KJKL", "KLBB", "KLCH", "KLGX",
+                      "KLIX", "KLNX", "KLOT", "KLRX", "KLSX", "KLTX", "KLVX",
+                      "KLWX", "KLZK", "KMAF", "KMAX", "KMBX", "KMHX", "KMKX",
+                      "KMLB", "KMOB", "KMPX", "KMQT", "KMRX", "KMSX", "KMTX",
+                      "KMUX", "KMVX", "KMXX", "KNKX", "KNQA", "KOAX", "KOHX",
+                      "KOKX", "KOTX", "KOUN", "KPAH", "KPBZ", "KPDT", "KPOE",
+                      "KPUX", "KRAX", "KRGX", "KRIW", "KRLX", "KRMX", "KRTX",
+                      "KSFX", "KSGF", "KSHV", "KSJT", "KSOX", "KSRX", "KTBW",
+                      "KTFX", "KTLH", "KTLX", "KTWX", "KTYX", "KUDX", "KUEX",
+                      "KVAX", "KVBX", "KVNX", "KVTX", "KVWX", "KYUX")
+  assert_that(radar %in% possible_radar, msg = paste0("radar ", radar,
+                                                      " doesn't exist"))
 
   # Stop if dates are not a string
   assert_that(is.string(date_min))
@@ -49,7 +71,7 @@ download_pvolfiles <- function(date_min, date_max, radar, directory = ".",
   check_date_format(date_max, "%Y-%m-%d %H:%M")
 
   # Stop if overwrite is not a logical
-  assert_that(is.logical(overwrite), msg='overwrite is not a logical')
+  assert_that(is.logical(overwrite), msg = "overwrite is not a logical")
 
   # Create series of unique days yyyy-mm-dd based on date_min/max:
   dates <- seq(
@@ -57,45 +79,40 @@ download_pvolfiles <- function(date_min, date_max, radar, directory = ".",
     as.Date(substring(date_max, 1, 10), tz = NULL),
     by = "days"
   )
-  year_months <- format(dates, "%Y/%m")
 
   # Start download and unzipping
-  message(paste0("Downloading data from ",bucket))
+  message(paste0("Downloading data from ", bucket))
 
-  for (d in dates) {
-    # Create filename of format bejab201610.zip (removing _ and /)
-    file_name <- paste0(gsub("/", "", gsub("_", "", radar_year_month)), ".zip")
-    # Create filepath of format directory/bejab201610.zip
-    file_path <- file.path(directory, file_name)
+  for (i_d in seq_len(length(dates))) {
 
     # set prefix
-    prefix = paste(gsub("-","/",d) ,radar, "" , sep="/")
+    prefix <- paste(gsub("-", "/", dates[i_d]), radar, "", sep = "/")
 
     # Get bucket matching the request
-    bucket_df = aws.s3::get_bucket_df(bucket = bucket, prefix = prefix)
+    bucket_df <- aws.s3::get_bucket_df(bucket = bucket, prefix = prefix)
 
     # filter bucket with exact date
-    isWithin = sapply(bucket_df$Key,function(x){
-      dd = strptime(substring(x,21,35), format = "%Y%m%d_%H%M%S")
-      dd>=strptime(date_min, format = "%Y-%m-%d %H:%M") & dd<=strptime(date_max, format = "%Y-%m-%d %H:%M")
+    isWithin <- sapply(bucket_df$Key, function(x) {
+      dd <- strptime(substring(x, 21, 35), format = "%Y%m%d_%H%M%S")
+      dd >= strptime(date_min, format = "%Y-%m-%d %H:%M") &
+        dd <= strptime(date_max, format = "%Y-%m-%d %H:%M")
     })
-    bucket_df = bucket_df[isWithin, ]
+    bucket_df <- bucket_df[isWithin, ]
 
     # create progresbar
-    message(paste0("Downloading pvol for ",prefix))
-    pb = txtProgressBar(min = 0, max = nrow(bucket_df), initial = 0, style = 3)
+    message(paste0("Downloading pvol for ", prefix))
+    pb <- txtProgressBar(min = 0, max = nrow(bucket_df), initial = 0, style = 3)
 
     for (row in 1:nrow(bucket_df)) {
-
-      # Create url of format base_url/be/jab/2016/bejab201610.zip (removing month)
-      aws.s3::save_object(object = bucket_df$Key[row],
-                          bucket = bucket,
-                          file = paste(directory, bucket_df$Key[row], sep = "/"),
-                          overwite = overwrite)
-
-      setTxtProgressBar(pb,row)
+      # Save file
+      aws.s3::save_object(
+        object = bucket_df$Key[row],
+        bucket = bucket,
+        file = paste(directory, bucket_df$Key[row], sep = "/"),
+        overwite = overwrite
+      )
+      setTxtProgressBar(pb, row)
     }
-
   }
 }
 
