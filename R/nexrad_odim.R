7ca38c33e25a565b551d0e01500328a57a3d82ca
Adriaan Dokter <adriaandokter@gmail.com>
2022-06-07 10:44:10 -0400

    implement vol2birdR into nexrad_to_odim()

--- R/nexrad_odim.R
+++ R/nexrad_odim.R
@@ -1,6 +1,14 @@
 #' Convert a NEXRAD polar volume file to an ODIM polar volume file
 #'
-#' @param pvolfile_nexrad Polar volume input file in RSL format.
+#' @param pvolfile_nexrad Character (vector). Either a path to a single radar polar volume
+#'   (`pvol`) file containing multiple scans/sweeps, or multiple paths to scan
+#'   files containing a single scan/sweep. Or a single `pvol` object. The file data format should be either 1)
+#'   [ODIM](https://github.com/adokter/vol2bird/blob/master/doc/OPERA2014_O4_ODIM_H5-v2.2.pdf)
+#'    format, which is the implementation of the OPERA data information model in
+#'   the [HDF5](https://support.hdfgroup.org/HDF5/) format, 2) a format
+#'   supported by the [RSL
+#'   library](https://trmm-fc.gsfc.nasa.gov/trmm_gv/software/rsl/) or 3) Vaisala
+#'   IRIS (IRIS RAW) format.
 #' @param pvolfile_odim Filename for the polar volume in ODIM HDF5 format to be
 #' generated.
 #'
@@ -26,18 +34,15 @@
 #' # clean up
 #' file.remove("~/KBGM_example", "~/KBGM_example.h5")
 #' }
-nexrad_to_odim <- function(pvolfile_nexrad, pvolfile_odim, verbose = FALSE,
-                           mount = dirname(pvolfile_nexrad), local_install) {
-  if (!file.exists(dirname(pvolfile_odim))) {
-    stop(paste("output directory", dirname(pvolfile_odim), "not found"))
-  }
-  if (file.access(dirname(pvolfile_odim), 2) == -1) {
-    stop(paste("No write permission in directory", dirname(pvolfile_odim)))
-  }
-  pvol_tmp <- nexrad_to_odim_tempfile(pvolfile_nexrad, verbose, mount, local_install)
-  file.rename(pvol_tmp, pvolfile_odim)
+nexrad_to_odim <- function(pvolfile_nexrad, pvolfile_odim, verbose = FALSE) {
+  assert_that(dir.exists(dirname(pvolfile_odim)),msg=paste("output directory", dirname(pvolfile_odim), "not found"))
+  assert_that(is.writeable(dirname(pvolfile_odim)))
+
+  config <- vol2birdR::vol2bird_config()
+  vol2birdR::rsl2odim(pvolfile_nexrad, pvolfile_odim, config, verbose)
 }
 
+# FIXME: REMOVE EVENTUALLY!
 nexrad_to_odim_tempfile <- function(pvolfile, verbose = FALSE,
                                     mount = dirname(pvolfile), local_install) {
   # check input arguments
