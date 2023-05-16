181aa9f79cc3ba014688609aa7d64c5b3e6f6b5d
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-16 09:51:06 +0200

    fix for style

--- R/calculate_param.R
+++ R/calculate_param.R
@@ -55,7 +55,8 @@ calculate_param <- function(x, ...) {
 #' @export
 calculate_param.pvol <- function(x, ...) {
   assert_that(is.pvol(x))
-  x$scans <- do.call(lapply, list(x$scans, calculate_param.scan, substitute(list(...))))
+  x$scans <-
+    do.call(lapply, list(x$scans, calculate_param.scan, substitute(list(...))))
   return(x)
 }
 
@@ -74,7 +75,7 @@ calculate_param.ppi <- function(x, ...) {
     if ("" == (name[[i]])) {
       name[[i]] <- deparse(nn, width.cutoff = 250L)[1]
     }
-    x$data@data[,name[[i]]]<-newParam
+    x$data@data[, name[[i]]] <- newParam
   }
   return(x)
 }
@@ -85,11 +86,16 @@ calculate_param.ppi <- function(x, ...) {
 calculate_param.scan <- function(x, ...) {
   assert_that(is.scan(x))
   # check if all parameters are equal
-  attr_to_check<-c('class','radar','datetime','geo','dim')
-  for(i in attr_to_check){
-    lapply(x$params, function(param, i) assert_that(has_attr(param, i)),i=i)
-    if(length(x$params)!=1)
-      lapply(lapply(x$params[-1], attr, i), function(x,y) assert_that(are_equal(x,y)), y=attr(x$params[[1]], i))
+  attr_to_check <- c("class", "radar", "datetime", "geo", "dim")
+  for (i in attr_to_check) {
+    lapply(x$params, function(param, i) assert_that(has_attr(param, i)), i = i)
+    if (length(x$params) != 1) {
+      lapply(
+        lapply(x$params[-1], attr, i),
+        function(x, y) assert_that(are_equal(x, y)),
+        y = attr(x$params[[1]], i)
+      )
+    }
   }
   if (as.character(as.list(substitute(...))[[1L]]) == "list") {
     calc <- as.list(substitute(...))[-1L]
@@ -107,9 +113,9 @@ calculate_param.scan <- function(x, ...) {
     }
     attr(newParam, "param") <- name[[i]]
     # reassign attributes if they are lost in operation
-    for(j in attr_to_check) {
-      if(!has_attr(newParam,j)) {
-        attr(newParam,j) <- attr(x$params[[1]], j)
+    for (j in attr_to_check) {
+      if (!has_attr(newParam, j)) {
+        attr(newParam, j) <- attr(x$params[[1]], j)
       }
     }
     x$params[[name[[i]]]] <- newParam
