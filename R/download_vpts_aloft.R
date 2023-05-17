15f62f136e5628aaf1da42eb392d5b33a4a41902
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-17 10:04:31 +0200

    init function

---
+++ R/download_vpts_aloft.R
@@ -0,0 +1,24 @@
+#' Title
+#'
+#' @param date_min
+#' @param date_max
+#' @param radars
+#' @param directory
+#' @param overwrite
+#' @param format
+#' @param source
+#'
+#' @return
+#' @export
+#'
+#' @examples
+download_vpts_aloft <- function(date_min = NULL,
+                 date_max = NULL,
+                 radars = NULL,
+                 directory = ".",
+                 overwrite = FALSE,
+                 format = "csv", # also hdf5
+                 source = "baltrad", # also ecog-04003
+                 ) {
+
+}
