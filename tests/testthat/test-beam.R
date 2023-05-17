14f58c43fe9253dd558b40bce2e6b5d06f9be637
Peter Desmet <peter.desmet@inbo.be>
2023-05-17 14:36:14 +0200

    Rename/reorder some tests

--- tests/testthat/test-beam.R
+++ tests/testthat/test-beam.R
@@ -1,51 +1,77 @@
-test_that("beam functions return error on incorrect parameters", {
+test_that("beam_distance() returns error on incorrect parameters", {
   expect_error(beam_distance(), 'argument "range" is missing, with no default')
   expect_error(beam_distance(1), 'argument "elev" is missing, with no default')
   expect_error(beam_distance(1:2, 1:3))
-  expect_error(beam_distance(1, 2, k = "notANumeric"))
+  expect_error(beam_distance(1, 2, k = "not_a_double"))
   expect_error(beam_distance(1, 2, k = NA))
-  expect_error(beam_distance(1, 2, lat = "notANumeric"))
+  expect_error(beam_distance(1, 2, lat = "not_a_double"))
   expect_error(beam_distance(1, 2, lat = NA))
-  expect_error(beam_distance(1, 2, re = "notANumeric"))
+  expect_error(beam_distance(1, 2, re = "not_a_double"))
   expect_error(beam_distance(1, 2, re = NA))
-  expect_error(beam_distance(1, 2, rp = "notANumeric"))
+  expect_error(beam_distance(1, 2, rp = "not_a_double"))
   expect_error(beam_distance(1, 2, rp = NA))
   expect_error(beam_distance(1, 2, k = 1:2))
   expect_error(beam_distance(1, 2, lat = 1:2))
   expect_error(beam_distance(1, 2, re = 1:2))
   expect_error(beam_distance(1, 2, rp = 1:2))
+})
+
+test_that("beam_height() returns error on incorrect parameters", {
   expect_error(beam_height(), 'argument "range" is missing, with no default')
   expect_error(beam_height(1), 'argument "elev" is missing, with no default')
   expect_error(beam_height(1:2, 1:3))
-  expect_error(beam_height(1, 2, k = "notANumeric"))
+  expect_error(beam_height(1, 2, k = "not_a_double"))
   expect_error(beam_height(1, 2, k = NA))
-  expect_error(beam_height(1, 2, lat = "notANumeric"))
+  expect_error(beam_height(1, 2, lat = "not_a_double"))
   expect_error(beam_height(1, 2, lat = NA))
-  expect_error(beam_height(1, 2, re = "notANumeric"))
+  expect_error(beam_height(1, 2, re = "not_a_double"))
   expect_error(beam_height(1, 2, re = NA))
-  expect_error(beam_height(1, 2, rp = "notANumeric"))
+  expect_error(beam_height(1, 2, rp = "not_a_double"))
   expect_error(beam_height(1, 2, rp = NA))
   expect_error(beam_height(1, 2, k = 1:2))
   expect_error(beam_height(1, 2, lat = 1:2))
   expect_error(beam_height(1, 2, re = 1:2))
   expect_error(beam_height(1, 2, rp = 1:2))
+})
+
+test_that("beam_range() returns error on incorrect parameters", {
   expect_error(beam_range(), 'argument "distance" is missing, with no default')
   expect_error(beam_range(1), 'argument "elev" is missing, with no default')
   expect_error(beam_range(1:2, 1:3))
-  expect_error(beam_range(1, 2, k = "notANumeric"))
+  expect_error(beam_range(1, 2, k = "not_a_double"))
   expect_error(beam_range(1, 2, k = NA))
-  expect_error(beam_range(1, 2, lat = "notANumeric"))
+  expect_error(beam_range(1, 2, lat = "not_a_double"))
   expect_error(beam_range(1, 2, lat = NA))
-  expect_error(beam_range(1, 2, re = "notANumeric"))
+  expect_error(beam_range(1, 2, re = "not_a_double"))
   expect_error(beam_range(1, 2, re = NA))
-  expect_error(beam_range(1, 2, rp = "notANumeric"))
+  expect_error(beam_range(1, 2, rp = "not_a_double"))
   expect_error(beam_range(1, 2, rp = NA))
   expect_error(beam_range(1, 2, k = 1:2))
   expect_error(beam_range(1, 2, lat = 1:2))
   expect_error(beam_range(1, 2, re = 1:2))
   expect_error(beam_range(1, 2, rp = 1:2))
 })
-test_that("Values match for `beam_distance`", {
+
+test_that("beam_profile() returns error on incorrect parameters", {
+  expect_error(beam_profile(), 'argument "height" is missing, with no default')
+  expect_error(beam_profile(1), 'argument "distance" is missing, with no default')
+  expect_error(beam_profile(1, 1), 'argument "elev" is missing, with no default')
+  expect_error(beam_profile(1:2, 1:3,1))
+  expect_error(beam_profile(1, 2, k = "not_a_double"))
+  expect_error(beam_profile(1, 2, k = NA))
+  expect_error(beam_profile(1, 2, lat = "not_a_double"))
+  expect_error(beam_profile(1, 2, lat = NA))
+  expect_error(beam_profile(1, 2, re = "not_a_double"))
+  expect_error(beam_profile(1, 2, re = NA))
+  expect_error(beam_profile(1, 2, rp = "not_a_double"))
+  expect_error(beam_profile(1, 2, rp = NA))
+  expect_error(beam_profile(1, 2, k = 1:2))
+  expect_error(beam_profile(1, 2, lat = 1:2))
+  expect_error(beam_profile(1, 2, re = 1:2))
+  expect_error(beam_profile(1, 2, rp = 1:2))
+})
+
+test_that("beam_distance() returns expected values", {
   expect_equal(beam_distance(100000, 5), 99495.125492)
   expect_equal(beam_distance(100000 * 2:3, 5), c(198721.311636, 297648.555804))
   expect_equal(beam_distance(100000, 2:3), c(99885.897399, 99785.935245))
@@ -53,14 +79,18 @@ test_that("Values match for `beam_distance`", {
   expect_equal(beam_distance(100000, c(2:3, NA)), c(99885.897399, 99785.935245, NA))
   expect_equal(beam_distance(100000 * 1:2, c(2, 5)), c(99885.897399, 198721.311636))
 })
-test_that("Values match for `beam_height` and `beam_range`", {
-  expect_equal(beam_range(100000, c(2:3, NA)), c(100114.2999242, 100214.7013404, NA))
-  expect_equal(beam_range(100000 * 1:2, c(2, 5)), c(100114.2999242, 201290.5648873))
+
+test_that("beam_height() returns expected values", {
   expect_equal(beam_height(100000, c(2:3, NA)), c(4077.563707055, 5820.194448376, NA))
   expect_equal(beam_height(100000 * 1:2, c(2, 5)), c(4077.563707055, 19762.53126097))
 })
 
-test_that("Results as expected for changing arguments of `beam_distance`", {
+test_that("beam_range() returns expected values", {
+  expect_equal(beam_range(100000, c(2:3, NA)), c(100114.2999242, 100214.7013404, NA))
+  expect_equal(beam_range(100000 * 1:2, c(2, 5)), c(100114.2999242, 201290.5648873))
+})
+
+test_that("beam_distance() returns expected values for different input", {
   expect_lt(beam_distance(100000, 6), beam_distance(100000, 5))
   expect_lt(beam_distance(100000, 5), beam_distance(100000, 5, rp = 7000))
   expect_lt(beam_distance(100000, 5), beam_distance(100000, 5, re = 7000))
@@ -70,7 +100,8 @@ test_that("Results as expected for changing arguments of `beam_distance`", {
     beam_distance(100000, 5, re = 7000, rp = 7000, lat = 50)
   )
 })
-test_that("Results as expected for changing arguments of `beam_range`", {
+
+test_that("beam_range() returns expected values for different input", {
   expect_gt(beam_range(100000, 6), beam_range(100000, 5))
   expect_gt(beam_range(100000, 5), beam_range(100000, 5, rp = 7000))
   expect_gt(beam_range(100000, 5), beam_range(100000, 5, re = 7000))
@@ -80,33 +111,14 @@ test_that("Results as expected for changing arguments of `beam_range`", {
     beam_range(100000, 5, re = 7000, rp = 7000, lat = 50)
   )
 })
-test_that("Round trip `beam_range` and `beam_distance`", {
+
+test_that("Distance can be calculated from range and vice versa", {
   expect_equal(beam_range(beam_distance(90000 * 1:2, elev = 3), elev = 3), 90000 * 1:2)
   expect_equal(beam_range(beam_distance(90000 * 1:2, elev = 3:4), elev = 3:4), 90000 * 1:2)
 })
 
-test_that("Beam over earth no height gain", {
+test_that("Distance equals range when beam is following the earth (elev = 0)", {
   expect_equal(beam_height(10000, 0, k = 10e7), 0)
   expect_equal(beam_distance(10000, 0, k = 10e7), 10000, tolerance = .000001)
   expect_equal(beam_range(10000, 0, k = 10e7), 10000, tolerance = .000001)
-})
-
-test_that("`beam_profile_overlap` fails as expected",{
-  expect_error(beam_profile(), 'argument "height" is missing, with no default')
-  expect_error(beam_profile(1), 'argument "distance" is missing, with no default')
-  expect_error(beam_profile(1, 1), 'argument "elev" is missing, with no default')
-  expect_error(beam_profile(1:2, 1:3,1))
-  expect_error(beam_profile(1, 2, k = "notANumeric"))
-  expect_error(beam_profile(1, 2, k = NA))
-  expect_error(beam_profile(1, 2, lat = "notANumeric"))
-  expect_error(beam_profile(1, 2, lat = NA))
-  expect_error(beam_profile(1, 2, re = "notANumeric"))
-  expect_error(beam_profile(1, 2, re = NA))
-  expect_error(beam_profile(1, 2, rp = "notANumeric"))
-  expect_error(beam_profile(1, 2, rp = NA))
-  expect_error(beam_profile(1, 2, k = 1:2))
-  expect_error(beam_profile(1, 2, lat = 1:2))
-  expect_error(beam_profile(1, 2, re = 1:2))
-  expect_error(beam_profile(1, 2, rp = 1:2))
-
 })
