98d67d07423b17f3f00cb801bde174a8812a974a
bart1 <1662852+bart1@users.noreply.github.com>
2023-05-17 16:16:31 +0200

    Update hooks.R
    Installation instructions are now with `check_installed`

--- R/hooks.R
+++ R/hooks.R
@@ -1,6 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   register_all_s3_methods() # dynamically registers non-imported pkgs (tidyverse) # nocov
-  #  assert_that(is.string(find.package("vol2birdR", verbose=F, quiet=T)),msg="package vol2birdR not found. See https://adokter.github.io/vol2birdR/ for install instructions")
 }
 
 .onAttach <- function(libname, pkgname) {
