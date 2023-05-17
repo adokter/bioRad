bb0ff4efb6ec34e62fd73dd27fcaba2e600fe866
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-17 15:15:52 +0200

    Check output type

--- tests/testthat/test-list_vpts_aloft.R
+++ tests/testthat/test-list_vpts_aloft.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("list_vpts_aloft() returns a character vector", {
+  expect_type(
+    list_vpts_aloft(date_min = "1990-01-01",
+                    date_max = "2050-01-01",
+                    radars = c("bejab","bewid")),
+    "character"
+  )
+})
+
+test_that("list_vpts_aloft() returns all data when no dates are provided", {
+
 })
