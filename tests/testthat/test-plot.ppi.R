5af2ae6fd3a9904b9df73c79bc06501163c0fc54
Cecilia Nilsson <cecilia.nilsson@biol.lu.se>
2023-05-17 15:08:01 +0200

    add snapshots to plot.ppi

--- tests/testthat/test-plot.ppi.R
+++ tests/testthat/test-plot.ppi.R
@@ -28,3 +28,36 @@ test_that("plot.ppi() uses param argument", {
 test_that("length does not differ", {
   expect_equal(length(example.plot$data$VRADH), length(ppi$data$VRADH))
 })
+
+test_that("plot.ppi() creates expected graph", {
+  # On first run: creates a snapshot, stored under testthat/_snap/function_name.
+  # On consecutive runs: creates new snapshot and checks that it looks identical
+  # to previously saved snapshot. If it fails, a warning is given and the new
+  # snapshot is stored with ".new" appended to name for manual inspection.
+
+  vdiffr::expect_doppelganger(
+    "example_plot_DBZH",
+    plot.ppi(ppi, param = "DBZH")
+  )
+
+  vdiffr::expect_doppelganger(
+    "example_plot_VRADH",
+    plot.ppi(ppi, param = "VRADH")
+  )
+
+  vdiffr::expect_doppelganger(
+    "example_plot_RHOHV",
+    plot.ppi(ppi, param = "RHOHV")
+  )
+
+  vdiffr::expect_doppelganger(
+    "example_plot_PHIDP",
+    plot.ppi(ppi, param = "PHIDP")
+  )
+
+  vdiffr::expect_doppelganger(
+    "example_plot_ZDR",
+    plot.ppi(ppi, param = "ZDR")
+  )
+})
+
