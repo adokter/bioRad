ae0c3486cd81fb2990bd3c49e8797b0a0f75a298
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-17 15:24:45 +0200

    add test headers

--- tests/testthat/test-list_vpts_aloft.R
+++ tests/testthat/test-list_vpts_aloft.R
@@ -1,3 +1,15 @@
+test_that("list_vpts_aloft() returns error for unknown source", {
+
+})
+
+test_that("list_vpts_aloft() returns error for invalid format", {
+
+})
+
+test_that("list_vpts_aloft() returns error if radar doesn't exist", {
+
+})
+
 test_that("list_vpts_aloft() returns a character vector", {
   expect_type(
     list_vpts_aloft(date_min = "1990-01-01",
