2bbb5f8801d12871d20102339276651ebcadf25a
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-23 14:49:12 +0200

    don't use `letters` in test

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -1,4 +1,10 @@
 test_that("extract_string() can extract a string from a vector", {
-  expect_true(all(sapply(letters, function(pattern)
-    extract_string(letters, pattern) == pattern)))
+  expect_identical(
+    extract_string(
+      "These are the voyages of the starship Enterprise",
+      "[a-z]{8}(?= Enterprise)",
+      perl = TRUE
+    ),
+    "starship"
+  )
 })
