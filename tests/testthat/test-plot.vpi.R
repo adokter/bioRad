f0503defd7a5f45f45b56fe1c6c0575e6a94256d
Cecilia Nilsson <cecilia.nilsson@biol.lu.se>
2023-05-26 13:37:21 +0200

    Update test-plot.vpi.R
    remove snapshot testing and add test for recorded plot object

--- tests/testthat/test-plot.vpi.R
+++ tests/testthat/test-plot.vpi.R
@@ -1,7 +1,8 @@
 example_vpi <- integrate_profile(example_vpts)
 
-test_that("plot() returns error on incorrect parameters", {
-  expect_error(plot("not_a_vpi"))
+test_that("plot.vpi() returns error on incorrect parameters", {
+  # use plot.vpts() to avoid defaulting to base plot()
+  expect_error(plot.vpi("not_a_vpi"))
   expect_error(plot(example_vpi, quantity = "not_a_quantity"))
 
   # Test error on "param" instead of "quantity"
@@ -12,54 +13,7 @@ test_that("plot() returns error on incorrect parameters", {
   expect_warning(plot(example_vpi, line.lwd = 1))
   })
 
- test_that("plot() creates expected graph", {
-   # On first run: creates a snapshot, stored under testthat/_snap/function_name.
-   # On consecutive runs: creates new snapshot and checks that it looks identical
-   # to previously saved snapshot. If it fails, a warning is given and the new
-   # snapshot is stored with ".new" appended to name for manual inspection.
+test_that("plot.vpi() produces a plot", {
+  expect_s3_class(recordPlot(plot(example_vpi)), "recordedplot")
 
-   vdiffr::expect_doppelganger(
-     "example_plot_mtr",
-      plot(example_vpi, quantity = "mtr")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_rtr",
-     plot(example_vpi, quantity = "rtr")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_vid",
-      plot(example_vpi, quantity = "vid")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_vir",
-     plot(example_vpi,quantity = "vir")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_mt",
-     plot(example_vpi, quantity = "mt")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_rt",
-     plot(example_vpi, quantity = "rt")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_ff",
-     plot(example_vpi, quantity = "ff")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_dd",
-     plot(example_vpi, quantity = "dd")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_u",
-     plot(example_vpi, quantity = "u")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_v",
-     plot(example_vpi, quantity = "v")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_height",
-     plot(example_vpi, quantity = "height")
-   )
- })
+})
