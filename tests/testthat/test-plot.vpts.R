6177bdff505bb4a189dd35d73cbd72d1b0c03803
Cecilia Nilsson <cecilia.nilsson@biol.lu.se>
2023-05-26 13:25:21 +0200

    Update test-plot.vpts.R
    Removed snapshot tests, added a test that a "recordedplot" is being produced and some adjustments to documentation

--- tests/testthat/test-plot.vpts.R
+++ tests/testthat/test-plot.vpts.R
@@ -1,12 +1,15 @@
 example_vpts_reg <- regularize_vpts(example_vpts)
 
-test_that("plot() returns error on incorrect parameters", {
-  expect_error(plot("not_a_vpts"))
+test_that("plot.vpts() returns error on incorrect parameters", {
+  # use plot.vpts() to avoid defaulting to base plot()
+  expect_error(plot.vpts("not_a_vpts"))
   expect_error(plot(example_vpts_reg, quantity = "not_a_quantity"))
 
   # Test error on "param" instead of "quantity"
   expect_error(plot(example_vpts_reg, param = "dens"))
+})
 
+test_that("plot.vpts() warns for certain datatypes", {
   # Test warning for not regularized vpts
   expect_warning(
     plot(example_vpts, quantity = "dens"),
@@ -20,47 +23,16 @@ test_that("plot() returns error on incorrect parameters", {
     plot(example_vpts_dup, quantity = "dens"),
     "duplicate datetime values"
   )
+})
 
-  # Test warning for deprecated arguments
+test_that("plot.vpts() warns for deprecated arguments", {
   expect_warning(plot(example_vpts_reg, quantity = "dens", barbs.h = 1))
   expect_warning(plot(example_vpts_reg, quantity = "dens", barbs.t = 1))
   expect_warning(plot(example_vpts_reg, quantity = "dens", barbs.dens = 1))
   expect_warning(plot(example_vpts_reg, quantity = "dens", legend.ticks = 1))
 })
 
-test_that("plot() creates expected graph", {
-   # On first run: creates a snapshot, stored under testthat/_snap/function_name.
-   # On consecutive runs: creates new snapshot and checks that it looks identical
-   # to previously saved snapshot. If it fails, a warning is given and the new
-   # snapshot is stored with ".new" appended to name for manual inspection.
+test_that("plot.vpts() produces a plot", {
+  expect_s3_class(recordPlot(plot(example_vpts_reg)), "recordedplot")
 
-   vdiffr::expect_doppelganger(
-     "example_plot_dens",
-     plot(example_vpts_reg, quantity = "dens")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_eta",
-     plot(example_vpts_reg, quantity = "eta")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_dbz",
-     plot(example_vpts_reg, quantity = "dbz")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_DBZH",
-     plot(example_vpts_reg,quantity = "DBZH")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_ff",
-     plot(example_vpts_reg,quantity = "ff")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_u",
-     plot(example_vpts_reg,quantity = "u")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_v",
-     plot(example_vpts_reg,quantity = "v")
-   )
-
- })
+})
