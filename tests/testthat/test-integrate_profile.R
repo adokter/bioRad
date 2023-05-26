1251657a5685fbfa2d3cc1db0d08dc9150a6f4f4
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-26 14:35:26 +0200

    test list method error

--- tests/testthat/test-integrate_profile.R
+++ tests/testthat/test-integrate_profile.R
@@ -13,6 +13,10 @@ test_that("integrate_profile() returns error on incorrect parameters", {
   expect_error(integrate_profile(example_vp, alt_min = 500, alt_max = 300))
   expect_error(integrate_profile(example_vp, height_quantile = "a"))
   expect_error(integrate_profile(example_vp, height_quantile = 2))
+test_that("integrate_profile() returns error on non vp object in list input",{
+  expect_error(integrate_profile(x = list(example_vp, example_vpts)),
+               regexp = "requires list of vp objects as input",
+               fixed = TRUE)
 })
 
 test_that("integrate_profile() checks value of interval_max", {
@@ -24,3 +28,4 @@ test_that("rotating transect by 180 degrees results in sign flip", {
   expect_equal(integrate_profile(example_vp, alpha=30)$mtr,-integrate_profile(example_vp, alpha=30+180)$mtr)
   expect_equal(integrate_profile(example_vpts, alpha=0)$mt,-integrate_profile(example_vpts, alpha=180)$mt)
 })
+
