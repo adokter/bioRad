e6160df664c5d68311d6a95666e21c0d21e099aa
Cecilia Nilsson <cecilia.nilsson@biol.lu.se>
2023-05-26 12:18:49 +0200

    Update test-plot.scan.R
    remove all snapshot testing, fine tuning of some of the other tests

--- tests/testthat/test-plot.scan.R
+++ tests/testthat/test-plot.scan.R
@@ -1,57 +1,38 @@
 data(example_scan)
-example.plot <- plot(example_scan, param = "VRADH", zlim = c(-5, 5))
+example.plot <- plot(example_scan, param = "VRADH")
 
-test_that("plot() returns error on incorrect parameters", {
-  expect_error(plot("not_a_scan"))
+test_that("plot.scan() returns error on incorrect parameters", {
+  # use plot.scan() to avoid defaulting to base plot()
+  expect_error(plot.scan("not_a_scan"))
   expect_error(plot(example_scan, param = "not_a_param"))
   expect_error(plot(example_scan, quantity = "DBZH"))
 })
 
-test_that("plot() defaults to DBZH on missing param", {
+test_that("plot.scan() defaults to DBZH on missing param", {
   expect_equal(plot(example_scan), plot(example_scan, param = "DBZH"))
 })
 
-test_that("plot() returns ggplot object", {
+test_that("plot.scan() returns ggplot object", {
   expect_true(all(class(plot(example_scan)) == c("gg", "ggplot")))
 })
 
-test_that("plot() takes zlim arguments", {
-  expect_true(all(example.plot$data$VRADH >= -5, na.rm = TRUE))
-  expect_true(all(example.plot$data$VRADH <= 5, na.rm = TRUE))
+test_that("plot.scan() uses the default zlim from color_scale", {
+  expect_equal(range(example.plot$data$VRADH, na.rm = TRUE), get_zlim("VRADH", zlim))
+
 })
 
-test_that("plot() uses param argument", {
+test_that("plot.scan() takes zlim argument", {
+  example.plot_zlim <- plot(example_scan, param = "VRADH", zlim = c(-5, 5))
+  expect_equal(range(example.plot_zlim$data$VRADH, na.rm = TRUE), c(-5, 5))
+
+})
+
+test_that("plot.scan() uses param argument", {
   expect_true("VRADH" %in% names(example.plot$data))
+
 })
 
-test_that("plot() returns as much data as in the scan", {
+test_that("plot.scan() returns as much data as in the scan", {
   expect_equal(length(example.plot$data$VRADH), length(example_scan$params$VRADH))
+
 })
-
- test_that("plot() creates expected graph", {
-   # On first run: creates a snapshot, stored under testthat/_snap/function_name.
-   # On consecutive runs: creates new snapshot and checks that it looks identical
-   # to previously saved snapshot. If it fails, a warning is given and the new
-   # snapshot is stored with ".new" appended to name for manual inspection.
-
-   vdiffr::expect_doppelganger(
-     "example_plot_DBZH",
-     plot(example_scan, param = "DBZH")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_VRADH",
-     plot(example_scan, param = "VRADH")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_RHOHV",
-     plot(example_scan, param = "RHOHV")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_PHIDP",
-     plot(example_scan, param = "PHIDP")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_ZDR",
-     plot(example_scan, param = "ZDR")
-   )
- })
