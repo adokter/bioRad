f579f8c990438fe2cad1dbf697cbfe39d671e37c
Cecilia Nilsson <cecilia.nilsson@biol.lu.se>
2023-05-26 13:45:08 +0200

    Update test-plot.vp.R
    Remove snapshot tests, add test for recorded plot

--- tests/testthat/test-plot.vp.R
+++ tests/testthat/test-plot.vp.R
@@ -1,5 +1,6 @@
 test_that("plot.vp() returns error on incorrect parameters", {
-  expect_error(plot("not_a_vp"))
+  # use plot.vp() to avoid defaulting to base plot()
+  expect_error(plot.vp("not_a_vp"))
   expect_error(plot(example_vp, quantity = "not_a_quantity"))
 
   # Test error on "param" instead of "quantity"
@@ -10,34 +11,8 @@ test_that("plot.vp() returns error on incorrect parameters", {
   expect_warning(plot(example_vp, line.lwd = 1))
 })
 
- test_that("plot.vp() creates expected graph", {
-   # On first run: creates a snapshot, stored under testthat/_snap/function_name.
-   # On consecutive runs: creates new snapshot and checks that it looks identical
-   # to previously saved snapshot. If it fails, a warning is given and the new
-   # snapshot is stored with ".new" appended to name for manual inspection.
 
-   vdiffr::expect_doppelganger(
-     "example_plot_ff",
-     plot(example_vp, quantity = "ff")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_dd",
-     plot(example_vp, quantity = "dd")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_dens",
-     plot(example_vp, quantity = "dens")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_eta",
-     plot(example_vp, quantity = "eta")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_dbz",
-     plot(example_vp, quantity = "dbz")
-   )
-   vdiffr::expect_doppelganger(
-     "example_plot_DBZH",
-     plot(example_vp, quantity = "DBZH")
-   )
- })
+test_that("plot.vp() produces a plot", {
+  expect_s3_class(recordPlot(plot(example_vp)), "recordedplot")
+
+})
