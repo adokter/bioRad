8c5742f5e6ff8498447f081b6bbbe0b37f53351d
iskandari <sasha.tedeschi@gmail.com>
2023-05-30 14:35:43 -0400

    added uniform file extension test

--- tests/testthat/test-read_vpts.R
+++ tests/testthat/test-read_vpts.R
@@ -4,11 +4,18 @@ hdf5_local_other_radar <- ""
 vpts_gz_remote_1 <- "https://aloft.s3-eu-west-1.amazonaws.com/baltrad/monthly/bejab/2023/bejab_vpts_202303.csv.gz"
 vpts_gz_remote_2 <- "https://aloft.s3-eu-west-1.amazonaws.com/baltrad/monthly/bejab/2023/bejab_vpts_202304.csv.gz"
 vpts_gz_other_radar <- "https://aloft.s3-eu-west-1.amazonaws.com/baltrad/monthly/bewid/2023/bewid_vpts_202303.csv.gz"
+
+
 # TODO: create the other vpts files from the files above, by downloading and gzipping
 
 test_that("read_vpts() returns error on incorrect parameters", {
-
+  # Prepare a vector of file paths with different extensions
+  files <- c("file1.csv", "file2.gz")
+  
+  # Expect an error when calling read_vpts() with this input
+  expect_error(read_vpts(files), "`files` must all have the same extension.")
 })
+
 
 test_that("read_vpts() returns error on mixed extensions", {
 
