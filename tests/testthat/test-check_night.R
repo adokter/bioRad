87fd9ee692081e8a1075305ce36e12503bd7dbde
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-30 14:08:33 +0200

    `expect_is()` is superseded in testthat3e

--- tests/testthat/test-check_night.R
+++ tests/testthat/test-check_night.R
@@ -10,10 +10,10 @@ test_that("check_night identifies night with numeric values", {
 })
 
 test_that("check_night identifies day/night for example data", {
-  expect_is(check_night(example_vp), "logical")
+  expect_type(check_night(example_vp), "logical")
   expect_true(check_night(example_vp))
 
-  expect_is(check_night(example_vpts), "logical")
+  expect_type(check_night(example_vpts), "logical")
   expect_true(check_night(example_vpts)[1])
   expect_false(check_night(example_vpts)[145])
   check_night(integrate_profile(example_vp))
