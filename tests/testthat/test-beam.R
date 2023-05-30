e3ac82aad39d4455b8094a078d69c6893d7708dd
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-30 16:57:48 +0200

    add tests for incorrect arguments in beam_profile_overlap()

--- tests/testthat/test-beam.R
+++ tests/testthat/test-beam.R
@@ -1,24 +1,67 @@
 test_that("beam_distance() returns error on incorrect parameters", {
-  expect_error(beam_distance(), 'argument "range" is missing, with no default')
-  expect_error(beam_distance(1), 'argument "elev" is missing, with no default')
-  expect_error(beam_distance(1:2, 1:3))
-  expect_error(beam_distance(1, 2, k = "not_a_double"))
-  expect_error(beam_distance(1, 2, k = NA))
-  expect_error(beam_distance(1, 2, lat = "not_a_double"))
-  expect_error(beam_distance(1, 2, lat = NA))
-  expect_error(beam_distance(1, 2, re = "not_a_double"))
-  expect_error(beam_distance(1, 2, re = NA))
-  expect_error(beam_distance(1, 2, rp = "not_a_double"))
-  expect_error(beam_distance(1, 2, rp = NA))
-  expect_error(beam_distance(1, 2, k = 1:2))
-  expect_error(beam_distance(1, 2, lat = 1:2))
-  expect_error(beam_distance(1, 2, re = 1:2))
-  expect_error(beam_distance(1, 2, rp = 1:2))
+  expect_error(
+    beam_distance(),
+    regexp = 'argument "range" is missing, with no default',
+    fixed = TRUE)
+  expect_error(
+    beam_distance(1),
+    regexp = 'argument "elev" is missing, with no default',
+    fixed = TRUE)
+  expect_error(
+    beam_distance(1:2, 1:3),
+    regex = "`range` and `elev` should either be equal length or either should have a length of one",
+    fixed = TRUE)
+  expect_error(
+    beam_distance(1, 2, k = "not_a_double"),
+    regexp = "k is not a number (a length one numeric vector).",
+    fixed = TRUE)
+  expect_error(
+    beam_distance(1, 2, k = NA),
+    regexp = "k is not a number (a length one numeric vector).",
+    fixed = TRUE)
+  expect_error(
+    beam_distance(1, 2, lat = "not_a_double"))
+  expect_error(
+    beam_distance(1, 2, lat = NA))
+  expect_error(
+    beam_distance(1, 2, re = "not_a_double"))
+  expect_error(
+    beam_distance(1, 2, re = NA),
+    regexp = "re is not a number (a length one numeric vector).",
+    fixed = TRUE)
+  expect_error(
+    beam_distance(1, 2, rp = "not_a_double"))
+  expect_error(
+    beam_distance(1, 2, rp = NA),
+    regexp = "rp is not a number (a length one numeric vector).",
+    fixed = TRUE)
+  expect_error(
+    beam_distance(1, 2, k = 1:2),
+    regexp = "k is not a number (a length one numeric vector).",
+    fixed = TRUE)
+  expect_error(
+    beam_distance(1, 2, lat = 1:2),
+    regexp = "lat is not a number (a length one numeric vector).",
+    fixed = TRUE)
+  expect_error(
+    beam_distance(1, 2, re = 1:2),
+    regexp = "re is not a number (a length one numeric vector).",
+    fixed = TRUE)
+  expect_error(
+    beam_distance(1, 2, rp = 1:2),
+    regexp = "rp is not a number (a length one numeric vector).",
+    fixed = TRUE)
 })
 
 test_that("beam_height() returns error on incorrect parameters", {
-  expect_error(beam_height(), 'argument "range" is missing, with no default')
-  expect_error(beam_height(1), 'argument "elev" is missing, with no default')
+  expect_error(
+    beam_height(),
+    regexp = 'argument "range" is missing, with no default',
+    fixed = TRUE)
+  expect_error(
+    beam_height(1),
+    regexp = 'argument "elev" is missing, with no default',
+    fixed = TRUE)
   expect_error(beam_height(1:2, 1:3))
   expect_error(beam_height(1, 2, k = "not_a_double"))
   expect_error(beam_height(1, 2, k = NA))
@@ -121,4 +164,194 @@ test_that("Distance equals range when beam is following the earth (elev = 0)", {
   expect_equal(beam_height(10000, 0, k = 10e7), 0)
   expect_equal(beam_distance(10000, 0, k = 10e7), 10000, tolerance = .000001)
   expect_equal(beam_range(10000, 0, k = 10e7), 10000, tolerance = .000001)
+})
+
+test_that("beam_profile_overlap() returns error on incorrect parameters", {
+  expect_error(
+    beam_profile_overlap(example_vp),
+    regexp = 'argument "elev" is missing, with no default',
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(example_vp, elev = "a"),
+    regexp = "'elev' must be a number or numeric vector with the beam elevation(s)",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap('a', elev = c(1:3)),
+    regexp = "'vp' must be an object of class vp",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(example_vp, elev = 1:4, distance = c(-2:4)),
+    regexp = "'distance' should be a positive numeric value or vector",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(example_vp, elev = 1:4, distance = "a"),
+    regexp = "'distance' should be a positive numeric value or vector",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp,
+      elev = 1:4,
+      distance = 90:100,
+      zlim = c(1, 9, 14)
+    ),
+    regexp = "'zlim' should be a numeric vector of length two",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp,
+      elev = 1:4,
+      distance = 90:100,
+      zlim = c(1, "a", 14)
+    ),
+    regexp = "'zlim' should be a numeric vector of length two",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp,
+      elev = 1:4,
+      distance = 90:100,
+      zlim = 12
+    ),
+    regexp = "'zlim' should be a numeric vector of length two",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp,
+      elev = 1:4,
+      distance = 90:100,
+      zlim = c("a","b")
+    ),
+    regexp = "'zlim' should be a numeric vector of length two",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp,
+      elev = 1:4,
+      distance = 90:100,
+      zlim = "a"
+    ),
+    regexp = "'zlim' should be a numeric vector of length two",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp,
+      elev = 1:4,
+      distance = 90:100,
+      zlim = c(NA, 1)
+    ),
+    regexp = "'zlim' should be a vector with two numeric values for upper and lower bound",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp,
+      elev = 1:4,
+      distance = 90:100,
+      zlim = c(2, NA)
+    ),
+    regexp = "'zlim' should be a vector with two numeric values for upper and lower bound",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp,
+      elev = 1:4,
+      distance = 90:100,
+      zlim = c(42, 1)
+    ),
+    regexp = "'zlim' should be a vector with two numeric values for upper and lower bound",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp,
+      elev = 1:4,
+      distance = 80:90,
+      steps = c(4,2)
+    ),
+    regexp = "'step' should be a numeric value",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp,
+      elev = 1:4,
+      distance = 80:90,
+      steps = "a"
+    ),
+    regexp = "'step' should be a numeric value",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp,
+      elev = 1:4,
+      distance = 70:80,
+      quantity = "not_a_valid_quanity"
+    ),
+    regexp = "'quantity' should be one of 'dens' or 'eta'",
+    fixed = TRUE
+  )
+  example_vp_no_antenna <- example_vp
+  example_vp_no_antenna$attributes$where$height <- NULL
+  expect_error(
+    beam_profile_overlap(
+      example_vp_no_antenna,
+      elev = 1:4,
+      distance = 1:3
+    ),
+    regexp = "antenna height cannot be found in polar volume, specify antenna height using 'antenna' argument",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp_no_antenna,
+      elev = 1:4,
+      distance = 1:3,
+      antenna = c(4,2)
+    ),
+    regexp = "antenna is not a number (a length one numeric vector).",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp_no_antenna,
+      elev = 1:4,
+      distance = 1:3,
+      antenna = "not_a_number"
+    ),
+    regexp = "antenna is not a number (a length one numeric vector).",
+    fixed = TRUE
+  )
+  example_vp_no_lat <- example_vp
+  example_vp_no_lat$attributes$where$lat <- NULL
+  expect_error(
+    beam_profile_overlap(
+      example_vp_no_lat,
+      elev = 1:4,
+      distance = 1:3
+    ),
+    regexp = "radar latitude cannot be found in polar volume, specify using 'lat' argument",
+    fixed = TRUE
+  )
+  expect_error(
+    beam_profile_overlap(
+      example_vp_no_lat,
+      elev = 1:4,
+      distance = 1:3,
+      lat = "b"
+    ),
+    regexp = "lat is not a number (a length one numeric vector).",
+    fixed = TRUE
+  )
 })
