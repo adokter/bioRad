1cbae70d741a3a61192e0fc08403f8acab561483
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-30 09:36:38 +0200

    test palette and barb helper error messages

--- tests/testthat/test-plot.vpts.R
+++ tests/testthat/test-plot.vpts.R
@@ -28,6 +28,70 @@ test_that("plot.vpts() returns error on incorrect parameters", {
   expect_warning(plot.vpts(example_vpts_reg, quantity = "dens", legend.ticks = 1))
 })
 
+test_that("plot.vpts() returns error on bad palette", {
+  expect_error(
+    plot(regularize_vpts(example_vpts), palette = 123),
+    regexp = "palette should be a character vector with hex color values",
+    fixed = TRUE
+  )
+})
+
+test_that("plot_wind_barbs() returns error on incorrect parameters", {
+  expect_error(
+    plot_wind_barbs(c(1, 2), c(4, 5, 8)),
+    regexp = "X AND Y COORDINATES SHOULD HAVE SAME LENGTH!",
+    fixed = TRUE
+  )
+
+  variable_error_message <-
+    "ALL VARIABLES SHOULD HAVE SAME LENGTH AS COORDINATES, OR BE MISSING!!!"
+
+  expect_error(
+    plot_wind_barbs(c(1, 2), c(4, 5)),
+    regexp = variable_error_message,
+    fixed = TRUE
+  )
+  expect_error(
+    plot_wind_barbs(c(1, 2), c(4, 5),
+                    speed = 46,
+                    direction = NA),
+    regexp = variable_error_message,
+    fixed = TRUE
+  )
+  expect_error(
+    plot_wind_barbs(c(1, 2), c(4, 5),
+                    speed = NA,
+                    direction = NA,
+                    fill = "red"),
+    regexp = variable_error_message,
+    fixed = TRUE
+  )
+  expect_error(
+    plot_wind_barbs(c(1, 2), c(4, 5),
+                    speed = NA,
+                    direction = c(4,98,2,42),
+                    fill = NA),
+    regexp = variable_error_message,
+    fixed = TRUE
+  )
+  expect_error(
+    plot_wind_barbs(c(1, 2), c(4, 5),
+                    speed = c(4,98,2,42),
+                    direction = NA,
+                    fill = NA),
+    regexp = variable_error_message,
+    fixed = TRUE
+  )
+  expect_error(
+    plot_wind_barbs(c(1, 2), c(4, 5),
+                    speed = NA,
+                    direction = NA,
+                    fill = c("slategray2","wheat3","tomato")),
+    regexp = variable_error_message,
+    fixed = TRUE
+  )
+})
+
 # test_that("plot.vpts() creates expected graph", {
 #   # On first run: creates a snapshot, stored under testthat/_snap/function_name.
 #   # On consecutive runs: creates new snapshot and checks that it looks identical
