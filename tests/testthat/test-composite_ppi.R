b1f73990aac2e6ec3f6c047df967f5b530f69375
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-31 11:47:53 +0200

    add tests for error messages in read_pvolfile()

--- tests/testthat/test-composite_ppi.R
+++ tests/testthat/test-composite_ppi.R
@@ -1,3 +1,29 @@
 test_that("composite_ppi() returns error on incorrect parameters", {
-
+  pvol <- read_pvolfile(system.file("extdata", "volume.h5", package = "bioRad"))
+  ppis <- lapply(pvol$scans, project_as_ppi)
+  expect_error(
+    composite_ppi(example_vp),
+    regexp = "'composite' expects objects of class ppi only",
+    fixed = TRUE
+  )
+  expect_error(
+    composite_ppi(ppis[2:3], nx = "a"),
+    regexp = "'nx' should be an integer",
+    fixed = TRUE
+  )
+  expect_error(
+    composite_ppi(ppis[2:3], ny = "a"),
+    regexp = "'ny' should be an integer",
+    fixed = TRUE
+  )
+  expect_error(
+    composite_ppi(ppis[2:3], xlim = "a"),
+    regexp = "'xlim' should be a numeric vector of length two",
+    fixed = TRUE
+  )
+  expect_error(
+    composite_ppi(ppis[2:3], xlim = c(1,9,4)),
+    regexp = "'xlim' should be a numeric vector of length two",
+    fixed = TRUE
+  )
 })
