74ca92145a927c94a9a6dfbe114637f068154d27
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-31 11:37:23 +0200

    add basic tests for error messages to colour_scale() and add_color_transparancy()

--- tests/testthat/test-color_scale.R
+++ tests/testthat/test-color_scale.R
@@ -1,3 +1,25 @@
 test_that("color_scale() returns error on incorrect parameters", {
+  expect_error(
+    color_scale(param = c("a")),
+    regexp = 'argument "zlim" is missing, with no default',
+    fixed = TRUE
+  )
+  expect_error(
+    color_scale(param = c("a","b"), zlim = -4),
+    regexp = 'the condition has length > 1',
+    fixed = TRUE
+  )
+})
 
+test_that("add_color_transparency() returns error on incorrect parameters", {
+  expect_error(
+    add_color_transparency(),
+    regexp = "Please provide a vector or matrix of colours.",
+    fixed = TRUE
+  )
+  expect_error(
+    add_color_transparency(c("red","green","not_a_colour")),
+    regexp = "invalid color name 'not_a_colour'",
+    fixed = TRUE
+  )
 })
