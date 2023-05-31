438616a1bdec87b092e57fc8427038f5d1feed65
iskandari <sasha.tedeschi@gmail.com>
2023-05-31 13:48:33 -0400

    added remote hdf5 to read_vpts test

--- tests/testthat/test-read_vpts.R
+++ tests/testthat/test-read_vpts.R
@@ -1,6 +1,8 @@
-hdf5_local_1 <- ""
-hdf5_local_2 <- ""
-hdf5_local_other_radar <- ""
+hdf5_local_vp_1 <- "https://lw-enram.s3-eu-west-1.amazonaws.com/be/jab/2016/09/19/23/bejab_vp_201609192315.h5"
+hdf5_local_vp_2 <- "https://lw-enram.s3-eu-west-1.amazonaws.com/be/jab/2016/09/19/23/bejab_vp_201609192320.h5"
+hdf5_local_vp_other_radar <- ""
+
+
 vpts_gz_remote_1 <- "https://aloft.s3-eu-west-1.amazonaws.com/baltrad/monthly/bejab/2023/bejab_vpts_202303.csv.gz"
 vpts_gz_remote_2 <- "https://aloft.s3-eu-west-1.amazonaws.com/baltrad/monthly/bejab/2023/bejab_vpts_202304.csv.gz"
 vpts_gz_other_radar <- "https://aloft.s3-eu-west-1.amazonaws.com/baltrad/monthly/bewid/2023/bewid_vpts_202303.csv.gz"
@@ -20,10 +22,42 @@ test_that("read_vpts() returns error on mixed extensions", {
 
 
 test_that("read_vpts() can read local vp hdf5 files", {
+
+  urls <- c(hdf5_local_vp_1, hdf5_local_vp_2)
+  n <- length(urls)
+
+  # Test for one file
+  {
+    GET(urls[1], write_disk(temp_file))
+    result <- read_vpts_hdf5(temp_file)
+    expect_true(length(result) == 1, "Expected one vp object to be returned when reading one file.")
+
+    # Test if the output is a vpts object
+    expect_true(is.vpts(result), "Coverstion to vpts object failed.")
+
+    file.remove(temp_file)
+  }
+  # Test for multiple files
+  {
+    temp_files <- lapply(urls, function(url) {
+      temp_file <- tempfile(fileext = ".h5")
+      GET(url, write_disk(temp_file))
+      temp_file
+    })
+    result <- read_vpts_hdf5(temp_files)
+    expect_true(length(result) == n, paste("Expected", n, "vp objects to be returned when reading", n, "files."))
+
+    # Test if the output is a vpts object
+    expect_true(all(sapply(result, is.vpts)), "Coverstion to vpts object failed.")
+    file.remove(temp_files)
+  }
+
   # Test for one file
   # Test for multiple files
   # Returns vpts class
+
 })
+
 
 test_that("read_vpts() returns error on multiple radars in vp hdf5 files", {
 
