de4f23ec9bf82686a48ba90f6a4ad2e2a5d4ed7b
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-31 16:13:38 +0200

    add error message tests for read_pvolfile()

--- tests/testthat/test-read_pvolfile.R
+++ tests/testthat/test-read_pvolfile.R
@@ -1,3 +1,78 @@
-test_that("read_pvolfile() returns error on incorrect parameters", {
+pvolfile <- system.file("extdata", "volume.h5", package = "bioRad")
 
+test_that("read_pvolfile() returns error on incorrect parameters", {
+  expect_error(
+    read_pvolfile("not_a_filename"),
+    regexp= "does not exist in current working directory"
+  )
+  expect_error(
+    read_pvolfile(pvolfile, sort = 1),
+    regexp = "'sort' should be logical",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, sort = c(TRUE,TRUE)),
+    regexp = "'sort' should be logical",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, lat = 173),
+    regexp = "'lat' should be numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, lat = -173),
+    regexp = "'lat' should be numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, lat = "not_a_latitude"),
+    regexp = "'lat' should be numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, lat = c(71,83)),
+    regexp = "'lat' should be numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, lon = "not_a_longitude"),
+    regexp = "'lon' should be numeric between -360 and 360 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, lon = -4893),
+    regexp = "'lon' should be numeric between -360 and 360 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, lon = 4E9),
+    regexp = "'lon' should be numeric between -360 and 360 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, lon = c(42,36)),
+    regexp = "'lon' should be numeric between -360 and 360 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, height = -12),
+    regexp = "'height' should be a positive number of meters above sea level",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, height = "not_an_integer"),
+    regexp = "'height' should be a positive number of meters above sea level",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, height = c(12,42)),
+    regexp = "'height' should be a positive number of meters above sea level",
+    fixed = TRUE
+  )
+  expect_error(
+    read_pvolfile(pvolfile, height = -10),
+    regexp = "'height' should be a positive number of meters above sea level",
+    fixed = TRUE
+  )
 })
