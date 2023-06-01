4484c4f6b35a1d2fe7ee1b66d8d95801844c55b8
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-06-01 16:33:56 +0200

    stylr

--- tests/testthat/test-read_vpts.R
+++ tests/testthat/test-read_vpts.R
@@ -15,69 +15,80 @@ test_that("read_vpts() returns error on incorrect parameters", {
   )
   expect_error(
     read_vpts(vptsfile,
-              lat = -180),
+      lat = -180
+    ),
     regexp = "'lat' should be numeric between -90 and 90 degrees",
     fixed = TRUE
   )
   expect_error(
     read_vpts(vptsfile,
-              lat = 180),
+      lat = 180
+    ),
     regexp = "'lat' should be numeric between -90 and 90 degrees",
     fixed = TRUE
   )
   expect_error(
     read_vpts(vptsfile,
-              lat = "a"),
+      lat = "a"
+    ),
     regexp = "'lat' should be a single numeric between -90 and 90 degrees",
     fixed = TRUE
   )
   expect_error(
     read_vpts(vptsfile,
-              lat = c(48,32)),
+      lat = c(48, 32)
+    ),
     regexp = "'lat' should be a single numeric between -90 and 90 degrees",
     fixed = TRUE
   )
   expect_error(
     read_vpts(vptsfile,
-              lat = 66),
+      lat = 66
+    ),
     regexp = "'radar' argument missing. Required to specify a radar identifier.",
     fixed = TRUE
   )
   expect_error(
     read_vpts(vptsfile,
-              lon = -180),
+      lon = -180
+    ),
     regexp = "'radar' argument missing. Required to specify a radar identifier.",
     fixed = TRUE
   )
   expect_error(
     read_vpts(vptsfile,
-              lon = -1080,
-              lat = 38),
+      lon = -1080,
+      lat = 38
+    ),
     regexp = "lon' should be numeric between -360 and 360 degrees",
     fixed = TRUE
   )
   expect_error(
     read_vpts(vptsfile,
-              height = -1,
-              regexp = "'height' should be a positive number of meters above sea level",
-              fixed = TRUE)
+      height = -1
+    ),
+    regexp = "'height' should be a positive number of meters above sea level",
+    fixed = TRUE
   )
   expect_error(
     read_vpts(vptsfile,
-              height = NA,
-              regexp = "'height' should be a positive number of meters above sea level",
-              fixed = TRUE)
+      height = NA
+    ),
+    regexp = "height is not a number (a length one numeric vector).",
+    fixed = TRUE
   )
   expect_error(
     read_vpts(vptsfile,
-              height = NA,
-              regexp = "'height' should be a positive number of meters above sea level",
-              fixed = TRUE)
+      height = NA
+    ),
+    regexp = "height is not a number (a length one numeric vector).",
+    fixed = TRUE
   )
   expect_error(
     read_vpts(vptsfile,
-              radar = "KBGM",
-              height = seq(50)),
+      radar = "KBGM",
+      height = seq(50)
+    ),
     regexp = "height is not a number (a length one numeric vector).",
     fixed = TRUE
   )
