0e5f524c29d50f6e652ea208df6f415dfdab8153
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-06-01 13:00:01 +0200

    read_vpts() : add explicit error expectations

--- tests/testthat/test-read_vpts.R
+++ tests/testthat/test-read_vpts.R
@@ -1,3 +1,53 @@
-test_that("read_vpts() returns error on incorrect parameters", {
+vptsfile <- system.file("extdata", "example_vpts.txt", package = "bioRad")
 
+test_that("read_vpts() returns error on incorrect parameters", {
+  empty_file <- tempfile()
+  file.create(empty_file)
+
+  expect_error(
+    read_vpts(empty_file),
+    regex = glue::glue("File {empty_file} is empty."),
+    fixed = TRUE
+  )
+  expect_error(
+    read_vpts(tempfile()),
+    regex = glue::glue("doesn't exist.")
+  )
+  expect_error(
+    read_vpts(vptsfile,
+              lat = -180),
+    regexp = "'lat' should be numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_vpts(vptsfile,
+              lat = 180),
+    regexp = "'lat' should be numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_vpts(vptsfile,
+              lat = "a"),
+    regexp = "'lat' should be a single numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_vpts(vptsfile,
+              lat = c(48,32)),
+    regexp = "'lat' should be a single numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_vpts(vptsfile,
+              lon = -180),
+    regexp = 'argument "lat" is missing, with no default',
+    fixed = TRUE
+  )
+  expect_error(
+    read_vpts(vptsfile,
+              lon = -1080,
+              lat = 38),
+    regexp = "lon' should be numeric between -360 and 360 degrees",
+    fixed = TRUE
+  )
 })
