dd69cb576213d338d2f99421d1e2c2c9e9c23162
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-06-01 17:09:31 +0200

    add tests for error messaging

--- tests/testthat/test-apply_mistnet.R
+++ tests/testthat/test-apply_mistnet.R
@@ -1,3 +1,54 @@
-test_that("apply_mistnet() returns error on incorrect parameters", {
+temp_dir <- tempdir()
+download.file(
+  "https://noaa-nexrad-level2.s3.amazonaws.com/2019/10/01/KBGM/KBGM20191001_000542_V06",
+  file.path(temp_dir, "KBGM_example"),
+  quiet = TRUE
+)
 
+
+test_that("apply_mistnet() forwards errors from rhdf5", {
+  expect_error(
+    apply_mistnet(tempfile()),
+    regexp = "does not exist"
+  )
+})
+
+test_that("apply_mistnet() returns error on incorrect parameters", {
+  expect_error(
+    suppressMessages(
+      apply_mistnet(
+        file.path(temp_dir, "KBGM_example"),
+        mistnet_elevations = "a"
+      )
+    ),
+    regexp = "mistnet_elevations is not a numeric or integer vector"
+  )
+  expect_error(
+    suppressMessages(
+      apply_mistnet(
+        file.path(temp_dir, "KBGM_example"),
+        mistnet_elevations = 1:4
+      )
+    ),
+    regexp = "length(mistnet_elevations) not equal to 5",
+    fixed = TRUE
+  )
+  expect_error(
+    suppressMessages(
+      apply_mistnet(
+        file.path(temp_dir, "KBGM_example"),
+        mistnet_elevations = c(1:4, "b")
+      )
+    ),
+    regexp = "mistnet_elevations is not a numeric or integer vector"
+  )
+  expect_error(
+    suppressMessages(
+      apply_mistnet(
+        file.path(temp_dir, "KBGM_example"),
+        pvolfile_out = tempfile(tmpdir = "not_a_real_dir")
+      )
+    ),
+    regexp = "output directory not_a_real_dir not found"
+  )
 })
