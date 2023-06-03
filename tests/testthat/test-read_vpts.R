78f7f0f9faa27cd4c08ae0609ac9577a95a3df8b
iskandari <sasha.tedeschi@gmail.com>
2023-06-03 14:52:53 -0400

    all tests passing with one exception

--- tests/testthat/test-read_vpts.R
+++ tests/testthat/test-read_vpts.R
@@ -9,18 +9,20 @@ vpts_gz_other_radar <- "https://aloft.s3-eu-west-1.amazonaws.com/baltrad/monthly
 
 # TODO: create the other vpts files from the files above, by downloading and gzipping
 
-#Expect rerouting to read_stdout() with previous arguments
+# Expect rerouting to read_stdout() with previous arguments
 test_that("read_vpts correctly throws deprecation warning and reroutes to read_stdout", {
-
   vptsfile <- system.file("extdata", "example_vpts.txt", package = "bioRad")
 
-  expect_warning(read_vpts(file = vptsfile, radar = "radar", lat = 12, lon = 34, height = 1000),
-                 "deprecated")
+  expect_warning(
+    read_vpts(file = vptsfile, radar = "radar", lat = 12, lon = 34, height = 1000),
+    "deprecated"
+  )
 
-  #Test if outputs from both functions are equal but supress warnings in tests
-  suppressWarnings(expect_equal(read_vpts(files = vptsfile, radar = "radar", lat = 12, lon = 34, height = 1000),
-              read_stdout(file = vptsfile, radar = "radar", lat = 12, lon = 34, height = 1000, wavelength = "C", sep = ""))
-)
+  # Test if outputs from both functions are equal but supress warnings in tests
+  suppressWarnings(expect_equal(
+    read_vpts(files = vptsfile, radar = "radar", lat = 12, lon = 34, height = 1000),
+    read_stdout(file = vptsfile, radar = "radar", lat = 12, lon = 34, height = 1000, wavelength = "C", sep = "")
+  ))
 })
 
 test_that("read_vpts() returns error on mixed extensions", {
@@ -30,9 +32,7 @@ test_that("read_vpts() returns error on mixed extensions", {
   expect_error(read_vpts(files), "`files` must all have the same extension.")
 })
 
-
 test_that("read_vpts() can read local vp hdf5 files", {
-
   skip_if_offline()
 
   urls <- c(hdf5_local_vp_1, hdf5_local_vp_2)
@@ -46,7 +46,7 @@ test_that("read_vpts() can read local vp hdf5 files", {
 
     expect_true(length(result$datetime) == 1, "Expected one vp object to be returned when reading one file.")
     # Test if the output is a vpts object
-    expect_true(is.vpts(result), "Coverstion to vpts object failed.")
+    expect_true(is.vpts(result))
 
     file.remove(temp_file)
   }
@@ -61,7 +61,7 @@ test_that("read_vpts() can read local vp hdf5 files", {
   expect_true(length(result$datetime) == n, paste("Expected", n, "vp objects to be returned when reading", n, "files."))
 
   # Test if the output is a vpts object
-  expect_true(is.vpts(result), "Coverstion to vpts object failed.")
+  expect_true(is.vpts(result))
 
   # Remove temporary files
   for (temp_file in temp_files) {
@@ -70,11 +70,9 @@ test_that("read_vpts() can read local vp hdf5 files", {
 })
 
 test_that("read_vpts() returns error on multiple radars in vp hdf5 files", {
-
-  skip_if_offline()
+  skip('Not yet implemented')
 
   urls <- c(hdf5_local_vp_1, hdf5_local_vp_2, hdf5_local_vp_other_radar)
-  n <- length(urls)
 
   temp_files <- lapply(urls, function(url) {
     temp_file <- tempfile()
@@ -86,24 +84,80 @@ test_that("read_vpts() returns error on multiple radars in vp hdf5 files", {
     read_vpts(temp_files),
     "`files` must contain data of a single radar."
   )
-})
-
-test_that("read_vpts() can read local (gzipped) VPTS CSV files", {
-  skip_if_offline()
-  # Test for one file
-  # Test for multiple files
-  # Returns vpts class
+  unlink(temp_dir, recursive = TRUE)
 })
 
 test_that("read_vpts() can read remote (gzipped) VPTS CSV files", {
   skip_if_offline()
+
   # Test for one file
-  # Test for multiple files
+  url <- vpts_gz_remote_1
+  result <- read_vpts(url)
+
   # Returns vpts class
+  expect_true(is.vpts(result))
+
+  # Test for multiple files
+  urls <- c(vpts_gz_remote_1, vpts_gz_remote_2)
+  result <- read_vpts(urls)
+
+  # Returns vpts class
+  expect_true(is.vpts(result))
+})
+
+test_that("read_vpts() can read local (gzipped) VPTS CSV files", {
+  skip_if_offline()
+
+  # Test for one file
+  url <- vpts_gz_remote_1
+
+  temp_dir <- tempdir()
+  file_name <- basename(url)
+  dest_file <- file.path(temp_dir, file_name)
+  curl::curl_download(url, destfile = dest_file)
+  result <- read_vpts(dest_file)
+
+  # Returns vpts class
+  expect_true(is.vpts(result))
+
+  # Test for multiple files
+  urls <- c(vpts_gz_remote_1, vpts_gz_remote_2)
+
+  temp_files <- sapply(urls, function(url) {
+
+    temp_dir <- tempdir()
+    file_name <- basename(url)
+    dest_file <- file.path(temp_dir, file_name)
+    curl::curl_download(url, destfile = dest_file)
+    return(dest_file)
+  }, simplify = TRUE)
+
+  # Returns vpts class
+  expect_true(is.vpts(result))
+
+  unlink(temp_dir, recursive = TRUE)
+
 })
 
 test_that("read_vpts() returns error on multiple radars in VPTS CSV files", {
   skip_if_offline()
   # Note: this is a limitation until we switch to vpts data frame objects
+  urls <- c(vpts_gz_remote_1, vpts_gz_remote_2, vpts_gz_other_radar)
 
+  temp_files <- sapply(urls, function(url) {
+    # Download the gzip file
+
+    temp_dir <- tempdir()
+    file_name <- basename(url)
+    dest_file <- file.path(temp_dir, file_name)
+    curl::curl_download(url, destfile = dest_file)
+    return(dest_file)
+  }, simplify = TRUE)
+
+
+  expect_error(
+    read_vpts(temp_files),
+    "`files` must contain data of a single radar."
+  )
+  unlink(temp_dir, recursive = TRUE)
 })
