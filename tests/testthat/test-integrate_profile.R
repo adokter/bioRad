dc6d3b731c4e7be65bb7b4f33232b254a720c7c6
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-06-05 11:07:57 +0200

    set explicit error and warning expectations

--- tests/testthat/test-integrate_profile.R
+++ tests/testthat/test-integrate_profile.R
@@ -5,17 +5,64 @@ test_that("Integrate_profile() return vpi", {
 })
 
 test_that("integrate_profile() returns error on incorrect parameters", {
-  expect_error(integrate_profile(example_vp, alt_min = 'notANumeric'))
-  expect_error(integrate_profile(example_vp, alt_max = 'notANumeric'))
-  expect_error(integrate_profile(example_vp, alpha = 'notANumeric'))
-  expect_error(integrate_profile(example_vpts, interval_max = 'notANumeric'))
-  expect_error(integrate_profile(example_vpts, interval_replace = 'notANumeric'))
-  expect_error(integrate_profile(example_vp, alt_min = 500, alt_max = 300))
-  expect_error(integrate_profile(example_vp, height_quantile = "a"))
-  expect_error(integrate_profile(example_vp, height_quantile = 2))
-  expect_error(integrate_profile(example_vpts, height_quantile = "a"))
-  expect_error(integrate_profile(example_vpts, height_quantile = 2))
-  expect_error(integrate_profile(example_vp, alt_max = 2, alt_min = 3),
+  expect_error(
+    integrate_profile(example_vp, alt_min = 'notANumeric'),
+    regexp = 'alt_min is not a numeric or integer vector or alt_min not equal to "antenna"',
+    fixed = TRUE
+    )
+  expect_error(
+    integrate_profile(example_vp, alt_max = 'notANumeric'),
+    regexp = "alt_max is not a number (a length one numeric vector).",
+    fixed = TRUE
+    )
+  expect_error(
+    integrate_profile(example_vp, alt_max = c(pi, 4)),
+    regexp = "alt_max is not a number (a length one numeric vector).",
+    fixed = TRUE
+  )
+  expect_error(
+    integrate_profile(example_vp, alpha = 'notANumeric'),
+    regexp = "is.na(alpha) is not TRUE or alpha is not a numeric or integer vector",
+    fixed = TRUE
+    )
+  expect_error(
+    integrate_profile(example_vpts, interval_max = 'notANumeric'),
+    regexp = "interval_max is not a number (a length one numeric vector).",
+    fixed = TRUE
+    )
+  expect_error(
+    integrate_profile(example_vpts, interval_replace = 'notANumeric'),
+    regexp = "interval_replace is not a number (a length one numeric vector).",
+    fixed = TRUE
+    )
+  expect_error(
+    integrate_profile(example_vp, alt_min = 500, alt_max = 300),
+    regexp = "'alt_min' should be smaller than 'alt_max'",
+    fixed = TRUE
+    )
+  expect_error(
+    integrate_profile(example_vp, height_quantile = "a"),
+    regexp = "height_quantile is not a number (a length one numeric vector).",
+    fixed = TRUE
+    )
+  expect_error(
+    integrate_profile(example_vp, height_quantile = 2),
+    regexp = "height_quantile not less than 1",
+    fixed = TRUE
+    )
+  expect_error(
+    integrate_profile(example_vpts, height_quantile = "a"),
+    regexp = "height_quantile is not a number (a length one numeric vector).",
+    fixed = TRUE
+    )
+  expect_error(
+    integrate_profile(example_vpts, height_quantile = 2),
+    regexp = "height_quantile not less than 1",
+    fixed = TRUE
+    )
+  expect_error(
+    integrate_profile(example_vp, alt_max = 2, alt_min = 3)
+    ,
                regexp = "'alt_min' should be smaller than 'alt_max'",
                fixed = TRUE)
   expect_error(integrate_profile(example_vpts, alt_max = 2, alt_min = 3),
@@ -30,8 +77,12 @@ test_that("integrate_profile() returns error on non vp object in list input",{
 })
 
 test_that("integrate_profile() checks value of interval_max", {
-  expect_warning(integrate_profile(example_vpts, interval_max = 1))
-  expect_warning(integrate_profile(example_vpts, interval_max = 1, interval_replace=10))
+  expect_warning(integrate_profile(example_vpts, interval_max = 1),
+                 regexp = "interval_max < median timestep of the time series (300 sec), consider a larger value.",
+                 fixed = TRUE)
+  expect_warning(integrate_profile(example_vpts, interval_max = 1, interval_replace=10),
+                 regexp = "interval_max < median timestep of the time series (300 sec), consider a larger value.",
+                 fixed = TRUE)
 })
 
 test_that("rotating transect by 180 degrees results in sign flip", {
