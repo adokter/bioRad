2a064b48426244989f206dc57bb32e79b98c0192
Peter Desmet <peter.desmet@inbo.be>
2023-06-08 09:46:31 +0200

    Create test-read_stdout.R
    Adapted from https://github.com/adokter/bioRad/commit/2c6dce50a179a280f394d1e9798cf98df2df39e4
    
    Co-Authored-By: Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>

---
+++ tests/testthat/test-read_stdout.R
@@ -0,0 +1,125 @@
+vptsfile <- system.file("extdata", "example_vpts.txt", package = "bioRad")
+
+test_that("read_stdout() returns error on incorrect parameters", {
+  empty_file <- tempfile()
+  file.create(empty_file)
+
+  expect_error(
+    read_stdout(empty_file),
+    regex = glue::glue("File {empty_file} is empty."),
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(tempfile()),
+    regex = glue::glue("doesn't exist.")
+  )
+  expect_error(
+    read_stdout(vptsfile, lat = -180),
+    regexp = "'lat' should be numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, lat = 180),
+    regexp = "'lat' should be numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, lat = "a"),
+    regexp = "'lat' should be numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, lat = c(48, 32)),
+    regexp = "'lat' should be a single numeric between -90 and 90 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, lat = 66),
+    regexp = "'radar' argument missing. Required to specify a radar identifier.",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, lon = -180),
+    regexp = "'radar' argument missing. Required to specify a radar identifier.",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, lon = -1080, lat = 38),
+    regexp = "lon' should be numeric between -360 and 360 degrees",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, height = -1),
+    regexp = "'height' should be a positive number of meters above sea level",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, height = NA),
+    regexp = "height is not a number (a length one numeric vector).",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, height = NA),
+    regexp = "height is not a number (a length one numeric vector).",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, radar = "KBGM", height = seq(50)),
+    regexp = "height is not a number (a length one numeric vector).",
+    fixed = TRUE
+  )
+
+  wavelength_msg <-
+    glue::glue(
+      "'wavelength' should be a single positive number",
+      ", or one of 'C' or 'S' for C-band and S-band radar, respectively."
+    )
+  expect_error(
+    read_stdout(vptsfile, wavelength = "a", radar = "KBGM"),
+    regexp = wavelength_msg,
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, wavelength = -12, radar = "KBGM"),
+    regexp = wavelength_msg,
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, wavelength = 1:3, radar = "KBGM"),
+    regexp = wavelength_msg,
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, wavelength = "Q", radar = "KBGM"),
+    regexp = wavelength_msg,
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, wavelength = "S", radar = "KBGM", sep = "|"),
+    regexp = "'sep' should be either \",\" or \"\"",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, sep = NA),
+    regexp = "'sep' should be either \",\" or \"\"",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, sep = c(",", "&")),
+    regexp = "'sep' should be either \",\" or \"\"",
+    fixed = TRUE
+  )
+  expect_error(
+    read_stdout(vptsfile, sep = c(",", "||")),
+    regexp = "'sep' should be either \",\" or \"\"",
+    fixed = TRUE
+  )
+})
+
+test_that("read_stdout() warns for missing wavelength", {
+  expect_warning(
+    read_stdout(vptsfile, radar = "KBGM"),
+    regexp = "No 'wavelength' argument provided, assuming radar operates at C-band",
+    fixed = TRUE
+  )
+})
