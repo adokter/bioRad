f3c9ee7031ab0b13f87312a58ad98e4eeecbd4d8
iskandari <sasha.tedeschi@gmail.com>
2023-07-13 10:57:05 -0400

    example sections to the main body of data.R file

--- R/data.R
+++ R/data.R
@@ -13,12 +13,6 @@
 #'
 #' # Get summary info
 #' example_scan
-#'
-#' ## example_scan was created with:
-#' # pvolfile <- system.file("extdata", "volume.h5", package = "bioRad")
-#' # pvol <- read_pvolfile(pvolfile)
-#' # example_scan <- pvol$scans[[1]]
-#' # save(example_scan, file = "data/example_scan.rda")
 "example_scan"
 
 #' Vertical profile (`vp`) example
@@ -36,11 +30,6 @@
 #'
 #' # Get summary info
 #' example_vp
-#'
-#' ## example_vp was created with:
-#' # vpfile <- system.file("extdata", "profile.h5", package = "bioRad")
-#' # example_vp <- read_vpfiles(vpfile)
-#' # save(example_vp, file = "data/example_vp.rda")
 "example_vp"
 
 #' Time series of vertical profiles (`vpts`) example
@@ -58,15 +47,26 @@
 #'
 #' # Get summary info
 #' example_vpts
-#'
-#' ## example_vpts was created with
-#' # vptsfile <- system.file("extdata", "vpts.txt.zip", package = "bioRad")
-#' # utils::unzip(vptsfile, exdir = (dirname(vptsfile)), junkpaths = TRUE)
-#' # vptsfile <- substr(vptsfile, 1, nchar(vptsfile) - 4)
-#' # example_vpts <- read_vpts(vptsfile, radar = "KBGM", wavelength = "S")
-#' # rcs(example_vpts) <- 11
-#' # sd_vvp_threshold(example_vpts) <- 2
-#' # example_vpts$attributes$where$lat <- 42.2
-#' # example_vpts$attributes$where$lon <- -75.98
-#' # save(example_vpts, file = "data/example_vpts.rda", compress = "xz")
 "example_vpts"
+
+## example_scan was created with:
+# pvolfile <- system.file("extdata", "volume.h5", package = "bioRad")
+# pvol <- read_pvolfile(pvolfile)
+# example_scan <- pvol$scans[[1]]
+# save(example_scan, file = "data/example_scan.rda")
+
+## example_vp was created with:
+# vpfile <- system.file("extdata", "profile.h5", package = "bioRad")
+# example_vp <- read_vpfiles(vpfile)
+# save(example_vp, file = "data/example_vp.rda")
+
+## example_vpts was created with
+# vptsfile <- system.file("extdata", "vpts.txt.zip", package = "bioRad")
+# utils::unzip(vptsfile, exdir = (dirname(vptsfile)), junkpaths = TRUE)
+# vptsfile <- substr(vptsfile, 1, nchar(vptsfile) - 4)
+# example_vpts <- read_vpts(vptsfile, radar = "KBGM", wavelength = "S")
+# rcs(example_vpts) <- 11
+# sd_vvp_threshold(example_vpts) <- 2
+# example_vpts$attributes$where$lat <- 42.2
+# example_vpts$attributes$where$lon <- -75.98
+# save(example_vpts, file = "data/example_vpts.rda", compress = "xz")
