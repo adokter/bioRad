8e61b760247b2c9c06776e2917f1466df4b05c03
iskandari <sasha.tedeschi@gmail.com>
2023-10-19 18:40:13 -0400

    added conditional logic for as.vpts on whether to name reflectivity dbz_all or DBZH

--- tests/testthat/test-as.vpts.R
+++ tests/testthat/test-as.vpts.R
@@ -9,15 +9,15 @@ test_that("as.vpts() returns error message for incorrect data", {
 })
 
 
-test_that("as.vpts() returns 'DBZH' or 'dbz_all' based on from_csv argument", {
-  
+test_that("as.vpts() names reflectivity 'DBZH' or 'dbz_all' based on from_csv argument", {
+
   file <- system.file("extdata", "example_vpts.csv", package = "bioRad")
-  
+
   # When as.vpts() is called via read_vpts(), the reflectivity variable is named dbz_all in the resulting data.frame
   vpts_df <-  as.data.frame(read_vpts(file))
-  expect_true(!"DBZH" %in% colnames(vpts_from_csv))
-  expect_true("dbz_all" %in% colnames(vpts_from_csv))
-  
+  expect_true(!"DBZH" %in% colnames(vpts_df))
+  expect_true("dbz_all" %in% colnames(vpts_df))
+
   # When as.vpts() is called on a dataframe, the reflectivity variable will be renamed DBZH in the resulting vpts object
   vpts_obj <- as.vpts(vpts_df, from_csv = FALSE)
   expect_true("DBZH" %in% names(vpts_obj$data))
