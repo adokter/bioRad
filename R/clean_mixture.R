720046b1a03377ffdc068114393c5c4ea65a3c1d
Adriaan Dokter <adriaandokter@gmail.com>
2024-03-11 23:18:52 -0400

    add prototype clean_mixture function

---
+++ R/clean_mixture.R
@@ -0,0 +1,49 @@
+#' Partition mixtures of animals using assumptions on airspeeds.
+#'
+#' Partition mixtures of animals using assumptions on airspeeds.
+#' @param x a mixture animal density or linear reflectivity eta.
+#' @param u the mixture's ground speed u component (west to east)
+#' @param v the mixture's ground speed v component (south to north)
+#' @param U the west to east wind component
+#' @param V the south to north wind component
+#' @param fast the fast component's airspeed
+#' @param slow the slow component's airspeed
+#' @param drop_slow_component when TRUE (default) output density, ground speed and
+#' heading for fast component, when FALSE for slow component.
+#' @return a data.frame with corrected density or reflectivity x, ground speed (u, v)
+#' and heading in clockwise degrees from north.
+#' @export
+#' @examples
+#' # drop the slow component (typically insects)
+#' clean_mixture(100,-13,13,-7,6, fast=12, slow=1)
+#' # drop the fast component (typically birds)
+#' clean_mixture(100,-13,13,-7,6, fast=12, slow=1, drop_slow_component=FALSE)
+clean_mixture <- function(x, u, v, U, V, slow = 1, fast = 12, drop_slow_component = TRUE){
+  wind_speed <- sqrt(U^2 + V^2)
+  wind_direction <- atan2(V,U)
+  mixture_airspeed <- sqrt((u-U)^2 + (v-V)^2)
+  if(mixture_airspeed > fast) warning("Airspeed of mixture exceeds airspeed of fast component")
+  if(mixture_airspeed < slow) warning("Airspeed of slow component exceeds airspeed of mixture.")
+  r <- slow/wind_speed
+  p1 <- slow^2 - fast^2
+  p2 <- 2*(wind_speed + fast^2 - slow*(u*U+v*V)/wind_speed)
+  p3 <- (u-U)^2 + (v-V)^2 - fast^2
+  # signal proportion attributed to the slow component:
+  f <- (-p2+sqrt(p2^2-4*p1*p3))/(2*p1)
+
+  if(drop_slow_component){
+    # fast component airspeed, typically birds:
+    air_u=((u-U)-(slow/wind_speed)*U*f)/(1-f)
+    air_v=((v-V)-(slow/wind_speed)*V*f)/(1-f)
+    x_corr=(1-f)*x
+  } else{
+    # slow component airspeed, typically insects:
+    air_u=slow*cos(wind_direction)
+    air_v=slow*sin(wind_direction)
+    x_corr=(f*x)
+  }
+
+  data.frame(x=x_corr,u=U+air_u,v=V+air_v, heading=(pi/2-atan2(air_v,air_u))*180/pi)
+}
+
+
