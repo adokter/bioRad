0c9d85989bde8e48b7d0784c5fb5f35f589ee893
Adriaan Dokter <adriaandokter@gmail.com>
2024-03-11 23:45:35 -0400

    add assertions

--- R/clean_mixture.R
+++ R/clean_mixture.R
@@ -19,22 +19,48 @@
 #' # drop the fast component (typically birds)
 #' clean_mixture(100,-13,13,-7,6, fast=12, slow=1, drop_slow_component=FALSE)
 clean_mixture <- function(x, u, v, U, V, slow = 1, fast = 12, drop_slow_component = TRUE){
+  # verify input
+  assertthat::assert_that(x >= 0)
+  assertthat::assert_that(assertthat::is.number(u))
+  assertthat::assert_that(assertthat::is.number(v))
+  assertthat::assert_that(assertthat::is.number(U))
+  assertthat::assert_that(assertthat::is.number(V))
+  assertthat::assert_that(slow >= 0)
+  assertthat::assert_that(fast > 0)
+  assertthat::assert_that(fast > slow)
+  assertthat::assert_that(is.flag(drop_slow_component))
+
+  # define helper quantities:
   wind_speed <- sqrt(U^2 + V^2)
   wind_direction <- atan2(V,U)
   mixture_airspeed <- sqrt((u-U)^2 + (v-V)^2)
-  if(mixture_airspeed > fast) warning("Airspeed of mixture exceeds airspeed of fast component")
-  if(mixture_airspeed < slow) warning("Airspeed of slow component exceeds airspeed of mixture.")
-  r <- slow/wind_speed
-  p1 <- slow^2 - fast^2
-  p2 <- 2*(wind_speed + fast^2 - slow*(u*U+v*V)/wind_speed)
-  p3 <- (u-U)^2 + (v-V)^2 - fast^2
-  # signal proportion attributed to the slow component:
-  f <- (-p2+sqrt(p2^2-4*p1*p3))/(2*p1)
+
+  # catch limiting cases
+  if(mixture_airspeed > fast){
+    warning("Airspeed of mixture exceeds airspeed of fast component, assigning all weight to fast component")
+    f=0
+  }
+  if(mixture_airspeed < slow){
+    warning("Airspeed of slow component exceeds airspeed of mixture, assigning all weight to slow component")
+    f=1
+  }
+  if(mixture_airspeed <= fast & mixture_airspeed >= slow){
+    p1 <- slow^2 - fast^2
+    p2 <- 2*(wind_speed + fast^2 - slow*(u*U+v*V)/wind_speed)
+    p3 <- (u-U)^2 + (v-V)^2 - fast^2
+    # signal proportion attributed to the slow component:
+    f <- (-p2+sqrt(p2^2-4*p1*p3))/(2*p1)
+  }
 
   if(drop_slow_component){
     # fast component airspeed, typically birds:
-    air_u=((u-U)-(slow/wind_speed)*U*f)/(1-f)
-    air_v=((v-V)-(slow/wind_speed)*V*f)/(1-f)
+    if(f==1){
+      air_u=NA
+      air_v=NA
+    } else{
+      air_u=((u-U)-(slow/wind_speed)*U*f)/(1-f)
+      air_v=((v-V)-(slow/wind_speed)*V*f)/(1-f)
+    }
     x_corr=(1-f)*x
   } else{
     # slow component airspeed, typically insects:
