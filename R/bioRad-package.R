690735c98d605e5b2d7a6804403dd672418a8b1b
Adriaan Dokter <adriaandokter@gmail.com>
2025-08-21 17:16:44 -0400

    rebuild doc

--- R/bioRad-package.R
+++ R/bioRad-package.R
@@ -7,8 +7,7 @@
 #' an introduction to bioRad's main functionalities.
 #' * [Function reference](https://adriaandokter.com/bioRad/reference/index.html)
 #' : an overview of all bioRad functions.
-#'
-#' See also package [getRad::getRad-package] for accessing and downloading radar data.
+#' * See also the \link[getRad]{getRad-package} for accessing and downloading radar data.
 #' @keywords internal
 #' @importFrom dplyr %>% .data
 "_PACKAGE"
