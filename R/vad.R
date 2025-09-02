8101251abf04ac79d1c767fa23efb86de9d984f4
Bart <bart@myotis>
2025-09-02 22:03:01 +0200

    fix cran checks

--- R/vad.R
+++ R/vad.R
@@ -1,3 +1,6 @@
+globalVariables(c("DBZH","VRADH","RHOHV"))
+NULL
+
 #' Create a velocity azimuth display (VAD) plot
 #'
 #' A velocity azimuth display plot visualized the radial velocity (`VRAD`) as a function of the azimuth from the radar.
@@ -13,16 +16,16 @@
 #'      If only one value is provided next to a `vp` then the height bin intersection with this elevation is plotted.
 #' @param range_gate_filter Optional filtering of the range gates. By default range gates are filtered for a `DBZH` less then 20 and a `RHOHV` less then 0.95.
 #'      Alternative filters could be used to highlight specific effects.
-#' @param point_geom The geom to visualize the range gates, the default is [geom_points()], in some cases this suffers from over plotting.
-#'      Alternatives that avoid over plotting could be [geom_bin2d()] or [ggpointdensity::geom_pointdensity()].
+#' @param point_geom The geom to visualize the range gates, the default is [ggplot2::geom_point()], in some cases this suffers from over plotting.
+#'      Alternatives that avoid over plotting could be [ggplot2::geom_bin2d()] or [ggpointdensity::geom_pointdensity()].
 #' @param point_geom_args Additional arguments to the `point_geom` function. For example, controlling the point size or alpha.
-#' @param annotate A [glue()] string that is used to annotate the plot with additional properties of the height bin of the `vp`.
+#' @param annotate A [glue::glue()] string that is used to annotate the plot with additional properties of the height bin of the `vp`.
 #'      The string is evaluated using the columns from `as.data.frame(vp)`.
 #'      Use `NULL` if no annotation is desired.
 #' @param vp_color The color used for the `vp` annotations and line.
 #' @param annotation_size The text size used for the annotation.
 #'
-#' @returns A [ggplot] object. Additional elements can be added using regular function in the `ggplot2` package.
+#' @returns A [ggplot2::ggplot] object. Additional elements can be added using regular function in the `ggplot2` package.
 #'
 #' @export
 #' @examples
@@ -61,8 +64,8 @@ vad.pvol <- function(x, vp = NULL,..., range = NULL, height = NULL,
   assertthat::assert_that(is.pvol(x))
   if (!is.null(vp)) {
     vp_df <- as.data.frame(vp) |> dplyr::mutate(
-      heightBin = glue::glue("{height}-{height + vp$attributes$where$interval} [m]"),
-      heightBin = factor(heightBin, levels = heightBin)
+      height_bin = glue::glue("{height}-{height + vp$attributes$where$interval} [m]"),
+      height_bin = factor(.data$height_bin, levels = .data$height_bin)
     )
 
     assertthat::assert_that(
@@ -101,46 +104,61 @@ vad.pvol <- function(x, vp = NULL,..., range = NULL, height = NULL,
               as.data.frame
       ),
       split(attribute_table(x) |>
-              dplyr::mutate(scanNr=1:dplyr::n())|>
-        dplyr::select(-param), 1:length(x$scans)), MoreArgs = list(row.names = NULL)
+              dplyr::mutate(scan_nr=1:dplyr::n())|>
+        dplyr::select(-"param"), 1:length(x$scans)), MoreArgs = list(row.names = NULL)
     ) |>
     dplyr::bind_rows() |>
     dplyr::filter(
-      !is.na(azim), !is.na(VRADH),
+      !is.na(.data$azim), !is.na(VRADH),
       .data$range > min(!!range), .data$range < max(!!range),
       .data$height < max(!!height), .data$height > min(!!height),
       !!rlang::enexpr(range_gate_filter)
     )
   if (!is.null(vp)) {
     s <- !(is.na(vp_df$ff) | is.na(vp_df$dd))
-    vp_geom <- purrr::pmap(
-      list(vp_df$ff[s], vp_df$dd[s], vp_df$height[s], vp_df$heightBin[s]),
-      ~ ggplot2::geom_function(
+    # vp_geom <- purrr::pmap(
+    #   list(vp_df$ff[s], vp_df$dd[s], vp_df$height[s], vp_df$height_bin[s]),
+    #   ~ ggplot2::geom_function(
+    #     data = dplyr::bind_cols(data, data.frame(
+    #       min_bin_height = ..3,
+    #       max_bin_height = ..3 + vp$attributes$where$interval,
+    #       height_bin = ..4
+    #     )),
+    #     fun = function(x, v, a) cos((x - a) / 180 * pi) * v,
+    #     args = list(a = ..2, v = ..1),
+    #     color = vp_color
+    #   )
+    # )
+    vp_geom <- mapply(SIMPLIFY = F,
+      function(spd, dir,hgt, bin)
+      {ggplot2::geom_function(
         data = dplyr::bind_cols(data, data.frame(
-          minBinHeight = ..3,
-          maxBinHeight = ..3 + vp$attributes$where$interval,
-          heightBin = ..4
+          min_bin_height = hgt,
+          max_bin_height = hgt + vp$attributes$where$interval,
+          height_bin = bin
         )),
         fun = function(x, v, a) cos((x - a) / 180 * pi) * v,
-        args = list(a = ..2, v = ..1),
+        args = list(a = dir, v = spd),
         color = vp_color
-      )
+      )},
+
+      vp_df$ff[s], vp_df$dd[s], vp_df$height[s], vp_df$height_bin[s]
     )
     if(length(vp_geom)>1){
-      vp_geom<-c(vp_geom, ggplot2::facet_wrap(~heightBin))
+      vp_geom<-c(vp_geom, ggplot2::facet_wrap(~.data$height_bin))
     }
     int <- findInterval(data$height, c(vp_df$height, max(vp_df$height) + vp$attributes$where$interval))
     int[int == 0] <- NA
-    data$heightBin <- vp_df$heightBin[int]
+    data$height_bin <- vp_df$height_bin[int]
   } else {
     vp_geom <- list()
   }
   if(!is.null(annotate) & is.vp(vp)){
-    df<-data.frame(label=as.character(glue::glue_data(annotate, .x=vp_df)),x=Inf,y=Inf, heightBin=vp_df$heightBin)
+    df<-data.frame(label=as.character(glue::glue_data(annotate, .x=vp_df)),x=Inf,y=Inf, height_bin=vp_df$height_bin)
     annotate_geom<-
      list( ggplot2::geom_label(
         data=df,
-        ggplot2::aes(x=x,y=y, label = label),
+        ggplot2::aes(x=x,y=y, label = .data$label),
         vjust = "inward", hjust = "inward", color=vp_color,
         fill=NA, label.size = 0, size=annotation_size
     ))
@@ -148,7 +166,7 @@ vad.pvol <- function(x, vp = NULL,..., range = NULL, height = NULL,
   }else{
     annotate_geom<-list()
   }
-  plt <- ggplot2::ggplot(data, ggplot2::aes(x = azim, y = VRADH)) +
+  plt <- ggplot2::ggplot(data, ggplot2::aes(x = .data$azim, y = VRADH)) +
     do.call(point_geom, point_geom_args) +
     ggplot2::scale_x_continuous(breaks = (0:4) * 90, minor_breaks = (0:12) * 30) +
     ggplot2::ylab("Radial velocity [m/s]") +
